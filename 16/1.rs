2392fa6155222e098f3f940b1baa2fc8ccab77e2
Konsta 'sooda' Hölttä <sooda@iki.fi>
2016-12-01 19:39:44 +0200

    1 part two, repeats
    track locations trivially, plus some stupid fixes

--- 16/1.rs
+++ 16/1.rs
@@ -11,15 +11,16 @@ fn readfile(name: &str) -> String {
 
 fn process(row: &str) {
     // current position
-    let mut x = 0;
-    let mut y = 0;
+    let mut x = 0i32;
+    let mut y = 0i32;
     // delta vector, each turn flips these somehow
-    let mut dx = 0;
-    let mut dy = 1;
+    let mut dx = 0i32;
+    let mut dy = 1i32;
+    let mut visits = Vec::new();
 
     for op in row.split(", ") {
         let rotation = op.chars().next().unwrap();
-        let travel = &op[1..].parse::<i32>().unwrap();
+        let travel = op[1..].parse::<i32>().unwrap();
         // "multiply" by a rotation matrix of 90 or -90 deg
         std::mem::swap(&mut dx, &mut dy);
         match rotation {
@@ -27,14 +28,20 @@ fn process(row: &str) {
             'R' => { dy = -dy; }, // dx, dy =  dy, -dx;
             _ => unreachable!()
         }
-        x += dx * travel;
-        y += dy * travel;
+        for _ in 0..travel {
+            x += dx;
+            y += dy;
+            if visits.contains(&(x, y)) {
+                println!("double {} {} dist {}", x, y, x.abs() + y.abs());
+            }
+            visits.push((x, y));
+        }
     }
     println!("x {}, y {} dist {}", x, y, x.abs() + y.abs());
 }
 
 fn main() {
-    let src = readfile("1.input");
+    let src = readfile(&std::env::args().nth(1).unwrap());
     for row in src.split("\n") {
         if row != "" { process(row); }
     }
