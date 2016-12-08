ad48ea2f747975ef9ac247f89334dee885dce3ea
Konsta 'sooda' Hölttä <sooda@iki.fi>
2016-12-08 19:39:31 +0200

    7b aba bab bruteforce
    stupid but simple loop thru all supernets and then all hypernets per
    each found aba

--- 16/7.rs
+++ 16/7.rs
@@ -1,6 +1,8 @@
 use std::fs::File;
 use std::io::Read;
 
+use std::str;
+
 fn readfile(name: &str) -> String {
     let mut f = File::open(name).unwrap();
     let mut s = String::new();
@@ -9,6 +11,7 @@ fn readfile(name: &str) -> String {
     s
 }
 
+// Autonomous Bridge Bypass Annotation
 fn has_abba(s: &str) -> bool {
     let bs = s.as_bytes();
     for i in 0..bs.len() - 3 {
@@ -20,6 +23,7 @@ fn has_abba(s: &str) -> bool {
     false
 }
 
+// transport-layer snooping
 fn support_tls(row: &str) -> u32 {
     let mut nonhyper_abba = false;
     // split into outer and inner parts of square brackets
@@ -42,10 +46,63 @@ fn support_tls(row: &str) -> u32 {
     nonhyper_abba as u32
 }
 
+// Area-Broadcast Accessor
+fn next_aba(s: &str, start: usize) -> Option<usize> {
+    let bs = s.as_bytes();
+    for i in start .. bs.len()-2 {
+        if bs[i] == bs[i+2] && bs[i] != bs[i+1] {
+            return Some(i);
+        }
+    }
+
+    None
+}
+
+// bab of aba in a hypernet?
+fn has_bab(row: &str, aba: &str) -> bool {
+    let aba = aba.as_bytes();
+    let bab = &[aba[1], aba[0], aba[1]];
+    let bab = str::from_utf8(bab).unwrap();
+
+    for pair in row.split("]") {
+        let mut parts = pair.split("[");
+        let _ = parts.next().unwrap(); // ignore supernet
+        if let Some(hyper) = parts.next() {
+            if hyper.find(bab).is_some() {
+                return true;
+            }
+        }
+    }
+
+    false
+}
+
+// super-secret listening
+fn support_ssl(row: &str) -> u32 {
+    // such bloat, search again for each supernet's aba
+    for pair in row.split("]") {
+        let mut parts = pair.split("[");
+        let supernet = parts.next().unwrap(); // always there
+        let mut idx = 0usize;
+        while let Some(next) = next_aba(supernet, idx) {
+            idx = next;
+            if has_bab(row, &supernet[idx..idx+3]) {
+                return 1;
+            }
+            idx += 1;
+        }
+        // hypernet ignored here
+    }
+
+    0
+}
+
 fn main() {
     let src = readfile(&std::env::args().nth(1).unwrap());
     let sum = src.trim().split("\n").map(support_tls).sum::<u32>();
-    println!("{}", sum);
+    println!("tls {}", sum);
+    let sum = src.trim().split("\n").map(support_ssl).sum::<u32>();
+    println!("ssl {}", sum);
 }
 
 
