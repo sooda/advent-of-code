70f5e62e6e6837d87759e5023fa2a63aa2e9c339
Konsta 'sooda' Hölttä <sooda@iki.fi>
2016-12-09 22:16:43 +0200

    9b format version two
    markers within decomp data are decompressed: recurse for each split,
    combine the results so many repeats get multiplied

--- 16/9.rs
+++ 16/9.rs
@@ -30,9 +30,32 @@ fn decompressed_length(mut input: &str) -> usize {
     len
 }
 
+// lol, not even quite sure how this works or if there's anything extra. recursion magic
+fn decompressed_length_v2b(mut input: &str) -> usize {
+    let mut len = 0;
+    while let Some(open_paren) = input.find('(') {
+        len += open_paren; // basic alphabet before the marker
+        let close = input.find(')').unwrap();
+        let parens = &input[open_paren + 1..close]; // let (parens) = input
+
+        let mut sp = parens.split("x");
+        let take = sp.next().unwrap().parse::<usize>().unwrap();
+        let repeat = sp.next().unwrap().parse::<usize>().unwrap();
+        // recurse for the number of chars captured by this repeat
+        len += repeat * decompressed_length_v2b(&input[close + 1..close + 1 + take]);
+
+        // +1 to start after ), skip take amount of chars
+        input = &input[close + 1 + take..];
+    }
+    len += input.len();
+
+    len
+}
+
 fn main() {
     let src = readfile(&std::env::args().nth(1).unwrap());
     for row in src.trim().split("\n") {
-        println!("{}", decompressed_length(row));
+        println!("v1 {}", decompressed_length(row));
+        println!("v2 {}", decompressed_length_v2b(row));
     }
 }
