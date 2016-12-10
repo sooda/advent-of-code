e958f4cfc7b63cf50c8e2b8adb1792a87765c60b
Konsta 'sooda' Hölttä <sooda@iki.fi>
2016-12-10 20:00:30 +0200

    10 balance bots
    Read bot instructions, evaluate the graph instructions for all values
    are propagated, then dump bot states to find which bot compares which
    two values.
    
    Values of each bot are only assigned once. There are no dupes in the
    graph.

---
+++ 16/10.rs
@@ -0,0 +1,142 @@
+use std::fs::File;
+use std::io::Read;
+use std::mem::swap;
+//
+// rustc -L foo/deps 10.rs
+extern crate regex;
+use regex::Regex;
+
+fn readfile(name: &str) -> String {
+    let mut f = File::open(name).unwrap();
+    let mut s = String::new();
+    f.read_to_string(&mut s).unwrap();
+
+    s
+}
+
+#[derive(Clone, Debug)]
+enum Dest {
+    Bot(usize),
+    Output(usize)
+}
+
+// no zeros for values in the input
+#[derive(Clone, Debug)]
+struct Bot {
+    lo: usize,
+    hi: usize,
+    lo_dest: Dest,
+    hi_dest: Dest
+}
+
+fn assign(bot: &mut Bot, val: usize) {
+    // is_none might be more rusty tho
+    if bot.lo == 0 {
+        bot.lo = val;
+    }
+    else {
+        assert!(bot.hi == 0); // must not be full yet
+        assert!(bot.lo != val); // no duplicates
+        bot.hi = val;
+        if bot.lo > bot.hi {
+            swap(&mut bot.lo, &mut bot.hi);
+        }
+    }
+}
+
+fn action(input: &str, bots: &mut Vec<Bot>) {
+    let re_goesto = Regex::new(r"value (\d+) goes to bot (\d+)").unwrap();
+    let re_gives = Regex::new(r"bot (\d+) gives low to (bot|output) (\d+) and high to (bot|output) (\d+)").unwrap();
+    if let Some(cap) = re_goesto.captures(input) {
+        let val = cap.at(1).unwrap().parse::<usize>().unwrap();
+        let bot_num = cap.at(2).unwrap().parse::<usize>().unwrap();
+
+        if bot_num >= bots.len() {
+            bots.resize(bot_num + 1, Bot { lo: 0, hi: 0, lo_dest: Dest::Bot(0), hi_dest: Dest::Bot(0) });
+        }
+
+        assign(&mut bots[bot_num], val);
+    } else if let Some(cap) = re_gives.captures(input) {
+        let bot_num = cap.at(1).unwrap().parse::<usize>().unwrap();
+        let lo_dest_type = cap.at(2).unwrap();
+        let lo_dest = cap.at(3).unwrap().parse::<usize>().unwrap();
+        let hi_dest_type = cap.at(4).unwrap();
+        let hi_dest = cap.at(5).unwrap().parse::<usize>().unwrap();
+
+        if bot_num >= bots.len() {
+            bots.resize(bot_num + 1, Bot { lo: 0, hi: 0, lo_dest: Dest::Bot(0), hi_dest: Dest::Bot(0) });
+        }
+
+        bots[bot_num].lo_dest =
+            if lo_dest_type == "bot" { Dest::Bot(lo_dest) } else { Dest::Output(lo_dest) };
+        bots[bot_num].hi_dest =
+            if hi_dest_type == "bot" { Dest::Bot(hi_dest) } else { Dest::Output(hi_dest) };
+    } else {
+        unreachable!()
+    }
+}
+
+fn evaluate(bots: &mut Vec<Bot>) {
+    let mut handled = vec![false; bots.len()];
+    let mut remaining = bots.len();
+    while remaining > 0 {
+        // cannot iter() because that would borrow and thus couldn't mutate
+        for i in 0..bots.len() {
+            if handled[i] {
+                continue;
+            }
+
+            let (lo, lod, hi, hid) = {
+                let b = &bots[i];
+                println!("{} {:?}", i, b);
+                let mut lo = 0; let mut lod = 0;
+                let mut hi = 0; let mut hid = 0;
+                // fully filled values, this is handled now
+                if b.lo != 0 && b.hi != 0 {
+                    handled[i] = true;
+                    remaining -= 1;
+                    // can give only when both lo and hi are known
+                    if let Dest::Bot(d) = b.lo_dest {
+                        if b.lo != 0 {
+                            lo = b.lo;
+                            lod = d;
+                        }
+                    }
+                    if let Dest::Bot(d) = b.hi_dest {
+                        if b.hi != 0 {
+                            hi = b.hi;
+                            hid = d;
+                        }
+                    }
+                }
+                (lo, lod, hi, hid)
+            };
+
+            // dests are bots, not outputs? then pass values to them
+            if lo != 0 {
+                assert!(lod != i);
+                println!("{} -> {}", lo, lod);
+                assign(&mut bots[lod], lo);
+            }
+            if hi != 0 {
+                assert!(hid != i);
+                println!("{} -> {}", hi, hid);
+                assign(&mut bots[hid], hi);
+            }
+        }
+
+    }
+}
+
+fn main() {
+    let src = readfile(&std::env::args().nth(1).unwrap());
+    let mut bots = Vec::new();
+    for row in src.trim().split("\n") {
+        action(row, &mut bots);
+    }
+    evaluate(&mut bots);
+    for (i, b) in bots.iter().enumerate() {
+        println!("bot {} compares {} with {}", i, b.lo, b.hi);
+    }
+}
+
