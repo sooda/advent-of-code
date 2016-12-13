553686a326d812d482124663708be9f5fa451a5c
Konsta 'sooda' Hölttä <sooda@iki.fi>
2016-12-13 20:11:58 +0200

    13b move at most 50 steps
    Count distinct cubicles that can be reached with at most 50 steps. No
    goal locations.

--- 16/13.rs
+++ 16/13.rs
@@ -56,10 +56,42 @@ fn search(x0: Coord, y0: Coord, endx: Coord, endy: Coord, fav: Coord) -> Depth {
     unreachable!()
 }
 
+fn search_steps(x0: Coord, y0: Coord, fav: Coord, max: usize) -> u32 {
+    let mut visited = Vec::new();
+    visited.resize(1 << 30, 0u64);
+    visited[(state(x0, y0) / 64) as usize] = 1 << (state(x0, y0) % 64);
+    let mut positions = 1;
+
+    let mut queue = VecDeque::new();
+    queue.push_back((0, x0, y0));
+
+    let moves = [(-1, 0), (0, 1), (1, 0), (0, -1)];
+    while let Some((steps, x, y)) = queue.pop_front() {
+        for &(dx, dy) in moves.iter() {
+            if dy == -1 && y == 0 { continue; }
+            if dx == -1 && x == 0 { continue; }
+
+            let xx = x + dx;
+            let yy = y + dy;
+            let s = state(xx, yy);
+
+            if !wall(xx, yy, fav) && visited[(s / 64) as usize] & (1 << (s % 64)) == 0 && steps + 1 <= max {
+                positions += 1;
+                visited[(s / 64) as usize] |= 1 << (s % 64);
+                queue.push_back((steps + 1, xx, yy));
+            }
+        }
+    }
+
+    positions
+}
+
 fn main() {
     // sample
     println!("{}", search(1, 1, 7, 4, 10));
     // input
     let favorite = readfile(&std::env::args().nth(1).unwrap()).trim().parse::<Coord>().unwrap();
     println!("{}", search(1, 1, 31, 39, favorite));
+    // second half
+    println!("{}", search_steps(1, 1, favorite, 50));
 }
