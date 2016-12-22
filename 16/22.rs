ada379da93310f19e187add31dadb26307efe2a4
Konsta 'sooda' Hölttä <sooda@iki.fi>
2016-12-22 23:02:52 +0200

    22 draw a map, add sample for b
    hmm, there's a long horizontal line of huge nodes and the empty node is
    under them.
    
    this looks like the puzzle with a grid of sliding pieces in all but one
    position, ugh

--- 16/22.rs
+++ 16/22.rs
@@ -32,6 +32,7 @@ fn parse(input: String) -> Option<Node> {
 fn main() {
     let input = BufReader::new(File::open(&std::env::args().nth(1).unwrap()).unwrap()).lines().map(Result::unwrap);
     let nodes = input.filter_map(parse).collect::<Vec<_>>();
+
     let mut viable = 0;
     for (i, a) in nodes.iter().enumerate() {
         for (j, b) in nodes.iter().enumerate() {
@@ -41,4 +42,20 @@ fn main() {
         }
     }
     println!("{}", viable);
+
+    let w = nodes.iter().map(|n| n.x).max().unwrap() + 1; // starts from 0
+    let h = nodes.iter().map(|n| n.y).max().unwrap() + 1;
+    let dummy = Node { x: 0, y: 0, size: 0, used: 0 };
+    let mut map = vec![&dummy; (w * h) as usize];
+    let pos = |x, y| (y * w + x) as usize;
+    for n in nodes.iter() {
+        map[pos(n.x, n.y)] = &n;
+    }
+    for y in 0..h {
+        for x in 0..w {
+            let n = map[pos(x, y)];
+            print!("{:3}/{:3} ", n.used, n.size);
+        }
+        println!("");
+    }
 }
