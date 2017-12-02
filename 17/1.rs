fac863b6f90c810c511c5415f53f9b6d201ea313
Konsta 'sooda' Hölttä <sooda@iki.fi>
2017-12-02 19:01:58 +0200

    1b halfway around the list

--- 17/1.rs
+++ 17/1.rs
@@ -2,19 +2,34 @@ use std::fs::File;
 use std::io::BufReader;
 use std::io::BufRead;
 
-fn solve(input: &str) -> u32 {
+fn solve(input: &str, offset: usize) -> u32 {
     let a = input.chars();
-    let b = input.chars().cycle().skip(1);
+    let b = input.chars().cycle().skip(offset);
     a.zip(b).map(
         |(i, j)| if i == j { i as u32 - '0' as u32 } else { 0 }).sum()
 }
 
+fn solve_a(input: &str) -> u32 {
+    solve(input, 1)
+}
+
+fn solve_b(input: &str) -> u32 {
+    solve(input, input.len() / 2)
+}
+
 fn main() {
-    assert!(solve("1122") == 3);
-    assert!(solve("1111") == 4);
-    assert!(solve("1234") == 0);
-    assert!(solve("91212129") == 9);
+    assert!(solve_a("1122") == 3);
+    assert!(solve_a("1111") == 4);
+    assert!(solve_a("1234") == 0);
+    assert!(solve_a("91212129") == 9);
     let input = BufReader::new(File::open(&std::env::args().nth(1).unwrap()).unwrap())
         .lines().next().unwrap().unwrap();
-    println!("{}", solve(&input));
+    println!("{}", solve_a(&input));
+
+    assert!(solve_b("1212") == 6);
+    assert!(solve_b("1221") == 0);
+    assert!(solve_b("123425") == 4);
+    assert!(solve_b("123123") == 12);
+    assert!(solve_b("12131415") == 4);
+    println!("{}", solve_b(&input));
 }
