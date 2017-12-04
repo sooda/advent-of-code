dd5a7b950239f19d37927a77932a9dcdae847c81
Konsta 'sooda' Hölttä <sooda@iki.fi>
2017-12-04 08:37:30 +0200

    4b for added security, no anagrams
    Build the set using sorted words, so words with identical characters
    become non-unique.

--- 17/4.rs
+++ 17/4.rs
@@ -11,6 +11,21 @@ fn valid_passphrase(line: &str) -> bool {
     words_in_order.len() == word_set.len()
 }
 
+fn added_security(line: &str) -> bool {
+    if !valid_passphrase(line) {
+        return false;
+    }
+    let words_in_order = line.split(" ").collect::<Vec<_>>();
+    let sort_chars = |&s: &&str| -> String {
+        let mut cs = s.chars().collect::<Vec<_>>();
+        cs.sort_by(|a, b| b.cmp(a));
+        String::from_iter(cs.iter())
+    };
+    let word_set: HashSet<String> = HashSet::from_iter(words_in_order.iter().map(sort_chars));
+
+    words_in_order.len() == word_set.len()
+}
+
 fn main() {
     assert!(valid_passphrase("aa bb cc dd ee"));
     assert!(!valid_passphrase("aa bb cc dd aa"));
@@ -19,6 +34,12 @@ fn main() {
         .lines().map(|x| x.unwrap()).collect::<Vec<_>>();
     let sum = input_lines.iter().map(|l| valid_passphrase(&l)).filter(|&x| x).count();
     println!("{}", sum);
+
+    assert!(added_security("abcde fghij"));
+    assert!(!added_security("abcde xyz ecdab"));
+    assert!(added_security("a ab abc abd abf abj"));
+    assert!(added_security("iiii oiii ooii oooi oooo"));
+    assert!(!added_security("oiii ioii iioi iiio"));
+    let sum = input_lines.iter().map(|l| added_security(&l)).filter(|&x| x).count();
+    println!("{}", sum);
 }
-
-
