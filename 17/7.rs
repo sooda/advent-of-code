a2ceadbb921b6547ecf48949c5a3de22bbfdfd57
Konsta 'sooda' Hölttä <sooda@iki.fi>
2017-12-07 21:30:20 +0200

    7b one has the wrong weight
    Resolving this needs some manual work because it's in the debug prints;
    couldn't be bothered to return the actual puzzle answer directly.
    
    Depth search the tree: the answer is the first anomaly in differing
    weights for the programs, lighter or heavier than others.

--- 17/7.rs
+++ 17/7.rs
@@ -7,11 +7,13 @@ use std::collections::HashMap;
 #[derive(Clone, Debug)]
 struct Node<'a> {
     name: &'a str,
+    weight: u32,
     children: Vec<usize>
 }
 
 struct NodeSpec {
     name: String,
+    weight: u32,
     children: Vec<String>
 }
 
@@ -20,7 +22,10 @@ fn parse_line(line: &str) -> NodeSpec {
     // jptl (61)
     let mut mapping = line.split(" -> ");
     let name_children = mapping.next().unwrap();
-    let name = name_children.split(" ").next().unwrap();
+    let mut name_weight = name_children.split(" ");
+    let name = name_weight.next().unwrap();
+    // should have used regexes, this is awful
+    let weight = (&name_weight.next().unwrap()[1..]).split(")").next().unwrap().parse().unwrap();
     let mut children_names = vec![];
     if let Some(children) = mapping.next() {
         for name in children.split(", ") {
@@ -28,7 +33,7 @@ fn parse_line(line: &str) -> NodeSpec {
         }
     }
 
-    NodeSpec { name: name.to_string(), children: children_names }
+    NodeSpec { name: name.to_string(), weight: weight, children: children_names }
 }
 
 // parent is the one that contains this program as a child
@@ -40,10 +45,42 @@ fn parent(programs: &[Node], program: usize) -> Option<usize> {
 }
 
 // bottom of the stack is the root of the tree: the node that has no parent
-fn bottom_program<'a>(programs: &'a [Node]) -> &'a str {
+fn bottom_program<'a>(programs: &'a [Node]) -> &'a Node<'a> {
     programs.iter().enumerate()
         .find(|&(i, _)| parent(programs, i).is_none())
-        .unwrap().1.name
+        .unwrap().1
+}
+
+fn bottom_program_name<'a>(programs: &'a [Node]) -> &'a str {
+    bottom_program(programs).name
+}
+
+fn track<'a>(programs: &'a [Node], n: &Node) -> u32 {
+    if n.children.len() == 0 {
+        return n.weight;
+    }
+
+    let mut weights = n.children.iter()
+        .map(|&ci| &programs[ci])
+        .map(|c| (track(programs, c), c.name, c.weight))
+        .collect::<Vec<_>>();
+
+    assert!(weights.len() > 1); // this seems to be the case
+    weights.sort();
+
+    if weights[0].0 < weights[1].0 || weights[weights.len() - 1].0 > weights[weights.len() - 2].0 {
+        println!("!! {} {:?}", n.name, weights);
+        println!("!! sides {} {}",
+                 weights[0].2 + weights[1].0 - weights[0].0,
+                 weights[weights.len() - 1].2 - (weights[weights.len() - 1].0 - weights[weights.len() - 2].0),
+                 );
+    }
+    //println!("{} {}", n.name, n.weight + weights.iter().sum::<u32>());
+    n.weight + weights.iter().map(|&(w, _, _)| w).sum::<u32>()
+}
+
+fn b<'a>(programs: &'a [Node]) -> u32 {
+    track(programs, bottom_program(programs))
 }
 
 fn main() {
@@ -51,7 +88,8 @@ fn main() {
         .lines().map(|x| parse_line(&x.unwrap())).collect::<Vec<_>>();
     // get node names without children first, because the children are indices
     let mut nodes = nodespecs.iter()
-        .map(|ns| Node { name: &ns.name, children: vec![] }).collect::<Vec<_>>();
+        .map(|ns| Node { name: &ns.name, weight: ns.weight, children: vec![] })
+        .collect::<Vec<_>>();
     // name -> index mapping
     let indices: HashMap<&str, usize> = nodes.iter().enumerate()
         .map(|(i, n)| { (n.name, i) }).collect();
@@ -64,5 +102,7 @@ fn main() {
     }
     println!("{:?}", nodes);
     // tknk for sample
-    println!("{}", bottom_program(&nodes));
+    println!("{}", bottom_program_name(&nodes));
+    // sample: ugml has to be 60
+    println!("total weight is {} but it's unrelated", b(&nodes));
 }
