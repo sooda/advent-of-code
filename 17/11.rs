5dd546d2008c8325db9b7084468516165c31cb92
Konsta 'sooda' Hölttä <sooda@iki.fi>
2017-12-11 22:58:39 +0200

    11b the furthest he ever got
    Just calculate the distance in each step and store the max.
    
    The number.max() vs std::cmp::max() stuff confuses me.

--- 17/11.rs
+++ 17/11.rs
@@ -2,8 +2,14 @@ use std::fs::File;
 use std::io::BufReader;
 use std::io::BufRead;
 
-fn steps_away(path: &[&str]) -> i32 {
+fn dist(x: i32, y: i32) -> i32 {
+    // this seems to work on pen & paper
+    std::cmp::max(std::cmp::max(x.abs(), y.abs()), (-x - y).abs())
+}
+
+fn steps_away(path: &[&str]) -> (i32, i32) {
     let (mut x, mut y) = (0i32, 0i32);
+    let mut maxdist = 0;
     for &turn in path {
         let (dx, dy) = match turn {
             "n" => (0, 1),
@@ -16,19 +22,19 @@ fn steps_away(path: &[&str]) -> i32 {
         };
         x += dx;
         y += dy;
+        maxdist = maxdist.max(dist(x, y))
     }
-    // this seems to work on pen & paper
-    println!("{} {} {}", x.abs(), y.abs(), (-x - y).abs());
-    std::cmp::max(std::cmp::max(x.abs(), y.abs()), (-x - y).abs())
+
+    (dist(x, y), maxdist)
 }
 
 fn main() {
-    assert!(steps_away(&"ne,ne,ne".split(",").collect::<Vec<_>>()) == 3);
-    assert!(steps_away(&"ne,ne,sw,sw".split(",").collect::<Vec<_>>()) == 0);
-    assert!(steps_away(&"ne,ne,s,s".split(",").collect::<Vec<_>>()) == 2);
-    assert!(steps_away(&"se,sw,se,sw,sw".split(",").collect::<Vec<_>>()) == 3);
+    assert!(steps_away(&"ne,ne,ne".split(",").collect::<Vec<_>>()).0 == 3);
+    assert!(steps_away(&"ne,ne,sw,sw".split(",").collect::<Vec<_>>()).0 == 0);
+    assert!(steps_away(&"ne,ne,s,s".split(",").collect::<Vec<_>>()).0 == 2);
+    assert!(steps_away(&"se,sw,se,sw,sw".split(",").collect::<Vec<_>>()).0 == 3);
     let line = BufReader::new(File::open(&std::env::args().nth(1).unwrap()).unwrap())
         .lines().next().unwrap().unwrap();
     let path = line.split(",").collect::<Vec<_>>();
-    println!("{}", steps_away(&path));
+    println!("{:?}", steps_away(&path));
 }
