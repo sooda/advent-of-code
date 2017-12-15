fdca9259fe136238296dcd031dd29a9d8fbe9a2a
Konsta 'sooda' Hölttä <sooda@iki.fi>
2017-12-15 08:31:59 +0200

    ok use iterator fun for 15 anyway
    Scan on an infinite iterator generates those very nicely, then just
    count what gets filtered.

--- 17/15.rs
+++ 17/15.rs
@@ -2,40 +2,16 @@ use std::fs::File;
 use std::io::BufReader;
 use std::io::BufRead;
 
-fn matches_in_pairs(mut a: u64, mut b: u64, num: usize) -> usize {
-    let mut matches = 0;
-    for _ in 0..num {
-        a = a * 16807 % 2147483647;
-        b = b * 48271 % 2147483647;
-        if a & 0xffff == b & 0xffff {
-            matches += 1;
-        }
-    }
-
-    matches
+fn matches_in_pairs(a0: u64, b0: u64, num: usize) -> usize {
+    let a_gen = (0..).scan(a0, |a, _| { *a = *a * 16807 % 2147483647; Some(*a) });
+    let b_gen = (0..).scan(b0, |b, _| { *b = *b * 48271 % 2147483647; Some(*b) });
+    a_gen.zip(b_gen).take(num).filter(|&(a, b)| a & 0xffff == b & 0xffff).count()
 }
 
-fn picky_matches_in_pairs(mut a: u64, mut b: u64, num: usize) -> usize {
-    let mut matches = 0;
-    for _ in 0..num {
-        loop {
-            a = a * 16807 % 2147483647;
-            if a & 3 == 0 {
-                break;
-            }
-        }
-        loop {
-            b = b * 48271 % 2147483647;
-            if b & 7 == 0 {
-                break;
-            }
-        }
-        if a & 0xffff == b & 0xffff {
-            matches += 1;
-        }
-    }
-
-    matches
+fn picky_matches_in_pairs(a0: u64, b0: u64, num: usize) -> usize {
+    let a_gen = (0..).scan(a0, |a, _| { *a = *a * 16807 % 2147483647; Some(*a) }).filter(|a| a & 3 == 0);
+    let b_gen = (0..).scan(b0, |b, _| { *b = *b * 48271 % 2147483647; Some(*b) }).filter(|b| b & 7 == 0);
+    a_gen.zip(b_gen).take(num).filter(|&(a, b)| a & 0xffff == b & 0xffff).count()
 }
 
 fn main() {
