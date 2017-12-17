84c424011aa1fab89993285733627ecf59b76574
Konsta 'sooda' Hölttä <sooda@iki.fi>
2017-12-17 10:19:23 +0200

    17b the spinlock gets more angry
    Now that we're only interested in the value after 0, the other values
    don't need to be computed since the place of 0 never changes. Otherwise
    shuffling them around 50M times would be too heavy.

--- 17/17.rs
+++ 17/17.rs
@@ -17,10 +17,28 @@ fn short_circuit_value(step: usize) -> usize {
     buf[prev]
 }
 
+fn more_angry_value(step: usize) -> usize {
+    let mut pos_after = 0;
+    let mut len = 1;
+    let mut after_0 = 0;
+
+    for value in 1..50000000 {
+        if pos_after == 0 {
+            after_0 = value;
+        }
+        len += 1;
+        pos_after += 1 + step;
+        pos_after %= len;
+    }
+
+    after_0
+}
+
 fn main() {
     assert!(short_circuit_value(3) == 638);
 
     let input = BufReader::new(File::open(&std::env::args().nth(1).unwrap()).unwrap())
         .lines().next().unwrap().unwrap().parse().unwrap();
     println!("{}", short_circuit_value(input));
+    println!("{}", more_angry_value(input));
 }
