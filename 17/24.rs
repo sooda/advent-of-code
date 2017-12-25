08f998350f1467336467a11037421dc7587bd732
Konsta 'sooda' Hölttä <sooda@iki.fi>
2017-12-25 10:33:50 +0200

    24b the bridge was not long enough
    Strength of the longest bridge is now of interest; count the dfs depth
    as well and find the max based on that.
    
    TODO: cleanup this copypasta mess

--- 17/24.rs
+++ 17/24.rs
@@ -84,8 +84,60 @@ fn depth(strewn: &[Component]) -> usize {
     amax.max(bmax)
 }
 
+fn dfs2(level: usize, strewn: &[Component], ci: usize, in_port_a: bool, conns: &[(Vec<(usize, bool)>, Vec<(usize, bool)>)], visited: &mut [bool]) -> (usize, usize) {
+    let c = &strewn[ci];
+    if visited[ci] {
+        return (0, 0);
+    }
+    //println!("{:->1$} lev {2} {3}/{4}", "", level, level, c.a, c.b);
+    visited[ci] = true;
+    let child = if in_port_a {
+        conns[ci].1.iter()
+            .map(|&(neigh_ci, neigh_in)| dfs2(level+1, strewn, neigh_ci, neigh_in, conns, visited)).max().unwrap_or((0, 0))
+    } else {
+        conns[ci].0.iter()
+            .map(|&(neigh_ci, neigh_in)| dfs2(level+1, strewn, neigh_ci, neigh_in, conns, visited)).max().unwrap_or((0, 0))
+    };
+    let ret = (1 + child.0, c.a + c.b + child.1);
+    //println!("{:->1$} lev {2} {3}/{4} skore {5}", "", level, level, c.a, c.b, ret);
+    visited[ci] = false; // mark this free for further calls; alternatively a clone of visited for each call could work
+    ret
+}
+
+// ugh but it doesn't make sense to dfs these really? at least not with a simple visited map...
+// however, a proper search might not be that heavy in this graph, looks like so
+fn depth2(strewn: &[Component]) -> (usize, usize) {
+    let mut conns = Vec::new(); // list of indices from either port
+    for (i, c) in strewn.iter().enumerate() {
+        let (mut a, mut b) = (Vec::new(), Vec::new());
+        for (j, cc) in strewn.iter().enumerate() {
+            if j == i {
+                continue;
+            }
+            if c.a == cc.a {
+                a.push((j, true));
+            }
+            if c.a == cc.b {
+                a.push((j, false));
+            }
+            if c.b == cc.a {
+                b.push((j, true));
+            }
+            if c.b == cc.b {
+                b.push((j, false));
+            }
+        }
+        conns.push((a, b));
+    }
+    let mut visited = vec![false; strewn.len()];
+    let amax = strewn.iter().enumerate().filter(|&(_, c)| c.a == 0).map(|(ci, _)| dfs2(0, strewn, ci, true, &conns, &mut visited)).max().unwrap_or((0, 0));
+    let bmax = strewn.iter().enumerate().filter(|&(_, c)| c.b == 0).map(|(ci, _)| dfs2(0, strewn, ci, false, &conns, &mut visited)).max().unwrap_or((0, 0));
+    amax.max(bmax)
+}
+
 fn main() {
     let compo_strewn = BufReader::new(File::open(&std::env::args().nth(1).unwrap()).unwrap())
         .lines().map(|x| parse_line(&x.unwrap())).collect::<Vec<_>>();
     println!("{}", depth(&compo_strewn));
+    println!("{:?}", depth2(&compo_strewn));
 }
