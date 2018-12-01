5e11dcbe66b7a9269bc94f4c44a1c4d61184477a
Konsta 'sooda' Hölttä <sooda@iki.fi>
2018-12-01 11:29:31 +0200

    1b frequency calibration
    Collect all frequency states in a set and detect the first repetition.

--- 18/1.rs
+++ 18/1.rs
@@ -1,9 +1,30 @@
 use std::fs::File;
 use std::io::BufReader;
 use std::io::BufRead;
+use std::collections::HashSet;
+
+fn calibrate(diffs: &[i32]) -> i32 {
+    let mut history = HashSet::new();
+    let mut current_freq = 0;
+
+    loop {
+        for diff in diffs {
+            history.insert(current_freq);
+            current_freq += diff;
+
+            if history.contains(&current_freq) {
+                return current_freq;
+            }
+        }
+    }
+}
 
 fn main() {
-    let diffs = BufReader::new(File::open(&std::env::args().nth(1).unwrap()).unwrap()).lines();
-    let x = diffs.fold(0, |acc, x| acc + x.unwrap().parse::<i32>().unwrap());
+    let parsed_diffs = BufReader::new(File::open(&std::env::args().nth(1).unwrap()).unwrap())
+        .lines().map(|x| x.unwrap().parse::<i32>().unwrap()).collect::<Vec<_>>();
+
+    let x = parsed_diffs.iter().fold(0, |acc, x| acc + x);
     println!("{}", x);
+
+    println!("{}", calibrate(&parsed_diffs));
 }
