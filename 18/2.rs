86e56626785cdd9571bc47eed269166c2ba34e4c
Konsta 'sooda' Hölttä <sooda@iki.fi>
2018-12-02 08:50:55 +0200

    2b prototype fabric boxes differ by just one char
    Loop pairs of boxes and find the pair that has exactly one differing
    character, and remove that character to get the common letters.

--- 18/2.rs
+++ 18/2.rs
@@ -13,6 +13,43 @@ fn exactly_n(id: &str, n: usize) -> bool {
     false
 }
 
+fn compare_boxes(a: &str, b: &str) -> Option<String> {
+    let mut pos = None;
+    for (i, (x, y)) in a.chars().zip(b.chars()).enumerate() {
+        if x != y {
+            if pos.is_some() {
+                // more than one chars differ
+                return None;
+            }
+            pos = Some(i);
+        }
+    }
+    if let Some(pos) = pos {
+        let chs = a.chars().enumerate()
+            .filter(|&(i, _)| i != pos)
+            .map(|(_, c)| c).collect::<Vec<_>>();
+
+        Some(chs.into_iter().collect())
+    } else {
+        // exactly the same
+        unreachable!();
+    }
+}
+fn proto_fabric_boxes(ids: &[String]) -> String {
+    for a in ids {
+        for b in ids {
+            if a != b {
+                let common_letters = compare_boxes(a, b);
+                if let Some(cl) = common_letters {
+                    return cl;
+                }
+            }
+        }
+    }
+
+    unreachable!()
+}
+
 fn main() {
     let ids = BufReader::new(File::open(&std::env::args().nth(1).unwrap()).unwrap())
         .lines().map(|x| x.unwrap()).collect::<Vec<_>>();
@@ -20,4 +57,5 @@ fn main() {
     let twos = ids.iter().filter(|x| exactly_n(x, 2)).count();
     let threes = ids.iter().filter(|x| exactly_n(x, 3)).count();
     println!("{}", twos * threes);
+    println!("{}", proto_fabric_boxes(&ids));
 }
