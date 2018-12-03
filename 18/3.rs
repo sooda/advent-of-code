bacd5599bf4a951e9a52885f917692885eafeb65
Konsta 'sooda' Hölttä <sooda@iki.fi>
2018-12-03 12:20:43 +0200

    3b one claim doesn't overlap
    Loop the claims again to see which one is the sole user of its cells.

--- 18/3.rs
+++ 18/3.rs
@@ -5,23 +5,24 @@ use std::io::BufRead;
 extern crate regex;
 use regex::Regex;
 
-fn parse_line(re: &Regex, line: &str) -> (usize, usize, usize, usize) {
+fn parse_line(re: &Regex, line: &str) -> (usize, usize, usize, usize, usize) {
     let cap = re.captures(line).unwrap();
-    let x = cap.get(1).unwrap().as_str().parse().unwrap();
-    let y = cap.get(2).unwrap().as_str().parse().unwrap();
-    let w = cap.get(3).unwrap().as_str().parse().unwrap();
-    let h = cap.get(4).unwrap().as_str().parse().unwrap();
+    let id = cap.get(1).unwrap().as_str().parse().unwrap();
+    let x = cap.get(2).unwrap().as_str().parse().unwrap();
+    let y = cap.get(3).unwrap().as_str().parse().unwrap();
+    let w = cap.get(4).unwrap().as_str().parse().unwrap();
+    let h = cap.get(5).unwrap().as_str().parse().unwrap();
 
-    (x, y, w, h)
+    (id, x, y, w, h)
 }
 
 fn main() {
-    let re = Regex::new(r"#\d+ @ (\d+),(\d+): (\d+)x(\d+)").unwrap();
+    let re = Regex::new(r"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)").unwrap();
     let claims = BufReader::new(File::open(&std::env::args().nth(1).unwrap()).unwrap())
         .lines().map(|x| parse_line(&re, &x.unwrap())).collect::<Vec<_>>();
     let mut fabric = vec![0; 1000 * 1000];
-    for c in claims {
-        let (x0, y0, w, h) = c;
+    for &c in &claims {
+        let (_, x0, y0, w, h) = c;
         for y in y0..y0+h {
             for x in x0..x0+w {
                 fabric[y * 1000 + x] += 1;
@@ -29,4 +30,16 @@ fn main() {
         }
     }
     println!("{}", fabric.iter().filter(|&&x| x > 1).count());
+    for &c in &claims {
+        let (id, x0, y0, w, h) = c;
+        let mut n = 0;
+        for y in y0..y0+h {
+            for x in x0..x0+w {
+                n += fabric[y * 1000 + x];
+            }
+        }
+        if n == w * h {
+            println!("{}", id);
+        }
+    }
 }
