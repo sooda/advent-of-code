bbc81888805f9e4f82c0bb684c507dc0253d19f4
Konsta 'sooda' Hölttä <sooda@iki.fi>
2018-12-13 21:33:50 +0200

    6b find a region, not coordinate
    We need the size of the region where the distance to all coordinates is
    less than 10000. Happens to work by just summing all positions around
    the given coordinates where the distances are such; the friendly region
    is (by chance?) contiguous.

--- 18/6.rs
+++ 18/6.rs
@@ -38,6 +38,19 @@ fn render(coords: &[(i32, i32)], map: &mut [usize], x0: i32, y0: i32, w: i32, h:
     counts
 }
 
+fn friendly_region(coords: &[(i32, i32)], x0: i32, y0: i32, w: i32, h: i32) -> i32 {
+    let mut count = 0;
+    for y in 0..h {
+        for x in 0..w {
+            let dist_sum = coords.iter().map(|&c| distance(c, x0 + x, y0 + y)).sum::<i32>();
+            if dist_sum < 10000 {
+                count += 1;
+            }
+        }
+    }
+    count
+}
+
 fn main() {
     let coords = BufReader::new(File::open(&std::env::args().nth(1).unwrap()).unwrap())
         .lines().map(|x| parse(&x.unwrap())).collect::<Vec<_>>();
@@ -54,4 +67,7 @@ fn main() {
     // hmm, cheating: happened to get the first part right even without checking whether this area
     // is infinite (i.e., on the border of the map just right)
     println!("{}", counts.iter().max().unwrap());
+    // the region is probably nicely contiguous
+    let blotch = friendly_region(&coords, x0, y0, w, h);
+    println!("{}", blotch);
 }
