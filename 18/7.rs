6b89c2791286ec90f5995e9b3cd58a2cb2e45ddc
Konsta 'sooda' Hölttä <sooda@iki.fi>
2018-12-13 22:44:44 +0200

    7b elves offer to help
    The sun will set soon, so distribute the work to a number of workers in
    parallel. Track the clock one second at a time and figure out how long
    it takes.

--- 18/7.rs
+++ 18/7.rs
@@ -9,9 +9,7 @@ fn parse_rule(line: &str) -> (usize, usize) {
     ((first - b'A') as usize, (then - b'A') as usize)
 }
 
-fn main() {
-    let rules = BufReader::new(File::open(&std::env::args().nth(1).unwrap()).unwrap())
-        .lines().map(|x| parse_rule(&x.unwrap())).collect::<Vec<_>>();
+fn ideal_order(rules: &[(usize, usize)]) -> String {
     let n = (b'z' - b'a' + 1) as usize;
 
     let mut finished = vec![false; n];
@@ -19,7 +17,7 @@ fn main() {
 
     loop {
         let mut pending = vec![false; n];
-        for &r in &rules {
+        for &r in rules {
             if !finished[r.0] {
                 pending[r.1] = true;
             }
@@ -34,5 +32,70 @@ fn main() {
         }
     }
 
+    order
+}
+
+fn parallel_order_time(rules: &[(usize, usize)], min_time: usize, worker_count: usize) -> usize {
+    let n = (b'z' - b'a' + 1) as usize;
+
+    let mut started = vec![false; n];
+    let mut finished = vec![false; n];
+    let mut order = String::new();
+    let mut clock = 0;
+    // (start_time, work_item_index)
+    let mut workers: Vec<Option<(usize, usize)>> = vec![None; worker_count];
+
+    loop {
+        // complete ongoing work
+        for w in &mut workers {
+            if let Some((started, item)) = *w {
+                if started + min_time + item == clock {
+                    println!("doned {:?} at {}", w, clock);
+                    finished[item] = true;
+                    order.push((b'A' + item as u8) as char);
+                    *w = None;
+                }
+            }
+
+        }
+
+        if finished.iter().all(|&f| f == true) {
+            break;
+        }
+
+        let mut pending = vec![false; n];
+        for &r in rules {
+            if !finished[r.0] {
+                pending[r.1] = true;
+            }
+        }
+
+        // find next work and start it for the elves
+        for w in &mut workers {
+            if w.is_some() {
+                continue;
+            }
+
+            if let Some(next) = pending.iter().enumerate().position(|(i, &p)| p == false && !started[i]) {
+                println!("start {} at {}", next, clock);
+                *w = Some((clock, next));
+                started[next] = true;
+            }
+        }
+
+        clock += 1;
+    }
+
     println!("{}", order);
+
+    clock
+}
+
+fn main() {
+    let rules = BufReader::new(File::open(&std::env::args().nth(1).unwrap()).unwrap())
+        .lines().map(|x| parse_rule(&x.unwrap())).collect::<Vec<_>>();
+
+    println!("{}", ideal_order(&rules));
+    // println!("{}", parallel_order_time(&rules, 1, 2)); // also fix 'z' -> 'f'
+    println!("{}", parallel_order_time(&rules, 61, 5));
 }
