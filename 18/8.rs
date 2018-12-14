d623cb2126677328d39301b4775fbded0753b8d7
Konsta 'sooda' Hölttä <sooda@iki.fi>
2018-12-14 11:44:24 +0200

    8b value of the root node
    Now the value of a node that has children is the sum of such children
    that are indexed by metadata, and there can be duplicates. Node with no
    children has a value of just the metadata sum; the recursion ends there.
    
    Not sure how effective this iterator dance is, but with raw vectors we'd
    still need to skip children like that. Trivially fast even without a
    clever lookup structure.

--- 18/8.rs
+++ 18/8.rs
@@ -1,3 +1,4 @@
+#![recursion_limit="10"]
 use std::fs::File;
 use std::io::BufReader;
 use std::io::BufRead;
@@ -13,10 +14,61 @@ fn metadata_sum(v: &mut Iter<u32>) -> u32 {
     child_sum + own_sum
 }
 
+// ignore contents, traverse iterator
+fn skip_node_recursive(v: &mut Iter<u32>) {
+    let children = *v.next().unwrap();
+    let metadata_entries = *v.next().unwrap();
+
+    for _ in 0..children {
+        skip_node_recursive(v);
+    }
+    for _ in 0..metadata_entries {
+        v.next();
+    }
+}
+
+// v is skipped to the start of next node
+fn node_value(v: &mut Iter<u32>) -> u32 {
+    let children = *v.next().unwrap();
+    let metadata_entries = *v.next().unwrap();
+
+    if children == 0 {
+        // just sum of metadata
+        (0..metadata_entries).fold(0, |total, _| total + v.next().unwrap())
+    } else {
+        // sum of child node values indexed by metadata entries of this node
+
+        // Hard to index children, easy to index metadata entries. Skip another iterator to the
+        // start of the metadata block.
+        let mut metadata = v.clone();
+        for _ in 0..children {
+            skip_node_recursive(&mut metadata);
+        }
+
+        let metas = metadata.take(metadata_entries as usize).collect::<Vec<_>>();
+
+        let mut sum = 0;
+        for i in 0..children {
+            let val = node_value(v);
+            let n = metas.iter().filter(|&&&v| v == 1 + i).count() as u32;
+            sum += n * val;
+        }
+
+        // Now v is at the metadata front, skip to back so the parent stays in sync.
+        for _ in 0..metadata_entries {
+            v.next();
+        }
+
+        sum
+    }
+}
+
 fn main() {
     let file = BufReader::new(File::open(&std::env::args().nth(1).unwrap()).unwrap())
         .lines().next().unwrap().unwrap();
     let license = file.split(" ").map(|x| x.parse::<u32>().unwrap()).collect::<Vec<_>>();
 
     println!("{}", metadata_sum(&mut license.iter()));
+    println!("{}", node_value(&mut license.iter()));
 }
+
