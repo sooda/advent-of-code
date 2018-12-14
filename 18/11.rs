55aac2561a5432a7a11c83c88b0091addca22ff8
Konsta 'sooda' Hölttä <sooda@iki.fi>
2018-12-14 17:15:33 +0200

    11b a size dial is discovered
    Trivial solver: find the square with the largest total power by testing
    all sizes up to 300. Finishes in almost four minutes. Should make this
    at least a bit more clever; looks like a DP problem.

--- 18/11.rs
+++ 18/11.rs
@@ -3,16 +3,20 @@ fn fuel_cell_power_level(x: i32, y: i32, serial: i32) -> i32 {
     (rack_id * y + serial) * rack_id / 100 % 10 - 5
 }
 
-fn square_power(x0: i32, y0: i32, serial: i32) -> i32 {
-    (0..3).flat_map(|y| (0..3).map(move |x| {
+fn square_power(x0: i32, y0: i32, serial: i32, sz: i32) -> i32 {
+    (0..sz).flat_map(|y| (0..sz).map(move |x| {
         fuel_cell_power_level(x0 + x, y0 + y, serial)
     })).sum()
 }
 
-fn powerest_square(serial: i32) -> (i32, i32, i32) {
-    (1..=300-2).flat_map(|y| (1..=300-2).map(move |x| {
-        (square_power(x, y, serial), x, y)
+fn powerest_square(serial: i32, sz: i32) -> (i32, i32, i32) {
+    (1..=300-sz+1).flat_map(|y| (1..=300-sz+1).map(move |x| {
+        (square_power(x, y, serial, sz), x, y)
     })).max().unwrap()
+}
+
+fn largest_total_square(serial: i32) -> ((i32, i32, i32), i32) {
+    (0..300).map(|sz| (powerest_square(serial, sz), sz)).max().unwrap()
 }
 
 fn main() {
@@ -20,10 +24,15 @@ fn main() {
     assert!(fuel_cell_power_level(122, 79, 57) == -5);
     assert!(fuel_cell_power_level(217, 196, 39) == 0);
     assert!(fuel_cell_power_level(101, 153, 71) == 4);
-    assert!(square_power(33, 45, 18) == 29);
-    assert!(square_power(21, 61, 42) == 30);
-    assert!(powerest_square(18) == (29, 33, 45));
-    assert!(powerest_square(42) == (30, 21, 61));
+    assert!(square_power(33, 45, 18, 3) == 29);
+    assert!(square_power(21, 61, 42, 3) == 30);
+    assert!(powerest_square(18, 3) == (29, 33, 45));
+    assert!(powerest_square(42, 3) == (30, 21, 61));
+
     let puzzle_input = 9110;
-    println!("{:?}", powerest_square(puzzle_input));
+    println!("{:?}", powerest_square(puzzle_input, 3));
+
+    println!("{:?}", largest_total_square(puzzle_input));
+    assert!(largest_total_square(18) == ((113, 90, 269), 16));
+    assert!(largest_total_square(42) == ((119, 232, 251), 12));
 }
