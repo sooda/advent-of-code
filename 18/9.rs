7914b39ff03fcd4a7babe1fe53f134cffb06e693
Konsta 'sooda' Hölttä <sooda@iki.fi>
2018-12-14 13:53:05 +0200

    9b the elves are curious
    Now that the game lasts 100 times as long, shuffling of data in the
    vector insertion becomes infeasible. Keep the vector as a storage for
    linked list nodes that use immutable indices; new nodes are appended at
    the end and deleted slots are kept as fragmented garbage.
    
    For my input, valgrind says:
    
    total heap usage: 71 allocs, 71 frees, 408,962,793 bytes allocated
    
    400 MB is quite a bit but nothing terrible.

--- 18/9.rs
+++ 18/9.rs
@@ -10,20 +10,55 @@ fn parse_configuration(line: &str) -> (usize, usize) {
     (players, length)
 }
 
+#[derive(Debug)]
+struct Link {
+    value: usize,
+    prev: usize,
+    next: usize,
+}
+
+// Add an element at the back of the storage, never bother about deletions (indexes are immutable,
+// deleted slots just stay there as garbage)
+fn insert(ring: &mut Vec<Link>, left: usize, value: usize) -> usize {
+    let right = ring[left].next;
+    let pos = ring.len();
+
+    ring.push(Link { value: value, prev: left, next: right });
+
+    ring[left].next = pos;
+    ring[right].prev = pos;
+
+    pos
+}
+
+fn unlink(ring: &mut Vec<Link>, pos: usize) -> usize {
+    let val = ring[pos].value;
+
+    let prev = ring[pos].prev;
+    let next = ring[pos].next;
+    ring[prev].next = next;
+    ring[next].prev = prev;
+
+    val
+}
+
 fn play(players: usize, length: usize) -> usize {
-    let mut ring = vec![0];
+    let mut ring = vec![Link { value: 0, prev: 0, next: 0 }];
     let mut current = 0;
     let mut score = vec![0; players];
 
     for round in 1..=length {
         let player = round % players;
         if round % 23 != 0 {
-            current = (current + 1) % ring.len() + 1;
-            ring.insert(current, round);
+            let left_pos = ring[current].next;
+            current = insert(&mut ring, left_pos, round);
         } else {
             score[player] += round;
-            current = (current + ring.len() - 7) % ring.len();
-            score[player] += ring.remove(current);
+            for _ in 0..6 {
+                current = ring[current].prev;
+            }
+            let del = ring[current].prev;
+            score[player] += unlink(&mut ring, del);
         }
     }
 
@@ -34,8 +69,10 @@ fn main() {
     let games = BufReader::new(File::open(&std::env::args().nth(1).unwrap()).unwrap())
         .lines().map(|x| parse_configuration(&x.unwrap())).collect::<Vec<_>>();
 
-    for g in games {
+    for g in &games {
         let winning_score = play(g.0, g.1);
         println!("{:?} {}", g, winning_score);
     }
+    let winning_score_100 = play(games[0].0, 100 * games[0].1);
+    println!("{}", winning_score_100);
 }
