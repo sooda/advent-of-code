7f60bf2b23c4e50f13c689fd0bf61f5f735f24bd
Konsta 'sooda' Hölttä <sooda@iki.fi>
2019-01-18 22:03:50 +0200

    11: use a proper summed-area table
    Sum from P to D is D - B - C + A:
    
    +----+---+
    |....|...|
    |....|...|
    |...A|...B
    +----P---+
    |....|...|
    +---C+---D
    
    Details double-checked easily here:
    https://en.wikipedia.org/wiki/Summed-area_table
    
    Now this is some orders of magnitude faster and much less kludgy, too.
    Indexing vs signed values are annoying though.
    
    Also change to assert_eq because that prints the values if they don't
    match. It doesn't print the actual strings passed to the macro tho :(

--- 18/11.rs
+++ 18/11.rs
@@ -15,53 +15,80 @@ fn powerest_square(serial: i32, sz: i32) -> (i32, i32, i32) {
     })).max().unwrap()
 }
 
-#[allow(dead_code)]
-fn largest_total_square_naive(serial: i32) -> ((i32, i32, i32), i32) {
-    (0..=300).map(|sz| (powerest_square(serial, sz), sz)).max().unwrap()
-}
+fn cumulative_powers(serial: i32) -> Vec<i32> {
+    let mut map = vec![0; 300 * 300];
+    let at = |x: i32, y: i32| (y * 300 + x) as usize;
 
-fn largest_total_square_faster(serial: i32) -> ((i32, i32, i32), i32) {
-    let mut max = ((0, 0, 0), 0);
+    // prime the borders
+    map[at(0, 0)] = fuel_cell_power_level(1, 1, serial);
+    for i in 1..300 {
+        // remember, coords for fuel cells start from (1, 1)
+        map[at(i, 0)] = map[at(i - 1,     0)] + fuel_cell_power_level(1 + i, 1, serial);
+        map[at(0, i)] = map[at(0    , i - 1)] + fuel_cell_power_level(1, 1 + i, serial);
+    }
 
-    for y0 in 0..=300 {
-        let maxsz_y = 300 - y0;
-        for x0 in 0..=300 {
-            let maxsz_x = 300 - x0;
-            let mut sum = fuel_cell_power_level(x0, y0, serial);
-            max = max.max(((sum, x0, y0), 1));
-
-            let sz = maxsz_y.min(maxsz_x);
-            for off in 1..sz {
-                let bot_x = x0 + off;
-                let bot_y = y0 + off;
-                // bottom right corner is included in the column part
-                let mut bottom_row = (x0..bot_x)
-                    .map(|x| fuel_cell_power_level(x, bot_y, serial)).sum::<i32>();
-                let mut right_col = (y0..=bot_y)
-                    .map(|y| fuel_cell_power_level(bot_x, y, serial)).sum::<i32>();
-                sum += bottom_row + right_col;
-                max = max.max(((sum, x0, y0), 1 + off));
-            }
+    // rest of the table is recursive in 2d
+    for y in 1..300 {
+        for x in 1..300 {
+            let x0y0 = fuel_cell_power_level(1 + x, 1 + y, serial);
+            let x1y0 = map[at(x - 1, y    )];
+            let x0y1 = map[at(x    , y - 1)];
+            let x1y1 = map[at(x - 1, y - 1)];
+            map[at(x, y)] = x0y0 + x1y0 + x0y1 - x1y1;
         }
     }
 
-    max
+    map
+}
+
+fn subsquare(map: &[i32], x: i32, y: i32, sz: i32) -> i32 {
+    // bottom right corner is in, others are just out
+    // P = (x, y), D = (x + sz - 1, y + sz - 1)
+    // +----+---+
+    // |....|...|
+    // |....|...|
+    // |...A|...B
+    // +----P---+
+    // |....|...|
+    // +---C+---D
+    // (might be easier to just add zero padding to the array)
+    let get = |x, y| if x >= 0 && y >= 0 { map[(y * 300 + x) as usize] } else { 0 };
+    get(x + sz - 1, y + sz - 1) -
+    get(x + sz - 1, y - 1     ) -
+    get(x - 1     , y + sz - 1) +
+    get(x - 1     , y - 1     )
+}
+
+fn largest_total_square(serial: i32) -> (i32, i32, i32, i32) {
+    let power_map = cumulative_powers(serial);
+
+    let search_space = (0..300).flat_map(|y| {
+        (0..300).flat_map(move |x| {
+            (1..=((300 - y).min(300 - x))).map(move |sz| {
+                (x, y, sz)
+            })
+        })
+    });
+
+    search_space.map(|(x, y, sz)|
+        (subsquare(&power_map, x, y, sz), 1 + x, 1 + y, sz)
+    ).max().unwrap()
 }
 
 fn main() {
-    assert!(fuel_cell_power_level(3, 5, 8) == 4);
-    assert!(fuel_cell_power_level(122, 79, 57) == -5);
-    assert!(fuel_cell_power_level(217, 196, 39) == 0);
-    assert!(fuel_cell_power_level(101, 153, 71) == 4);
-    assert!(square_power(33, 45, 18, 3) == 29);
-    assert!(square_power(21, 61, 42, 3) == 30);
-    assert!(powerest_square(18, 3) == (29, 33, 45));
-    assert!(powerest_square(42, 3) == (30, 21, 61));
+    assert_eq!(fuel_cell_power_level(3, 5, 8), 4);
+    assert_eq!(fuel_cell_power_level(122, 79, 57), -5);
+    assert_eq!(fuel_cell_power_level(217, 196, 39), 0);
+    assert_eq!(fuel_cell_power_level(101, 153, 71), 4);
+    assert_eq!(square_power(33, 45, 18, 3), 29);
+    assert_eq!(square_power(21, 61, 42, 3), 30);
+    assert_eq!(powerest_square(18, 3), (29, 33, 45));
+    assert_eq!(powerest_square(42, 3), (30, 21, 61));
 
     let puzzle_input = 9110;
     println!("{:?}", powerest_square(puzzle_input, 3));
 
-    println!("{:?}", largest_total_square_faster(puzzle_input));
-    assert!(largest_total_square_faster(18) == ((113, 90, 269), 16));
-    assert!(largest_total_square_faster(42) == ((119, 232, 251), 12));
+    println!("{:?}", largest_total_square(puzzle_input));
+    assert_eq!(largest_total_square(18), (113, 90, 269, 16));
+    assert_eq!(largest_total_square(42), (119, 232, 251, 12));
 }
