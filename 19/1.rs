ea8123656b7ceb7d933c4d55257975ee4bcb1f23
Konsta 'sooda' Hölttä <sooda@iki.fi>
2019-12-01 13:27:02 +0200

    1b rocket equation double checker stops the launch
    Fuel is also needed to lift fuel, so implement the naive recursion
    that's obvious from the specifications. This results in an increase of
    ~49.9% in fuel consumption. No problem. Comes out of the fuckin' ground!
    You *cannot* lose.

--- 19/1.rs
+++ 19/1.rs
@@ -1,7 +1,21 @@
 use std::io::{self, BufRead};
 
 fn fuel_requirements(mass: u32) -> u32 {
-    mass / 3 - 2
+    if mass > 2 * 3 {
+        mass / 3 - 2
+    } else {
+        // now we instead wish really hard
+        0
+    }
+}
+
+fn total_fuel_requirements(mass: u32) -> u32 {
+    let fuel = fuel_requirements(mass);
+    if fuel > 0 {
+        fuel + total_fuel_requirements(fuel)
+    } else {
+        fuel
+    }
 }
 
 fn main() {
@@ -10,7 +24,13 @@ fn main() {
     assert_eq!(fuel_requirements(1969), 654);
     assert_eq!(fuel_requirements(100756), 33583);
 
-    let all_spacecrafts_up_high: u32 = io::stdin().lock().lines().map(|massline|
-        fuel_requirements(massline.unwrap().parse().unwrap())).sum();
-    println!("{}", all_spacecrafts_up_high);
+    assert_eq!(total_fuel_requirements(14), 2);
+    assert_eq!(total_fuel_requirements(1969), 966);
+    assert_eq!(total_fuel_requirements(100756), 50346);
+
+    let all_spacecrafts_up_high: (u32, u32) = io::stdin().lock().lines()
+        .map(|massline| massline.unwrap().parse().unwrap())
+        .map(|mass| (fuel_requirements(mass), total_fuel_requirements(mass)))
+        .fold((0, 0), |sum, i| (sum.0 + i.0, sum.1 + i.1));
+    println!("{:?}", all_spacecrafts_up_high);
 }
