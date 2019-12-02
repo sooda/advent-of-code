a550f02888d865d6a2a5f1d313e78d20da9a9c9e
Konsta 'sooda' Hölttä <sooda@iki.fi>
2019-12-02 23:22:19 +0200

    2b determine correct pair of inputs for 19690720
    The program execution is already rather clean, so generalize the program
    inputs at memory 1 and 2. Just brute force search the right ones because
    the search space is a silly 100 by 100 grid.

--- 19/2.rs
+++ 19/2.rs
@@ -32,6 +32,23 @@ fn execute(program: &[u32]) -> u32 {
     program[0]
 }
 
+fn exec_input(program: &[u32], noun: u32, verb: u32) -> u32 {
+    let mut program = program.to_vec();
+
+    program[1] = noun;
+    program[2] = verb;
+
+    execute(&mut program)
+}
+
+fn bruteforce(program: &[u32], output: u32) -> u32 {
+    // the puzzle answer is 100 * noun + verb which is conveniently the position in this flattened
+    // list of 2d search parameters: for each noun we have 100 verbs
+    (0..100).flat_map(|noun| (0..100).map(move |verb| (noun, verb)))
+        .position(|(noun, verb)| exec_input(&program, noun, verb) == output)
+        .unwrap() as u32
+}
+
 fn main() {
     assert_eq!(execute(&[1,9,10,3,2,3,11,0,99,30,40,50]), 3500);
     assert_eq!(execute(&[1,0,0,0,99]), 2);
@@ -39,11 +56,9 @@ fn main() {
     assert_eq!(execute(&[2,4,4,5,99,0]), 2);
     assert_eq!(execute(&[1,1,1,4,99,5,6,0,99]), 30);
 
-    let mut program: Vec<u32> = io::stdin().lock().lines().next().unwrap().unwrap()
+    let program: Vec<u32> = io::stdin().lock().lines().next().unwrap().unwrap()
         .split(',').map(|n| n.parse().unwrap()).collect();
 
-    program[1] = 12;
-    program[2] = 2;
-
-    println!("{}", execute(&program));
+    println!("{}", exec_input(&program, 12, 2));
+    println!("{}", bruteforce(&program, 19690720));
 }
