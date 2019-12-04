734dd27b18c720679d7164f9f05755dd558f99b9
Konsta 'sooda' Hölttä <sooda@iki.fi>
2019-12-04 23:32:43 +0200

    4b just exactly two matching digits
    The two digits must not be part of a longer series of digits, so keep
    track of how long series of consecutive identical digits are found.
    
    Note that there can be a bad long series in the password and then also a
    two-digit pair that meets the additional criterion.

--- 19/4.rs
+++ 19/4.rs
@@ -32,6 +32,29 @@ fn meets_criteria(pw: u32) -> bool {
     adjacent_digits(pw) && never_decrease(pw)
 }
 
+fn adjacent_digits_pair(pw: u32) -> bool {
+    let mut len = 1;
+    for i in 1..DIGITS {
+        if digit(pw, i) == digit(pw, i - 1) {
+            // same as before, keep counting
+            len += 1;
+        } else {
+            if len == 2 {
+                // changed to something else, so this is a pair
+                return true;
+            }
+            len = 1;
+        }
+    }
+
+    // can't change anymore if at the end, so check separately
+    len == 2
+}
+
+fn meets_criteria_b(pw: u32) -> bool {
+    adjacent_digits_pair(pw) && never_decrease(pw)
+}
+
 fn main() {
     assert!(digit(135679, 0) == 1);
     assert!(digit(135679, 1) == 3);
@@ -50,5 +73,7 @@ fn main() {
 
     // the six-digit key fact is trivial because the puzzle input is contained
     let n = (range[0]..=range[1]).filter(|&pw| meets_criteria(pw)).count();
+    println!("{}", n);
+    let n = (range[0]..=range[1]).filter(|&pw| meets_criteria_b(pw)).count();
     println!("{}", n);
 }
