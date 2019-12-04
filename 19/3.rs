f24612b2fb834c2314d4c59ea0803603c36e663e
Konsta 'sooda' Hölttä <sooda@iki.fi>
2019-12-04 23:13:41 +0200

    3b minimize actually the signal delay
    Instead of looking at the grid positions only, compute the number of
    steps when moving along the wires. Store those in a hashmap.
    
    Not sure what makes this last over two seconds (on my machine). It's
    unfortunate that the hashmap keys are probably rehashed (~150k entries
    in both wire trails) when converting them to hashsets, but not sure if
    that's the bottleneck. The walk is pretty naive too.
    
    I might do some actual perf analysis on these things this year to study
    something new in Rust.

--- 19/3.rs
+++ 19/3.rs
@@ -1,5 +1,5 @@
 use std::io::{self, BufRead};
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 use std::str::FromStr;
 use std::num::ParseIntError;
 
@@ -19,10 +19,11 @@ impl FromStr for Step {
     }
 }
 
-fn walk(path: &[Step]) -> HashSet<(i32, i32)> {
-    let mut map = HashSet::new();
+fn walk(path: &[Step]) -> HashMap<(i32, i32), usize> {
+    let mut map = HashMap::new();
     let mut x = 0;
     let mut y = 0;
+    let mut steps = 0;
 
     // note: the origin is not in the map
     for s in path {
@@ -36,7 +37,8 @@ fn walk(path: &[Step]) -> HashSet<(i32, i32)> {
             };
             x = nx;
             y = ny;
-            map.insert((x, y));
+            steps += 1;
+            map.entry((x, y)).and_modify(|e| *e = *e).or_insert(steps);
         }
     }
 
@@ -44,12 +46,23 @@ fn walk(path: &[Step]) -> HashSet<(i32, i32)> {
 }
 
 fn crossing_distance(a: &[Step], b: &[Step]) -> i32 {
-    let map_a = walk(a);
-    let map_b = walk(b);
-    let wire_crossings = map_a.intersection(&map_b);
+    let trail_a = walk(a).keys().cloned().collect::<HashSet<_>>();
+    let trail_b = walk(b).keys().cloned().collect::<HashSet<_>>();
+    let wire_crossings = trail_a.intersection(&trail_b);
     let closest = wire_crossings.min_by_key(|(a, b)| a.abs() + b.abs()).unwrap();
 
     closest.0.abs() + closest.1.abs()
+}
+
+fn crossing_distance_steps(a: &[Step], b: &[Step]) -> i32 {
+    let map_a = walk(a);
+    let map_b = walk(b);
+    let trail_a = map_a.keys().cloned().collect::<HashSet<_>>();
+    let trail_b = map_b.keys().cloned().collect::<HashSet<_>>();
+    let wire_crossings = trail_a.intersection(&trail_b);
+    let closest = wire_crossings.min_by_key(|&pos| { map_a[pos] + map_b[pos]}).unwrap();
+
+    (map_a[closest] + map_b[closest]) as i32
 }
 
 fn parseline(stepline: &str) -> Vec<Step> {
@@ -72,8 +85,21 @@ fn main() {
             &parseline("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")),
             135);
 
+    assert_eq!(crossing_distance_steps(
+            &parseline("R8,U5,L5,D3"),
+            &parseline("U7,R6,D4,L4")),
+            30);
+    assert_eq!(crossing_distance_steps(
+            &parseline("R75,D30,R83,U83,L12,D49,R71,U7,L72"),
+            &parseline("U62,R66,U55,R34,D71,R55,D58,R83")),
+            610);
+    assert_eq!(crossing_distance_steps(
+            &parseline("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"),
+            &parseline("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")),
+            410);
     let wire_paths: Vec<Vec<Step>> = io::stdin().lock().lines()
         .map(|stepline| parseline(&stepline.unwrap()))
         .collect();
     println!("{}", crossing_distance(&wire_paths[0], &wire_paths[1]));
+    println!("{}", crossing_distance_steps(&wire_paths[0], &wire_paths[1]));
 }
