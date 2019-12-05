d6478476dc7d20c54afaebf3139b48b47f4de4cf
Konsta 'sooda' Hölttä <sooda@iki.fi>
2019-12-05 20:54:46 +0200

    5b extend the thermal radiators
    Now some control flow: jump if/not 0, compare less/equal. Spent way too
    much time struggling with less copypastay immediate/indirect addressing
    modes and lifetimes, but this turned out okay.
    
    Fix output parsing bug: don't store if nothing comes out. It didn't
    affect the first part.

--- 19/5.rs
+++ 19/5.rs
@@ -1,46 +1,70 @@
 use std::io::{self, BufRead};
 
-fn aluop(program: &mut [i32], ip: usize, imm: (bool, bool, bool), op: &dyn Fn(i32, i32) -> i32) {
-    let p1 = program[ip + 1];
-    let p2 = program[ip + 2];
-    let p3 = program[ip + 3];
-    let a = if imm.0 { p1 } else { program[p1 as usize] };
-    let b = if imm.1 { p2 } else { program[p2 as usize] };
-    assert!(!imm.2);
-    let dest = &mut program[p3 as usize];
-    *dest = op(a, b);
-}
-
-fn step(program: &mut [i32], ip: usize, input: i32) -> Option<(usize, Option<i32>)> {
+fn step<'a>(program: &'a mut [i32], ip: usize, input: i32) -> Option<(usize, Option<i32>)> {
     let opcode = program[ip] % 100;
-    let mode1 = program[ip] / 100 % 10;
-    let mode2 = program[ip] / 1000 % 10;
-    let mode3 = program[ip] / 10000 % 10;
+    if opcode == 99 {
+        // short circuit this, the discontinuity is annoying
+        return None;
+    }
+
+    let mode0 = program[ip] / 100 % 10;
+    let mode1 = program[ip] / 1000 % 10;
+    let mode2 = program[ip] / 10000 % 10;
+    assert!(mode0 <= 1);
     assert!(mode1 <= 1);
     assert!(mode2 <= 1);
-    assert!(mode3 <= 1);
-    let immflags = (mode1 == 1, mode2 == 1, mode3 == 1);
+    let immflags = (mode0 == 1, mode1 == 1, mode2 == 1);
+
+    // indirection via closures instead of direct variables because indexing might go off with
+    // arbitrary values: only evaluate when it's known that indexing is okay. Need this to avoid
+    // repetition in the opcode decoding
+
+    let imm0 = || program[ip + 1];
+    let imm1 = || program[ip + 2];
+    let val0 = || if immflags.0 { imm0() } else { program[imm0() as usize ] };
+    let val1 = || if immflags.1 { imm1() } else { program[imm1() as usize ] };
+    // program as input for lifetime; using imm0 here would cause more lifetime trouble
+    let mut0 = |program: &'a mut [i32]| { assert!(!immflags.0); &mut program[program[ip + 1] as usize] };
+    let mut2 = |program: &'a mut [i32]| { assert!(!immflags.2); &mut program[program[ip + 3] as usize] };
 
     match opcode {
         1 => {
-            aluop(program, ip, immflags, &|a, b| a + b);
+            *mut2(program) = val0() + val1();
             Some((ip + 4, None))
         },
         2 => {
-            aluop(program, ip, immflags, &|a, b| a * b);
+            *mut2(program) = val0() * val1();
             Some((ip + 4, None))
         },
         3 => {
-            let dest = program[ip + 1];
-            program[dest as usize] = input;
+            *mut0(program) = input;
             Some((ip + 2, None))
         }
         4 => {
-            let src = program[ip + 1];
-            let out = if immflags.0 { src } else { program[src as usize] };
-            Some((ip + 2, Some(out)))
+            Some((ip + 2, Some(val0())))
         },
-        99 => None,
+        5 => {
+            if val0() != 0 {
+                Some((val1() as usize, None))
+            } else {
+                Some((ip + 3, None))
+            }
+        },
+        6 => {
+            if val0() == 0 {
+                Some((val1() as usize, None))
+            } else {
+                Some((ip + 3, None))
+            }
+        },
+        7 => {
+            *mut2(program) = if val0() < val1() { 1 } else { 0 };
+            Some((ip + 4, None))
+        },
+        8 => {
+            *mut2(program) = if val0() == val1() { 1 } else { 0 };
+            Some((ip + 4, None))
+        },
         _ => panic!("something went wrong at {}: {}", ip, program[ip])
     }
 }
@@ -50,15 +74,17 @@ fn execute(program: &[i32], input: i32) -> Option<i32> {
     let mut ip = 0;
     let mut output = None;
 
+    //println!(". {:?} {} {:?}", &program[..ip], program[ip], &program[ip+1..]);
     while let Some((newip, newout)) = step(&mut program, ip, input) {
         // all outputs must be zero except the final diagnostic code,
         // or the diagnostic program wasn't run successfully
         if newout.is_some() {
             assert_eq!(output.unwrap_or_else(|| 0), 0);
+            output = newout;
         }
 
         ip = newip;
-        output = newout;
+        //println!("  {:?} {} {:?}", &program[..ip], program[ip], &program[ip+1..]);
     }
 
     output
@@ -68,8 +94,43 @@ fn main() {
     assert_eq!(execute(&[1002,4,3,4,33], 1), None);
     assert_eq!(execute(&[1101,100,-1,4,0], 1), None);
 
+    assert_eq!(execute(&[3,9,8,9,10,9,4,9,99,-1,8], 7), Some(0));
+    assert_eq!(execute(&[3,9,8,9,10,9,4,9,99,-1,8], 8), Some(1));
+    assert_eq!(execute(&[3,9,8,9,10,9,4,9,99,-1,8], 9), Some(0));
+
+    assert_eq!(execute(&[3,9,7,9,10,9,4,9,99,-1,8], 7), Some(1));
+    assert_eq!(execute(&[3,9,7,9,10,9,4,9,99,-1,8], 8), Some(0));
+    assert_eq!(execute(&[3,9,7,9,10,9,4,9,99,-1,8], 9), Some(0));
+
+    assert_eq!(execute(&[3,3,1108,-1,8,3,4,3,99], 7), Some(0));
+    assert_eq!(execute(&[3,3,1108,-1,8,3,4,3,99], 8), Some(1));
+    assert_eq!(execute(&[3,3,1108,-1,8,3,4,3,99], 9), Some(0));
+
+    assert_eq!(execute(&[3,3,1107,-1,8,3,4,3,99], 7), Some(1));
+    assert_eq!(execute(&[3,3,1107,-1,8,3,4,3,99], 8), Some(0));
+    assert_eq!(execute(&[3,3,1107,-1,8,3,4,3,99], 9), Some(0));
+
+    assert_eq!(execute(&[3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9], 0), Some(0));
+    assert_eq!(execute(&[3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9], 42), Some(1));
+
+    assert_eq!(execute(&[3,3,1105,-1,9,1101,0,0,12,4,12,99,1], 0), Some(0));
+    assert_eq!(execute(&[3,3,1105,-1,9,1101,0,0,12,4,12,99,1], 42), Some(1));
+
+    assert_eq!(execute(&[3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+            1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+            999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99], 8), Some(1000));
+
+    assert_eq!(execute(&[3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+            1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+            999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99], 7), Some(999));
+
+    assert_eq!(execute(&[3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+            1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+            999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99], 9), Some(1001));
+
     let program: Vec<i32> = io::stdin().lock().lines().next().unwrap().unwrap()
         .split(',').map(|n| n.parse().unwrap()).collect();
 
     println!("{:?}", execute(&program, 1));
+    println!("{:?}", execute(&program, 5));
 }
