ffd1fba5fd2269d3e71a480c16e23e3eaa2433f1
Konsta 'sooda' Hölttä <sooda@iki.fi>
2019-12-07 12:50:26 +0200

    7b rewire it into a feedback loop
    The amplifier oscillates now with a different input (with phase settings
    5..9): the output of the last amp is connected to the first one's input.
    The values grow quite a bit, so use i64.
    
    Now that the programs run longer and they need to live "concurrently"
    before stopping, the "computers" need to be executed only a bit at a
    time each; this is implemented by running them until the next output
    occurs (to be injected into the next one in the loop).
    
    Both the initialization vector from the phase settings and the first
    input from the previous amp (or 0 for the very first run) are read
    before the first output, so the input still needs to be an iterator
    (unless the execution would break for each input; then they could be
    "primed" with just the phases).

--- 19/7.rs
+++ 19/7.rs
@@ -1,6 +1,6 @@
 use std::io::{self, BufRead};
 
-fn step<'a, 'b, I: Iterator<Item = &'b i32>>(program: &'a mut [i32], ip: usize, input: &mut I) -> Option<(usize, Option<i32>)> {
+fn step<'a, 'b, I: Iterator<Item = &'b i64>>(program: &'a mut [i64], ip: usize, input: &mut I) -> Option<(usize, Option<i64>)> {
     let opcode = program[ip] % 100;
     if opcode == 99 {
         // short circuit this, the discontinuity is annoying
@@ -24,8 +24,8 @@ fn step<'a, 'b, I: Iterator<Item = &'b i32>>(program: &'a mut [i32], ip: usize, 
     let val0 = || if immflags.0 { imm0() } else { program[imm0() as usize ] };
     let val1 = || if immflags.1 { imm1() } else { program[imm1() as usize ] };
     // program as input for lifetime; using imm0 here would cause more lifetime trouble
-    let mut0 = |program: &'a mut [i32]| { assert!(!immflags.0); &mut program[program[ip + 1] as usize] };
-    let mut2 = |program: &'a mut [i32]| { assert!(!immflags.2); &mut program[program[ip + 3] as usize] };
+    let mut0 = |program: &'a mut [i64]| { assert!(!immflags.0); &mut program[program[ip + 1] as usize] };
+    let mut2 = |program: &'a mut [i64]| { assert!(!immflags.2); &mut program[program[ip + 3] as usize] };
 
     match opcode {
         1 => {
@@ -69,23 +69,21 @@ fn step<'a, 'b, I: Iterator<Item = &'b i32>>(program: &'a mut [i32], ip: usize, 
     }
 }
 
-fn execute(program: &[i32], inputs: &[i32]) -> Option<i32> {
+fn execute(program: &[i64], inputs: &[i64]) -> Option<i64> {
     let mut program = program.to_vec();
     let mut ip = 0;
     let mut output = None;
     let mut input = inputs.iter();
 
-    //println!(". {:?} {} {:?}", &program[..ip], program[ip], &program[ip+1..]);
     while let Some((newip, newout)) = step(&mut program, ip, &mut input) {
         output = newout;
         ip = newip;
-        //println!("  {:?} {} {:?}", &program[..ip], program[ip], &program[ip+1..]);
     }
 
     output
 }
 
-fn swap_codes(a: i32, i: usize, j: usize) -> i32 {
+fn swap_codes(a: i64, i: usize, j: usize) -> i64 {
     let ai = (a >> (4 * i)) & 0xf;
     let aj = (a >> (4 * j)) & 0xf;
     let mi = !(0xf << (4 * i));
@@ -95,7 +93,7 @@ fn swap_codes(a: i32, i: usize, j: usize) -> i32 {
     b
 }
 
-fn permutations(k: usize, a: &mut i32, out: &mut Vec<i32>) {
+fn permutations(k: usize, a: &mut i64, out: &mut Vec<i64>) {
     if k == 1 {
         assert!(!out.contains(a));
         out.push(*a);
@@ -112,7 +110,7 @@ fn permutations(k: usize, a: &mut i32, out: &mut Vec<i32>) {
     }
 }
 
-fn thruster_signal(program: &[i32], mut phasecode: i32) -> i32 {
+fn thruster_signal(program: &[i64], mut phasecode: i64) -> i64 {
     let mut prev = 0;
     let mut x = Vec::new();
     for _amp in 0..5 { // A..E
@@ -125,12 +123,77 @@ fn thruster_signal(program: &[i32], mut phasecode: i32) -> i32 {
     prev
 }
 
-fn max_thruster_signal(program: &[i32]) -> i32 {
+fn max_thruster_signal(program: &[i64]) -> i64 {
     let mut pers = Vec::new();
-    // 5 digits, each is 0..4, encoded in i32
+    // 5 digits, each is 0..4, encoded in i64
     permutations(5, &mut 0x43210, &mut pers);
     assert_eq!(pers.len(), 120);
     pers.iter().map(|&phasecode| (phasecode, thruster_signal(program, phasecode)))
+        .max_by_key(|&(_, sig)| sig).unwrap().1
+}
+
+struct Computer<'a> {
+    program: &'a mut [i64],
+    ip: usize
+}
+
+fn drive_output(machine: &mut Computer, inputs: &[i64]) -> Option<i64> {
+    let program = &mut machine.program;
+    let mut ip = machine.ip;
+    let mut input = inputs.iter();
+
+    while let Some((newip, out)) = step(program, ip, &mut input) {
+        ip = newip;
+        if out.is_some() {
+            machine.ip = ip;
+            return out;
+        }
+    }
+
+    // stopped without input
+    None
+}
+
+fn thruster_signal_loop(program: &[i64], mut phasecode: i64) -> i64 {
+    let amps = &mut [
+        Computer { program: &mut program.to_vec(), ip: 0 },
+        Computer { program: &mut program.to_vec(), ip: 0 },
+        Computer { program: &mut program.to_vec(), ip: 0 },
+        Computer { program: &mut program.to_vec(), ip: 0 },
+        Computer { program: &mut program.to_vec(), ip: 0 },
+    ];
+    let mut prev = 0;
+
+    // prime the phases in
+    for mut amp in amps.iter_mut() {
+        let phase = phasecode & 0xf;
+        phasecode >>= 4;
+        let next = drive_output(&mut amp, &[phase, prev]);
+        prev = next.unwrap();
+    }
+
+    // now the programs only input one thing and output another thing
+    'outer: loop {
+        for (i, mut amp) in amps.iter_mut().enumerate() {
+            match drive_output(&mut amp, &[prev]) {
+                Some(next) => prev = next,
+                None => {
+                    // must stop at the end of a full iteration
+                    assert!(i == 0);
+                    break 'outer;
+                }
+            }
+        }
+    }
+    prev
+}
+
+fn max_thruster_signal_loop(program: &[i64]) -> i64 {
+    let mut pers = Vec::new();
+    // 5 digits, each is 0..4, encoded in i64
+    permutations(5, &mut 0x98765, &mut pers);
+    assert_eq!(pers.len(), 120);
+    pers.iter().map(|&phasecode| (phasecode, thruster_signal_loop(program, phasecode)))
         .max_by_key(|&(_, sig)| sig).unwrap().1
 }
 
@@ -144,8 +207,21 @@ fn main() {
     assert_eq!(max_thruster_signal(&[3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,
             1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0]), 65210);
 
-    let program: Vec<i32> = io::stdin().lock().lines().next().unwrap().unwrap()
+    assert_eq!(thruster_signal_loop(&[
+            3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,
+            27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5], 0x56789), 139629729);
+    assert_eq!(max_thruster_signal_loop(&[
+            3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,
+            27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]), 139629729);
+
+    assert_eq!(max_thruster_signal_loop(&[
+            3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,
+            -5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,
+            53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10]), 18216);
+
+    let program: Vec<i64> = io::stdin().lock().lines().next().unwrap().unwrap()
         .split(',').map(|n| n.parse().unwrap()).collect();
 
     println!("{}", max_thruster_signal(&program));
+    println!("{}", max_thruster_signal_loop(&program));
 }
