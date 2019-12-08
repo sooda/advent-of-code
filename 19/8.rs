2178995981440f1859a205bfb70af43945c6a059
Konsta 'sooda' Hölttä <sooda@iki.fi>
2019-12-08 08:24:32 +0200

    8b ready to decode the image
    I guessed that the layers would be bit planes but they're just
    transparent films that go on top of each other. The first one with
    either black or white is displayed at each pixel. Loop over the layers
    and build an output image for the still transparent pixels.

--- 19/8.rs
+++ 19/8.rs
@@ -11,9 +11,31 @@ fn num_digits(layer: &[u8]) -> (usize, usize, usize) {
     })
 }
 
+fn decode(digits: &[u8]) -> [u8; 25 * 6] {
+    let mut image = [b'2'; 25 * 6];
+    for layer in digits.chunks(25 * 6) {
+        for (&input, output) in layer.iter().zip(image.iter_mut()) {
+            if *output == b'2' {
+                *output = input;
+            }
+        }
+    }
+    image
+}
+
+fn print_layer(layer: &[u8]) {
+    for row in layer.chunks(25) {
+        for &ch in row {
+            print!("{}", if ch == b'1' { '#' } else { ' ' });
+        }
+        println!();
+    }
+}
+
 fn main() {
     let digits: Vec<u8> = io::stdin().lock().lines().next().unwrap().unwrap()
         .bytes().collect();
     let fewest_0 = digits.chunks(25 * 6).map(num_digits).min_by_key(|&d| d.0).unwrap();
     println!("{}", fewest_0.1 * fewest_0.2);
+    print_layer(&decode(&digits));
 }
