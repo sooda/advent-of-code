d3b784dd4f8c5b5288860d9a77fb7d4d0c9c6b05
Konsta 'sooda' Hölttä <sooda@iki.fi>
2019-12-10 19:43:37 +0200

    10b vaporize with a giant laser
    The only solution is awesome. Is it *complete* vaporization though if
    the asteroid on which the monitoring station is set up is not vaporized?
    The spec doesn't say anything about that.
    
    The pulverization occurs clockwise with a spinning gun so that shadowed
    objects are destroyed on the next round. In an infinitely large space
    with asteroids very close to each other when thinking solid angles, we
    might want to use fractional numbers (tan alpha = dy / dx). In this
    silly exercise just atan2 will do.
    
    On each round the zapped objects are tracked and the objects shadowed
    them are hidden so that removal is left for the next round.

--- 19/10.rs
+++ 19/10.rs
@@ -1,4 +1,7 @@
+#![feature(vec_remove_item)]
+
 use std::io::{self, BufRead};
+use std::cmp::Ordering;
 
 fn blocked_i(src: &(i32, i32), dxd: i32, dyd: i32, m: &(i32, i32)) -> bool {
     // dist to "middle" candidate
@@ -32,18 +35,110 @@ fn visibility(src: &(i32, i32), space: &Vec<(i32, i32)>) -> usize {
     space.iter().filter(|&x| x != src).filter(|x| !blocked(src, x, space)).count()
 }
 
+fn delta(a: &(i32, i32), b: &(i32, i32)) -> (i32, i32) {
+    // XXX y goes up for math thinking
+    (a.0 - b.0, -(a.1 - b.1))
+}
+
+// "atan" (not 2) but for integer stuff, left here as a historical document on what might be done
+// in a much larger universe where floating points would not have enough precision
+fn _earlier_angle(a: &(i32, i32), b: &(i32, i32)) -> Ordering {
+    // both vertically up? return the closer one
+    if a.0 == 0 && b.0 == 0 {
+        a.1.cmp(&b.1)
+    } else if a.0 == 0 {
+        Ordering::Less
+    } else if b.0 == 0 {
+        Ordering::Greater
+    } else {
+        // NOTE: larger numbers are smaller in angle
+        let x = (b.1 * a.0).cmp(&(a.1 * b.0));
+        if x != Ordering::Equal {
+            x
+        } else {
+            // closer to origin is better (these coords are positive)
+            (a.0 + a.1).cmp(&(b.0 + b.1))
+        }
+    }
+}
+
+// Probably incomplete. This has only been tested but not proved correct.
+fn atan3(a: &(i32, i32)) -> f64 {
+    use std::f64::consts::PI;
+    let q = (a.1 as f64).atan2(a.0 as f64);
+    let w = if q >= 0.0 {
+        q
+    } else {
+        q + 2.0 * PI
+    };
+    // rotate so up becomes 0, keep to 0..2pi
+    let e = (w - 0.5 * PI + 2.0 * PI) % (2.0 * PI);
+    // inverse so we go clockwise, keep in 0..2pi (for 0 e)
+    (2.0 * PI - e) % (2.0 * PI)
+}
+
+fn anglecmp(a: &(i32, i32), b: &(i32, i32)) -> Ordering {
+    let aa = atan3(a);
+    let ab = atan3(b);
+    // if same angle, order by distance
+    if blocked_i(&(0, 0), b.0, b.1, a) {
+        Ordering::Less
+    } else if blocked_i(&(0, 0), a.0, a.1, b) {
+        Ordering::Greater
+    } else if aa < ab {
+        Ordering::Less
+    } else if aa > ab {
+        Ordering::Greater
+    } else {
+        panic!("{:?} cmp {:?} ??", a, b);
+    }
+}
+
+fn pew_round(origin: &(i32, i32), roids: &mut Vec<(i32, i32)>) -> Vec<(i32, i32)> {
+    let mut zapped = Vec::new();
+
+    while !roids.is_empty() {
+        roids.sort_by(|a, b| anglecmp(&delta(&a, &origin), &delta(&b, &origin)));
+        let rm = roids[0];
+        roids.remove(0);
+        zapped.push(rm);
+
+        // would be shadowed by rm? leave for the next round, but don't zap
+        while !roids.is_empty() && blocked_i(&origin, roids[0].0 - origin.0, roids[0].1 - origin.1, &rm) {
+            roids.remove(0);
+        }
+    }
+
+    zapped
+}
+
+fn pewpew(origin: &(i32, i32), roids: &mut Vec<(i32, i32)>, limit: usize) -> (i32, i32) {
+    roids.remove_item(origin);
+    let mut gone = 0;
+    loop {
+        let zapped = pew_round(origin, roids);
+        if gone + zapped.len() >= limit {
+            return zapped[limit - 1 - gone]; // limit is 1-indexed
+        }
+        gone += zapped.len();
+        for z in zapped {
+            roids.remove_item(&z);
+        }
+    }
+}
+
 fn main() {
     // can't flat_map directly because those line strings are temporary, so two collect()s :(
-    let roids: Vec<_> = io::stdin().lock().lines().enumerate().map(
+    let mut roids: Vec<_> = io::stdin().lock().lines().enumerate().map(
         |(y, line)| line.unwrap().bytes().enumerate().filter_map(
-            |(x, ch)| if ch == b'#' { Some((x as i32, y as i32)) } else { None }
+            |(x, ch)| if ch != b'.' { Some((x as i32, y as i32)) } else { None }
         ).collect::<Vec<_>>()
     )
     .flat_map(|line| line.into_iter()).collect();
 
-    println!("{:?}", roids);
-    println!("n={}", roids.len());
-
-    let (bestval, bestpos) = roids.iter().map(|roid| (visibility(roid, &roids), roid)).max().unwrap();
+    let (bestval, bestpos) = roids.iter().map(|roid| (visibility(roid, &roids), *roid)).max().unwrap();
     println!("best {} at {:?}", bestval, bestpos);
+
+    let correct_stardust = pewpew(&bestpos, &mut roids, 200);
+    println!("{:?} {}", correct_stardust, 100 * correct_stardust.0 + correct_stardust.1);
 }
