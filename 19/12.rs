28ccf746a76e9271268a985b6d98ce7610dadb87
Konsta 'sooda' Hölttä <sooda@iki.fi>
2019-12-12 19:45:18 +0200

    12b history repeats itself
    The history of the moons appears to repeat itself, just like the AoC.
    The simulation will repeat from the beginning at an absurdly late point
    in time. Like before, the trick is to notice that each position and
    velocity in each axis repeats itself much earlier than the total
    sequence. The whole sequence then repeats itself at the least common
    multiple of the periods of all repeating series.
    
    A period of a single value is brute-force searched by running the
    simulation a reasonably long time and looking for overlapping sequences.
    Very small sequences would be false positives, but such anomalies don't
    seem to occur.

--- 19/12.rs
+++ 19/12.rs
@@ -1,9 +1,11 @@
 use std::io::{self, BufRead};
+use std::collections::HashSet;
 
 extern crate regex;
 use regex::Regex;
 
-type Vek = (i32, i32, i32);
+// The coords stay rather small. Keep the datatype small for less memory traffic with the history
+type Vek = (i16, i16, i16);
 
 fn gravity(moons: &[Vek], velocities: &[Vek]) -> Vec<Vek> {
     let mut out = Vec::new();
@@ -42,16 +44,16 @@ fn velocity(moons: &[Vek], velocities: &[Vek]) -> Vec<Vek> {
     out
 }
 
-fn kinetic_energy(moon: &Vek, vel: &Vek) -> i32 {
+fn kinetic_energy(moon: &Vek, vel: &Vek) -> i16 {
     (moon.0.abs() + moon.1.abs() + moon.2.abs()) *
         (vel.0.abs() + vel.1.abs() + vel.2.abs())
 }
 
-fn total_energy(moons: &[Vek], velocities: &[Vek]) -> i32 {
+fn total_energy(moons: &[Vek], velocities: &[Vek]) -> i16 {
     moons.iter().zip(velocities).map(|(m, v)| kinetic_energy(m, v)).sum()
 }
 
-fn energy_after(mut moons: Vec<Vek>, n: usize) -> i32 {
+fn energy_after(mut moons: Vec<Vek>, n: usize) -> i16 {
     let mut vels = vec![(0, 0, 0); moons.len()];
 
     for _step in 0..n {
@@ -66,10 +68,102 @@ fn energy_after(mut moons: Vec<Vek>, n: usize) -> i32 {
         vels = gravity(&moons, &vels);
         moons = velocity(&moons, &vels);
     }
-    println!("m {:?}", moons);
-    println!("v {:?}", vels);
 
     total_energy(&moons, &vels)
+}
+
+fn fully_overlapping<F: Fn(&(Vek, Vek)) -> i16>(hist: &[Vec<(Vek, Vek)>],
+        a: usize, b: usize, len: usize, moon_ix: usize, moon_prop: F) -> bool {
+    (&hist[a..(a + len)]).iter().zip(&hist[b..(b + len)])
+        .all(|(amoons, bmoons)| moon_prop(&amoons[moon_ix]) == moon_prop(&bmoons[moon_ix]))
+}
+
+fn gcd(mut a: usize, mut b: usize) -> usize {
+    while a != 0 {
+        let c = b % a;
+        b = a;
+        a = c;
+    }
+    b
+}
+
+fn lcm(a: usize, b: usize) -> usize {
+    a * b / gcd(a, b)
+}
+
+fn find_common_cycle(hist: &[Vec<(Vek, Vek)>]) -> usize {
+    // this much overlap can be detected
+    let cyc_len_max = hist.len() / 2;
+    let moon_count = hist[0].len();
+
+    // (SoA might be faster than AoS due to gaps in memory but a few seconds is good enough)
+    let moon_props: &[Box<dyn Fn(&(Vek, Vek)) -> i16>] = &[
+        Box::new(|&(a, _)| a.0 ),
+        Box::new(|&(a, _)| a.1 ),
+        Box::new(|&(a, _)| a.2 ),
+        Box::new(|&(_, b)| b.0 ),
+        Box::new(|&(_, b)| b.1 ),
+        Box::new(|&(_, b)| b.2 ),
+    ];
+    // the cycle may appear more than once in the log
+    let mut scalar_cycles = vec![Vec::new(); moon_count * moon_props.len()];
+    // certainly no shorter cycles than 1000 or so but whatever
+    for cyc_len in 2..=cyc_len_max {
+        let mut cycling_props = 0;
+        // find the cycle of each scalar separately
+        let mut found_iter = scalar_cycles.iter_mut();
+        for moon_ix in 0..moon_count {
+            for moon_prop in moon_props.iter() {
+                let start_a = hist.len() - 2 * cyc_len;
+                let start_b = hist.len() - cyc_len;
+                let foundvec = found_iter.next().unwrap();
+                if fully_overlapping(hist, start_a, start_b, cyc_len, moon_ix, moon_prop) {
+                    cycling_props += 1;
+                    foundvec.push(cyc_len);
+                }
+            }
+        }
+        if cycling_props == 4 * 6 {
+            println!("oops, found all already");
+            return cyc_len;
+        }
+    }
+
+    // sanity check
+    for ith_cycles in &scalar_cycles {
+        if false {
+            println!("cycles found: {:?}", ith_cycles);
+        }
+        let smallest = ith_cycles[0];
+        for cyc in &ith_cycles[1..] {
+            if cyc % smallest != 0 {
+                panic!("consider increasing the smallest allowed cycle, these don't add up");
+            }
+        }
+    }
+
+    // remove dupes
+    let unique_cycles = scalar_cycles.into_iter().map(|v| v[0]).collect::<HashSet<_>>();
+
+    println!("lcm {:?}", unique_cycles);
+    unique_cycles.into_iter().fold(1, lcm)
+}
+
+fn world_period(mut moons: Vec<Vek>) -> usize {
+    let mut vels = vec![(0, 0, 0); moons.len()];
+    let mut hist = Vec::new();
+    // from [[moonpos], [moonvel]] into [(moonpos, moonvel)]
+    let history_transform = |moons: &Vec<Vek>, vels: &Vec<Vek>| moons.iter().zip(vels)
+        .map(|(&x, &y)| (x, y)).collect::<Vec<(Vek, Vek)>>();
+
+    // this is surely long enough for each cycle
+    for _step in 0..500000 {
+        hist.push(history_transform(&moons, &vels));
+        vels = gravity(&moons, &vels);
+        moons = velocity(&moons, &vels);
+    }
+
+    find_common_cycle(&hist)
 }
 
 fn parse_moon(line: String) -> Vek {
@@ -88,4 +182,5 @@ fn main() {
     ).collect();
 
     println!("{}", energy_after(moons.clone(), 1000));
+    println!("{}", world_period(moons.clone()));
 }
