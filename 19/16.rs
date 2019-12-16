a67ea5519e4db55ceea5bcdc2138c7dbaf6ba2f6
Konsta 'sooda' Hölttä <sooda@iki.fi>
2019-12-16 18:29:43 +0200

    16b the bigger real signal is easier to compute
    The real signal is the original concatenated 10k times and the position
    of the eight-digit message is not at the beginning but at an offset
    described by the first seven digits. The offset makes the computation
    relatively easier.
    
    The offset is quite large, so the pattern becomes a string of zeros
    followed by a string of ones that can be used in O(n) time instead of
    O(n^2) time, speeding up quite a bit when the repeated signal length is
    6,5 million digits. Each digit computation refers to either no digits or
    all the digits to the right from it, using their sum, so fold the sum
    from the right and store the value of each resulting digit.

--- 19/16.rs
+++ 19/16.rs
@@ -16,18 +16,58 @@ fn fft_once(digits: &[i32]) -> Vec<i32> {
     out
 }
 
-fn fft_100(mut digits: Vec<i32>) -> Vec<i32> {
+fn fft_100_message(mut digits: Vec<i32>) -> String {
     for _ in 0..100 {
         digits = fft_once(&digits);
     }
-    digits
+    digits.into_iter().take(8).map(|d| (d as u8 + b'0') as char).collect::<String>()
+}
+
+/*
+ * p: positive 1
+ * n: negative 1
+ * after n = len / 2 for big n, nth pattern is n+1 zeros and rest ones
+ *    0 1 2 3 4 5 6 7 8 n
+ * 0: 0 p 0 n 0 p 0 n 0
+ * 1: 0 0 p p 0 0 n n 0
+ * 2: 0 0 0 p p p 0 0 0
+ * 3: 0 0 0 0 p p p p 0
+ * 4: 0 0 0 0 0 p p p p
+ * 5: 0 0 0 0 0 0 p p p
+ * 6: 0 0 0 0 0 0 0 p p
+ * 7: 0 0 0 0 0 0 0 0 p
+ */
+fn fft_100_message_real(digits: &[i32]) -> String {
+    let offset = digits.iter().take(7).fold(0, |acc, x| acc * 10 + x) as usize;
+    let mut signal = Vec::with_capacity(10000 * digits.len());
+
+    for _ in 0..10000 {
+        signal.extend_from_slice(digits);
+    }
+
+    let len = signal.len();
+    assert!(offset >= len / 2);
+
+    for _ in 0..100 {
+        let mut next = vec![0; len];
+        let mut acc = 0;
+        // start from the end of the digit sequence, keep accumulating the sum in reverse
+        for (digit, n) in signal.into_iter().zip(next.iter_mut()).rev().take(len / 2) {
+            acc += digit;
+            *n = acc % 10;
+        }
+        // 0..len/2 stay zeros
+
+        signal = next;
+    }
+    let result_digits = signal.into_iter().skip(offset).take(8);
+    result_digits.map(|d| (d as u8 + b'0') as char).collect::<String>()
 }
 
 fn main() {
     let digits: Vec<i32> = io::stdin().lock().lines().next().unwrap().unwrap()
         .bytes().map(|b| (b - b'0') as i32).collect();
 
-    let first_8 = &fft_100(digits.clone())[0..8];
-    let as_string = first_8.into_iter().map(|&i| ((i as u8) + b'0') as char).collect::<String>();
-    println!("{}", as_string);
+    println!("{}", fft_100_message(digits.clone()));
+    println!("{}", fft_100_message_real(&digits));
 }
