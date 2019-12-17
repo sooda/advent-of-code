b834fd24dab2b08d68f46157b3d1974f518f6e42
Konsta 'sooda' Hölttä <sooda@iki.fi>
2019-12-17 20:46:36 +0200

    17b notify all other robots on the scaffold
    Override the randomness of the vacuum robot and substitute new rules.
    The robot needs to access every part at least once, and the map seems to
    have a few repeating structures and it's not too big, so running a
    compression algorithm manually in a text editor is enough to solve this.
    An automated version is coming soon.
    
    The algorithm packs the repeats into three "functions" called by the
    "main routine". Given the rules, the robot starts to animate its
    movement and it finally prints out the amount of dust collected.

--- 19/17.rs
+++ 19/17.rs
@@ -108,7 +108,6 @@ fn read_map(program: &[i64]) -> HashMap<(i64, i64), char> {
     let mut y = 0;
     while let Some(out) = execute(&mut computer) {
         let ch = out as u8 as char;
-        print!("{}", ch);
         if ch == '\n' {
             y += 1;
             x = 0;
@@ -120,8 +119,22 @@ fn read_map(program: &[i64]) -> HashMap<(i64, i64), char> {
     pixels
 }
 
+fn dump(pixels: &HashMap<(i64, i64), char>) {
+    let minx = pixels.keys().map(|&(x, _)| x).min().unwrap();
+    let maxx = pixels.keys().map(|&(x, _)| x).max().unwrap();
+    let miny = pixels.keys().map(|&(_, y)| y).min().unwrap();
+    let maxy = pixels.keys().map(|&(_, y)| y).max().unwrap();
+    for y in miny..=maxy {
+        for x in minx..=maxx {
+            print!("{}", pixels[&(x, y)]);
+        }
+        println!();
+    }
+}
+
 fn alignment_parameters(program: &[i64]) -> i64 {
     let pixels = read_map(program);
+    dump(&pixels);
 
     let mut calib = 0;
     for (&(px, py), &pixel) in &pixels {
@@ -137,9 +150,120 @@ fn alignment_parameters(program: &[i64]) -> i64 {
     calib
 }
 
+fn execute_dusting(computer: &mut Computer, inputs: &[i64]) -> Option<i64> {
+    let mut input = inputs.iter();
+    let mut dust = None;
+    while let Some((newip, newbase, newout)) =
+            step(&mut computer.program, computer.ip, computer.base, &mut input) {
+        computer.ip = newip;
+        computer.base = newbase;
+        if let Some(out) = newout {
+            if out <= 127 {
+                // the animation is epic
+                print!("{}", newout.unwrap() as u8 as char);
+            } else {
+                assert!(dust.is_none());
+                dust = Some(out);
+            }
+        }
+    }
+    dust
+}
+
+
+fn dust_scaffold(program: &[i64], functions: &[&str]) -> i64 {
+    let mut prog = program.to_vec();
+    prog.resize(prog.len() + 2000, 0);
+    prog[0] = 2;
+    let mut computer = Computer {
+        program: prog,
+        ip: 0,
+        base: 0
+    };
+    let input = functions.join("\n");
+    let mut input = input.as_bytes().iter().map(|&x| x as i64).collect::<Vec<_>>();
+    input.push('\n' as i64);
+    input.push('y' as i64);
+    input.push('\n' as i64);
+
+    execute_dusting(&mut computer, &input).unwrap()
+}
+
+fn walk_scaffold(pixels: &HashMap<(i64, i64), char>) -> Vec<String> {
+    let map = |x, y| pixels.get(&(x, y)).copied().unwrap_or('.');
+    // the robot seems to be always facing up and at the end of the scaffold
+    let (&(mut robot_x, mut robot_y), &_) = pixels.iter().find(|&(&(_, _), &px)| px == '^').unwrap();
+    let (mut dx, mut dy) = (0, 1); // dy 1 is up
+
+    let mut trip = Vec::new();
+    let mut forward = 0;
+
+    loop {
+        let (nx, ny) = (robot_x + dx, robot_y - dy);
+        let tile = map(nx, ny);
+        if tile == '#' {
+            // scaffold continues
+            robot_x = nx;
+            robot_y = ny;
+            forward += 1;
+        } else {
+            // next step would throw the robot tumbling through space uncontrollably, so turn
+            assert!(tile == '.');
+            let leftd = (-dy, dx);
+            let rightd = (dy, -dx);
+            let leftpos = (robot_x + leftd.0, robot_y - leftd.1);
+            let rightpos = (robot_x + rightd.0, robot_y - rightd.1);
+            let leftway = map(leftpos.0, leftpos.1) == '#';
+            let rightway = map(rightpos.0, rightpos.1) == '#';
+            if !leftway && !rightway {
+                // no way, reached the end
+                break;
+            }
+            // one of these is set; the map contains only turns, no T junctions
+            assert_ne!(leftway, rightway);
+
+            // the very first step is likely a turn (is in my input and some samples)
+            if forward > 0 {
+                // log the walked path
+                trip.push(forward.to_string());
+                forward = 0;
+            }
+
+            // log the turn and do it
+            if leftway {
+                trip.push('L'.to_string());
+                dx = leftd.0;
+                dy = leftd.1;
+            } else {
+                trip.push('R'.to_string());
+                dx = rightd.0;
+                dy = rightd.1;
+            }
+        }
+    }
+
+    trip
+}
+fn sweep_clean(program: &[i64]) -> i64 {
+    let pixels = read_map(program);
+    let steps = walk_scaffold(&pixels);
+
+    // TODO: less manual algorithm
+    println!("{}", steps.join(","));
+    let functions = &[
+        "A,B,A,B,C,B,C,A,B,C",
+        "R,4,R,10,R,8,R,4",
+        "R,10,R,6,R,4",
+        "R,4,L,12,R,6,L,12"
+    ];
+    // find other robots, notify of the impending solar flare, and clean the path
+    dust_scaffold(program, functions)
+}
+
 fn main() {
     let program: Vec<i64> = io::stdin().lock().lines().next().unwrap().unwrap()
         .split(',').map(|n| n.parse().unwrap()).collect();
 
     println!("{}", alignment_parameters(&program));
+    println!("{}", sweep_clean(&program));
 }
