8a25b709fc5bee57d457fc781962afcc02ac9c97
Konsta 'sooda' Hölttä <sooda@iki.fi>
2019-12-24 13:47:20 +0200

    24b eris is an old plutonian settlement
    With increased knowledge about the recursively-folded space, the
    cellular life computation is slightly more involved at the borders of
    each recursion level. The 5x5 grids cascade like the cross sections of a
    funnel. Now that the puzzle is about the number of bugs and that the
    scan lasts only 200 minutes, 100 plus one recursion levels are enough to
    encode the whole infinite recursion; when bugs initially exist only at a
    middle level, this is the amount of cells that can possibly be infested
    at the end.
    
    The code repetition is nasty but manageable. Now the simulation counts
    the sum of cells in each neighbour because the next level in the center
    is five neighs that may be occupied.

--- 19/24.rs
+++ 19/24.rs
@@ -50,6 +50,10 @@ impl Eris {
         self.0
     }
 
+    fn bugs_alive(&self) -> usize {
+        self.0.count_ones() as usize
+    }
+
     fn dump(&self) {
         let mut data = self.0;
         for _y in 0..5 {
@@ -107,10 +111,101 @@ fn first_repetition(mut bugs: Eris) -> Rating {
     bugs.biodiversity_rating()
 }
 
+fn next_border_count(layers: &[Eris], index: usize, from_x: i32, from_y: i32) -> usize {
+    if index == layers.len() {
+        // the simulation doesn't last long enough for anything to appear here
+        0
+    } else if from_x == 2 && from_y == 1 {
+        // top
+        (0..5).filter(|&x| layers[index].bug_at(x, 0)).count()
+    } else if from_x == 2 && from_y == 3 {
+        // bottom
+        (0..5).filter(|&x| layers[index].bug_at(x, 4)).count()
+    } else if from_x == 1 && from_y == 2 {
+        // left
+        (0..5).filter(|&y| layers[index].bug_at(0, y)).count()
+    } else if from_x == 3 && from_y == 2 {
+        // right
+        (0..5).filter(|&y| layers[index].bug_at(4, y)).count()
+    } else {
+        panic!("logic error");
+    }
+}
+
+fn next_alive_recursive(layers: &[Eris], current: usize, x: i32, y: i32) -> bool {
+    // TODO: bitmasks and popcount
+    let adjacent = [
+        (x - 1, y    ),
+        (x + 1, y    ),
+        (x    , y - 1),
+        (x    , y + 1),
+    ];
+    let surrounding_bugs = adjacent.into_iter().map(|&(xi, yi)| {
+        if (xi, yi) == (2, 2) {
+            next_border_count(layers, current + 1, x, y)
+        // the simulation doesn't last long enough for anything to appear in outermost layer 0
+        } else if yi == -1 && current > 0 {
+            // top
+            if layers[current - 1].bug_at(2, 1) { 1 } else { 0 }
+        } else if yi == 5 && current > 0 {
+            // bottom
+            if layers[current - 1].bug_at(2, 3) { 1 } else { 0 }
+        } else if xi == -1 && current > 0 {
+            // left
+            if layers[current - 1].bug_at(1, 2) { 1 } else { 0 }
+        } else if xi == 5 && current > 0 {
+            // right
+            if layers[current - 1].bug_at(3, 2) { 1 } else { 0 }
+        } else {
+            if layers[current].bug_at(xi, yi) { 1 } else { 0 }
+        }
+    }).sum::<usize>();
+    if layers[current].bug_at(x, y) {
+        surrounding_bugs == 1
+    } else {
+        surrounding_bugs == 1 || surrounding_bugs == 2
+    }
+}
+
+fn round_recursive(input: &[Eris]) -> Vec<Eris> {
+    let depth = input.len();
+    let mut output = vec![Eris::new(); depth];
+    for (i, (_inlayer, outlayer)) in input.iter().zip(output.iter_mut()).enumerate() {
+        for (x, y) in (0..5).flat_map(|y| (0..5).map(move |x| (x, y))) {
+            if x == 2 && y == 2 {
+                // this bit is never used; it's the portal to the next recursion level
+                continue;
+            }
+            if next_alive_recursive(input, i, x, y) {
+                outlayer.infect_at(x, y);
+            }
+        }
+    }
+    output
+}
+
+fn play_recursive(bugs: Eris, steps: usize) -> usize {
+    // "steps/2" number of layers on top and below: border and center are two cells away
+    let mut layers = vec![Eris::new(); steps + 1];
+    layers[steps / 2] = bugs;
+    for i in 0..steps {
+        layers = round_recursive(&layers);
+        if false {
+            println!("after minutes {}", i + 1);
+            for (i, l) in layers.iter().enumerate() {
+                println!("Depth {}:", (i as i32) - (steps as i32));
+                l.dump();
+            }
+        }
+    }
+    layers.into_iter().map(|bugs| bugs.bugs_alive()).sum()
+}
+
 fn main() {
     let input: Vec<Vec<char>> = io::stdin().lock().lines().map(|line|
         line.unwrap().chars().collect()).collect();
     let bugs = Eris::from_lines(&input);
 
     println!("{:?}", first_repetition(bugs));
+    println!("{:?}", play_recursive(bugs, 200));
 }
