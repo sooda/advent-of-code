e62b2bc84a443a7063ed83e1b365dedb1986ac40
Konsta 'sooda' Hölttä <sooda@iki.fi>
2019-12-26 00:03:40 +0200

    20b ancient plutonians built recursive spaces
    The connections are not actually portals but doors into inner and outer
    dimensions of the same structure (day 24b looks much like this). The
    "portal" pairs are on the inner and outer sides of the donut structure.
    Add a hacky flag to the main search to support recursion mode such that
    the portals change a depth number in the state. The original teleporting
    search goes on level 0 only.
    
    Fake the search a bit and don't step over 100 deep. Maybe the number of
    portals and the number of steps between them could work as a heuristic.

--- 19/20.rs
+++ 19/20.rs
@@ -25,38 +25,42 @@ struct Portal {
     label: (char, char),
     begin: Vec2,
     end: Vec2,
+    outer: bool,
 }
 
 impl Portal {
-    fn new(label: (char, char), begin: Vec2, end: Vec2) -> Self {
-        Portal { label, begin, end }
+    fn new(label: (char, char), begin: Vec2, end: Vec2, outer: bool) -> Self {
+        Portal { label, begin, end, outer }
     }
 }
 
 impl std::fmt::Debug for Portal {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}{}@({},{})", self.label.0, self.label.1, self.begin.0, self.begin.1)
+        write!(f, "{}{}@({},{}/{:?})", self.label.0, self.label.1, self.begin.0, self.begin.1, self.outer)
     }
 }
 
 fn point_of_interest(map: &Map, x: usize, y: usize, require_head: bool) -> Option<Portal> {
+    let h = map.len(); // sizes include sentinel borders
+    let w = map[0].len();
     let ch = map[y][x];
     if ch == '.' || ch == '#' || ch == ' ' {
         None
     } else {
         assert!(is_alpha(ch));
+        let outer = x <= 2 || y <= 2 || x >= w - 3 || y >= h - 3;
         if is_alpha(map[y + 1][x]) {
             // this is the top of a vertical port name
-            Some(Portal::new((map[y][x], map[y + 1][x]), (x, y), (x, y + 1)))
+            Some(Portal::new((map[y][x], map[y + 1][x]), (x, y), (x, y + 1), outer))
         } else if is_alpha(map[y][x + 1]) {
             // this is the left of a horizontal port name
-            Some(Portal::new((map[y][x], map[y][x + 1]), (x, y), (x + 1, y)))
+            Some(Portal::new((map[y][x], map[y][x + 1]), (x, y), (x + 1, y), outer))
         } else if !require_head && is_alpha(map[y - 1][x]) {
             // this is the bottom of a vertical port name
-            Some(Portal::new((map[y - 1][x], map[y][x]), (x, y - 1), (x, y)))
+            Some(Portal::new((map[y - 1][x], map[y][x]), (x, y - 1), (x, y), outer))
         } else if !require_head && is_alpha(map[y][x - 1]) {
             // this is the right of a horizontal port name
-            Some(Portal::new((map[y][x - 1], map[y][x]), (x - 1, y), (x, y)))
+            Some(Portal::new((map[y][x - 1], map[y][x]), (x - 1, y), (x, y), outer))
         } else {
             None
         }
@@ -136,48 +140,97 @@ fn bfs_places(map: &Map, origin: Portal) -> DestinationMap {
             point_of_interest(map, x, y, false).map(|port| (port, dist))
         )
         .collect();
-    println!("from {:?}: {:?}", origin, interesting);
+    //println!("from {:?}: {:?}", origin, interesting);
     interesting
 }
 
-type GdistMap = HashMap<Portal, usize>;
+// // (distance, depth)
+// (portal id, depth) - FIXME: new type for deep portal?
+type GdistMap = HashMap<(Portal, usize), usize>;
 
-fn portal_dijkstra(dl: &DistanceList, origin: Portal) -> usize {
-    let mut heap: BinaryHeap<(i64, Portal)> = BinaryHeap::new(); // -dist, node
-    let mut distances: GdistMap = dl.iter()
-        .map(|(&port, _dist)| {
-            (port, std::usize::MAX)
-        }).collect();
-
-    distances.insert(origin, 0);
-    heap.push((0, origin));
+fn portal_dijkstra(dl: &DistanceList, origin: Portal, recursion_mode: bool) -> usize {
+    let mut heap: BinaryHeap<(i64, Portal, usize)> = BinaryHeap::new(); // -dist, node, depth
+    // can't prime this because depth is arbitrary
+    let mut distances = GdistMap::new();
+    heap.push((0, origin, 0));
+    let mut parents = HashMap::new();
 
     while let Some(current) = heap.pop() {
-        let (dist_i, port_i) = current;
+        let (dist_i, port_i, depth_i) = current;
         let dist_i = (-dist_i) as usize;
+        //println!("at dist {:?} portal {:?} dep {:?}", dist_i, port_i, depth_i);
+        let outer_i = port_i.outer;
 
         for (&port_j, &dist_ij) in &dl[&port_i] {
+            let pair = port_i.label == port_j.label; // same name, other side
+            let outer_j = port_j.outer;
             let dist_j = dist_i + dist_ij;
-            if dist_j < distances[&port_j] {
-                heap.push((-(dist_j as i64), port_j));
-                // XXX: maybe the entry api would hash this just once? look into it
-                distances.insert(port_j, dist_j);
+
+            let depth_j = if recursion_mode && pair && outer_j {
+                // teleports deeper: entering the outer side of an enclosed space
+                if depth_i == 100 {
+                    // heuristic: certainly not this far
+                    continue;
+                }
+                depth_i + 1
+            } else if recursion_mode && pair && outer_i {
+                // teleports back: entering the inner side from within
+                if depth_i == 0 /*&& port_j.label != ('Z', 'Z')*/ {
+                    //println!("    cannot dep {} into {:?}", depth_i, port_j);
+                    continue;
+                }
+                depth_i - 1
+            } else {
+                assert!(!pair || !recursion_mode);
+                /* different portal on the same level */
+                if depth_i != 0 && port_j.label == ('Z', 'Z') {
+                    continue;
+                }
+                depth_i
+            };
+            if port_j.label == ('A', 'A') {
+                //println!("no back again");
+                continue;
+            }
+            // XXX: maybe the entry api would hash this just once? look into it
+            if dist_j < *distances.get(&(port_j, depth_j)).unwrap_or(&std::usize::MAX) {
+                //println!("    look port {:?} dep {:?} dist {:?} steps {:?}", port_j, depth_j, dist_j, dist_ij);
+                heap.push((-(dist_j as i64), port_j, depth_j));
+                distances.insert((port_j, depth_j), dist_j);
+                parents.insert((port_j, depth_j), (port_i, depth_i));
             }
         }
     }
 
+    println!("boarding completed");
+
+    let mut portal = (*dl.keys().filter(|&&k| k.label == ('Z', 'Z')).next().unwrap(), 0);
+    if false {
+        let mut dist = 0;
+        while let Some(&next) = parents.get(&portal) {
+            dist += dl[&next.0][&portal.0];
+            println!("{:?} {}", next, dist);
+            portal = next;
+        }
+    }
+
     distances.into_iter()
-        .find(|&(port, _dist)| port.label == ('Z', 'Z'))
-        .map(|(_port, dist)| dist)
+        .find(|&((port, _depth), _dist)| port.label == ('Z', 'Z'))
+        .map(|((_port, _depth), dist)| dist)
         .unwrap()
 }
 
 // AA and ZZ have no pairs
 fn port_pair(a: Portal, ports: &[Portal]) -> Option<Portal> {
-    ports.iter().find(|&&p| p.label == a.label && p != a).cloned()
+    let pair = ports.iter().find(|&&p| p.label == a.label && p != a).cloned();
+    if let Some(b) = pair {
+        // these must be on opposite recursion directions
+        assert!(a.outer != b.outer);
+    }
+    pair
 }
 
-fn step_maze(map: &Map) -> usize {
+fn step_maze(map: &Map, recursion_mode: bool) -> usize {
     // the dist list keys don't need the portal end positions
     let sightseeing: Vec<Portal> = (0..map.len()).flat_map(|y| {
         (0..map[0].len()).filter_map(move |x|
@@ -198,7 +251,7 @@ fn step_maze(map: &Map) -> usize {
     }
 
     // player starts at AA that is unique
-    portal_dijkstra(&dl, *sightseeing.iter().find(|&p| p.label == ('A', 'A')).unwrap())
+    portal_dijkstra(&dl, *sightseeing.iter().find(|&p| p.label == ('A', 'A')).unwrap(), recursion_mode)
 }
 
 fn parse_map(raw_map: Vec<Vec<char>>) -> Map {
@@ -224,5 +277,6 @@ fn main() {
     let map = parse_map(input);
 
     dump(&map);
-    println!("{}", step_maze(&map));
+    println!("{}", step_maze(&map, false));
+    println!("{}", step_maze(&map, true));
 }
