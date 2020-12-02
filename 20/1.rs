3ceb6d5e0022cf9debc08decd2a2c7bf85a2f949
Konsta Hölttä <sooda@iki.fi>
2020-12-02 21:16:27 +0200

    1b more starfish coins
    Loop over the 200 expenses in O(n^3) now with another flat_map,
    remembering to skip over the already stepped items again. The move with
    the innermost closures might become a problem with bigger objects than
    these integers btw, but at least I figured it out faster than last year.

--- 20/1.rs
+++ 20/1.rs
@@ -2,9 +2,17 @@ use std::io::{self, BufRead};
 
 fn accounting(expenses: &[u32]) -> u32 {
     let pair = expenses.iter().enumerate()
-        .flat_map(|(i, a)| expenses.iter().skip(i).map(move |b| (a, b)))
+        .flat_map(|(i, a)| expenses.iter().skip(i).map(move |&b| (a, b)))
         .find(|&(x, y)| x + y == 2020).unwrap();
     pair.0 * pair.1
+}
+
+fn three_entry_accounting(expenses: &[u32]) -> u32 {
+    let pair = expenses.iter().enumerate()
+        .flat_map(|(i, a)| expenses.iter().enumerate().skip(i).map(move |(j, &b)| (a, (j, b))))
+        .flat_map(|(a, (j, b))| expenses.iter().skip(j).map(move |&c| (a, b, c)))
+        .find(|&(x, y, z)| x + y + z == 2020).unwrap();
+    pair.0 * pair.1 * pair.2
 }
 
 fn main() {
@@ -12,4 +20,5 @@ fn main() {
         .map(|cashline| cashline.unwrap().parse().unwrap())
         .collect();
     println!("{}", accounting(&expense_report));
+    println!("{}", three_entry_accounting(&expense_report));
 }
