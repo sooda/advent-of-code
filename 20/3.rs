5bf07a1cd86fd19a20576b8e950ad9c3e2e65b31
Konsta Hölttä <sooda@iki.fi>
2020-12-03 08:00:30 +0200

    3b arboreal stop probability
    With a few different slopes of trajectory, determine the stop count and
    multiply them together. The "right 1, down 2" is special so the outer
    loop needs a step_by() now to be fully generic.

--- 20/3.rs
+++ 20/3.rs
@@ -1,19 +1,18 @@
 use std::io::{self, BufRead};
 
-const TOBOGGAN_SLOPE: usize = 3;
 const TREE: u8 = b'#';
 
 type Map = Vec<Vec<u8>>;
 
-fn tree_pattern(map: &Map) -> usize {
+fn tree_pattern(map: &Map, x_slope: usize, y_slope: usize) -> usize {
     let w = map[0].len();
     let mut x = 0;
     let mut trees_encountered = 0;
-    for row in map {
+    for row in map.iter().step_by(y_slope) {
         if row[x % w] == TREE {
             trees_encountered += 1;
         }
-        x += TOBOGGAN_SLOPE;
+        x += x_slope;
     }
     trees_encountered
 }
@@ -22,5 +21,15 @@ fn main() {
     let map: Map = io::stdin().lock().lines()
         .map(|line| line.unwrap().into_bytes())
         .collect();
-    println!("{}", tree_pattern(&map));
+    println!("{}", tree_pattern(&map, 3, 1));
+    let slopes = &[
+        (1, 1),
+        (3, 1),
+        (5, 1),
+        (7, 1),
+        (1, 2)
+    ];
+    let tree_product = slopes.iter()
+        .fold(1, |prod, &(x, y)| prod * tree_pattern(&map, x, y));
+    println!("{}", tree_product);
 }
