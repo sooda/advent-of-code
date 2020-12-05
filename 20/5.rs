c07974b40e6246c7fd4ab5f372a74673e81dc83d
Konsta Hölttä <sooda@iki.fi>
2020-12-05 07:38:02 +0200

    5 binary boarding
    With airplane ticket seat specs ordered as interesting instructions, the
    binary space partitioning based seat search collapses to just binary
    numbers for the row and the seat. Certainly by chance, seat IDs match
    the row first then column format, but parse those two separately anyway
    for clarity and possible future generalization.

---
+++ 20/5.rs
@@ -0,0 +1,25 @@
+use std::io::{self, BufRead};
+
+fn bsp(input: &str, one: char) -> usize {
+    // could also replace the chars with 0 and 1 and parse that...
+    input.chars().fold(0, |acc, x| acc * 2 + if x == one { 1 } else { 0 })
+}
+
+fn seat_id(boarding_pass: &str) -> usize {
+    let row_spec = &boarding_pass[0..7];
+    let col_spec = &boarding_pass[7..10];
+    let row = bsp(row_spec, 'B');
+    let col = bsp(col_spec, 'R');
+    // the whole pass could be parsed as a single int because the seat id is formed just so, but
+    // maybe part b needs these as separate numbers? so let's be more specific
+    row * 8 + col
+}
+
+fn main() {
+    let boarding_passes: Vec<_> = io::stdin().lock().lines()
+        .map(|line| line.unwrap())
+        .collect();
+    // why no coercion with map(seat_id) :(
+    let max_seat_id = boarding_passes.iter().map(|p| seat_id(p)).max().unwrap();
+    println!("{}", max_seat_id);
+}
