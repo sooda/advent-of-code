c4a3542538ee2864698272e9a46e4eb579bfa196
Konsta Hölttä <sooda@iki.fi>
2020-12-06 07:44:57 +0200

    6b everyone, not anyone
    Now that the entire group has to agree on the answers, loop through the
    answers again and find the intersection instead of the union of the
    answers of members per group. This too would be even easier in bit
    manipulation, but maybe in the future the customs folks would extend the
    rules even further. Valgrind reports ~8k allocations, but this still
    runs in four milliseconds so whatever.

--- 20/6.rs
+++ 20/6.rs
@@ -6,16 +6,26 @@ fn main() {
     io::stdin().read_to_string(&mut group_questions).unwrap();
     group_questions.truncate(group_questions.len() - 1); // strip off last newline
     let groups: Vec<Vec<&str>> = group_questions.split("\n\n").map(|group| {
-        // (could also parse the answers to bitmaps and "|" them together for epic perf)
+        // (could also parse the answers to bitmaps and "|" or "&" them together for epic perf)
         group.split('\n').collect()
     }).collect();
 
-    let sum_yesgroups: usize = groups.iter().map(|people_results| {
+    let sum_someyes: usize = groups.iter().map(|people_results| {
         let mut anyone_yes = HashSet::new();
         for person_results in people_results {
             anyone_yes.extend(person_results.chars());
         }
         anyone_yes.len()
     }).sum();
-    println!("{}", sum_yesgroups);
+    println!("{}", sum_someyes);
+
+    let sum_fullyes: usize = groups.iter().map(|people_results| {
+        let mut all_yes: HashSet<_> = people_results[0].chars().collect();
+        for person_results in people_results.iter().skip(1) {
+            let set: HashSet<_> = person_results.chars().collect();
+            all_yes = all_yes.intersection(&set).copied().collect();
+        }
+        all_yes.len()
+    }).sum();
+    println!("{}", sum_fullyes);
 }
