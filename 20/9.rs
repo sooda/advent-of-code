0e399297f151b1229e5455563a47628dcb951557
Konsta Hölttä <sooda@iki.fi>
2020-12-09 07:56:59 +0200

    9b break xmas with the invalid number
    The backstory is still a bit lacking in concreteness, but now we're
    looking for a window of numbers that sums to the invalid number. This
    time a brute force is too boring, so let's do something that in 2D would
    be a summed-area table but is too easy in 1D and wouldn't even need that
    extra storage, but is cool nevertheless. Scan the elements and produce a
    new list of sums to each element; a window sum is then obtained by
    subtracting two numbers.
    
    Could just loop the two range ends and calculate the ongoing sum while
    doing that, but this is a fun exercise for Iterator::scan.
    
    This particular implementation style has a slight bug of not being able
    to handle the front or back of the list, depending on indexing. Doesn't
    matter here because the solution clearly is somewhere in the middle.
    This is still AoC, not enterprise stuff.

--- 20/9.rs
+++ 20/9.rs
@@ -1,4 +1,5 @@
 use std::io::{self, BufRead};
+use std::cmp::Ordering;
 
 fn no_sum_of_two(input: &[u64], n: usize, i: usize) -> bool {
     let x = input[i];
@@ -22,10 +23,43 @@ fn first_with_bad_property(input: &[u64], n: usize) -> Option<u64> {
     None
 }
 
+fn contiguous_sum(input: &[u64], x: u64) -> u64 {
+    // With this we'd need a special case for the first element. Could also accumulate the sum of
+    // above elements not including the loop elem, but then the last one would be special.
+    let sum_upto: Vec<_> = input.iter().scan(0, |total, &elem| {
+        *total += elem;
+        Some(*total)
+    }).collect();
+
+    // this is the range we're looking for, probably won't start from the top. Could also
+    // chain std::iter::once() with input.iter() above and adjust the loop.
+    let mut a = 1;
+    let mut b = 2;
+
+    // This could be done without the allocation above but splitting the problem makes it a bit
+    // easier to read, and the input is just 1000 things long.
+    loop {
+        // minus one to delete the stuff above this range, but don't touch the first of this range
+        let range_sum = sum_upto[b] - sum_upto[a - 1];
+        match range_sum.cmp(&x) {
+            Ordering::Equal => {
+                let range = &input[a..=b];
+                return range.iter().min().unwrap() + range.iter().max().unwrap();
+            },
+            Ordering::Less => b += 1,
+            Ordering::Greater => a += 1,
+        }
+    }
+}
+
 fn main() {
     let numbers: Vec<u64> = io::stdin().lock().lines()
         .map(|line| line.unwrap().parse().unwrap())
         .collect();
-    println!("{:?}", first_with_bad_property(&numbers, 5));
-    println!("{:?}", first_with_bad_property(&numbers, 25));
+    let a5 = first_with_bad_property(&numbers, 5);
+    let a25 = first_with_bad_property(&numbers, 25);
+    println!("{:?}", a5);
+    println!("{:?}", a25);
+    println!("{:?}", contiguous_sum(&numbers, a5.unwrap()));
+    println!("{:?}", a25.map(|a25| contiguous_sum(&numbers, a25)));
 }
