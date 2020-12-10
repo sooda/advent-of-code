b6bc21d613d1b39d267986fc49ab96a3d3382a4f
Konsta Hölttä <sooda@iki.fi>
2020-12-10 09:02:03 +0200

    10b adapters in all the ways
    Now the list of adapters really is a graph, and we're looking for the
    number of paths in it from begin to end. DFS the graph and memoize the
    results so that most of the paths that are very similar wouldn't get
    recomputed. Looks like the list could actually be traversed in one pass
    without the recursive fun, but oh well this is more generic.

--- 20/10.rs
+++ 20/10.rs
@@ -24,13 +24,38 @@ fn use_adapters(adapters: &[u32]) -> (u32, u32) {
     (jumps_of_one, jumps_of_three)
 }
 
+// ways to arrange the rest of adapter chain starting from i. memoize results to ways
+fn ways_to_arrange(adapters: &[u32], i: usize, ways: &mut [Option<usize>]) -> usize {
+    if let Some(w) = ways[i] {
+        return w;
+    }
+    let these_ways = if i == adapters.len() - 1 {
+        1
+    } else {
+        let mut tot = 0;
+        let a = adapters[i];
+        for (j, &b) in (&adapters[i + 1..]).iter().take(3).enumerate() {
+            if b - a <= 3 {
+                tot += ways_to_arrange(adapters, i + 1 + j, ways);
+            }
+        }
+        tot
+    };
+    ways[i] = Some(these_ways);
+    these_ways
+}
+
 fn main() {
     let mut adapters: Vec<u32> = io::stdin().lock().lines()
         .map(|line| line.unwrap().parse().unwrap())
         .collect();
     adapters.sort_unstable();
-    let adapters = adapters;
-
     let one_three = use_adapters(&adapters);
     println!("{} {} {}", one_three.0, one_three.1, one_three.0 * one_three.1);
+
+    // avoid special case with the arrangement, could have this for the part 1 too though...
+    adapters.insert(0, 0);
+    // no need to add the device for this, by definition it's reachable only from the last so it
+    // would not contribute to path count
+    println!("{}", ways_to_arrange(&adapters, 0, &mut vec![None; adapters.len()]));
 }
