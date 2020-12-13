741f30e51eb0db2534ee7faf0abbe4df0770e65c
Konsta Hölttä <sooda@iki.fi>
2020-12-13 10:08:59 +0200

    13b shuttle company contest
    Given the buses and their offsets, we get a system of congruences and
    some time wasted on staring at lcm/gcd. This CRT solution had to be
    looked up, there's probably some less fancy trick though but the sieve
    was fun to implement in the morning based on just the math. No need to
    order the numbers by decreasing moduli because this is fast already. Or
    at least on my input, it is.
    
    "The Chinese remainder theorem (expressed in terms of congruences) is
    true over every principal ideal domain." mmhm thanks wikipedia

--- 20/13.rs
+++ 20/13.rs
@@ -8,6 +8,32 @@ fn next_bus(timestamp: u64, schedule: &[Option<u64>]) -> (u64, u64) {
     }).min().unwrap()
 }
 
+fn bus_race(schedule: &[Option<u64>]) -> u64 {
+    // (bus, index) for the remainders, skip nones
+    let sched: Vec<(u64, u64)> = schedule.iter().enumerate()
+        .filter_map(|(i, n)| n.map(|n| (n, i as u64)))
+        .collect();
+    // sieve for chinese remainder theorem on this system of congruences
+    let mut prev_start = 0;
+    let mut prev_mult = 1;
+    // time = offs (mod bus) for each transport line
+    for window in sched.windows(2) {
+        let ((bus0, _), (bus1, offs1)) = (window[0], window[1]);
+        // some buses with a short cycle are relatively far away, so they start after completing
+        // many cycles; this method needs the offsets to be within the cycle so make it so
+        let offs1 = offs1 % bus1;
+        for i in 1.. {
+            // this is unreadable so look it up on wikipedia
+            if (prev_start + i * bus0 * prev_mult + offs1) % bus1 == 0 {
+                prev_start += i * bus0 * prev_mult;
+                prev_mult *= bus0;
+                break;
+            }
+        }
+    }
+    prev_start
+}
+
 fn main() {
     let stdin = io::stdin();
     // aaargh
@@ -18,4 +44,5 @@ fn main() {
 
     let bus = next_bus(timestamp, &schedule);
     println!("{} minutes for bus {} = {}", bus.0, bus.1, bus.0 * bus.1);
+    println!("{}", bus_race(&schedule));
 }
