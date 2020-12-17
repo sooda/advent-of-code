ebb298b84333f1368408adae121bfaacd86e85fe
Konsta Hölttä <sooda@iki.fi>
2020-12-17 08:38:27 +0200

    17b pocket dimension is 4d
    Extend the game to work in four dimensions, and for the first part of
    the puzzle, just discard everything but coordinates in the w=0
    hyperplane at the end of each iteration so that the result stays valid.
    Some extra computation is thus made for the border dimensions of w, but
    that's the price of this lazy approach. Could extend the functions to
    some trait thing around a generic coordinate. That should be a neat rust
    exercise for another day.

--- 20/17.rs
+++ 20/17.rs
@@ -1,15 +1,17 @@
 use std::io::{self, BufRead};
 use std::collections::HashSet;
 
-type Map = HashSet<(i32, i32, i32)>;
+type Map = HashSet<(i32, i32, i32, i32)>;
 
-fn neighbourhood(map: &Map, x: i32, y: i32, z: i32) -> usize {
+fn neighbourhood(map: &Map, x: i32, y: i32, z: i32, w: i32) -> usize {
     let mut total = 0;
     for dx in -1..=1 {
         for dy in -1..=1 {
             for dz in -1..=1 {
-                if dx != 0 || dy != 0 || dz != 0 {
-                    total += map.contains(&(x + dx, y + dy, z + dz)) as usize;
+                for dw in -1..=1 {
+                    if dx != 0 || dy != 0 || dz != 0 || dw != 0 {
+                        total += map.contains(&(x + dx, y + dy, z + dz, w + dw)) as usize;
+                    }
                 }
             }
         }
@@ -17,19 +19,19 @@ fn neighbourhood(map: &Map, x: i32, y: i32, z: i32) -> usize {
     total
 }
 
-fn update_cell(map: &Map, x: i32, y: i32, z: i32) -> bool {
-    let n = neighbourhood(map, x, y, z);
-    if map.contains(&(x, y, z)) {
+fn update_cell(map: &Map, x: i32, y: i32, z: i32, w: i32) -> bool {
+    let n = neighbourhood(map, x, y, z, w);
+    if map.contains(&(x, y, z, w)) {
         n == 2 || n == 3
     } else {
         n == 3
     }
 }
 
-fn dimensions(map: &Map) -> ((i32, i32), (i32, i32), (i32, i32)) {
+fn dimensions(map: &Map) -> ((i32, i32), (i32, i32), (i32, i32), (i32, i32)) {
     // the world starts at level 0 so all of these are ok to use, in the worst case this would just
     // be too conservative but wouldn't break anything
-    let (mut x0, mut x1, mut y0, mut y1, mut z0, mut z1) = (0, 0, 0, 0, 0, 0);
+    let (mut x0, mut x1, mut y0, mut y1, mut z0, mut z1, mut w0, mut w1) = (0, 0, 0, 0, 0, 0, 0, 0);
     for k in map.iter() {
         x0 = x0.min(k.0);
         x1 = x1.max(k.0);
@@ -37,8 +39,10 @@ fn dimensions(map: &Map) -> ((i32, i32), (i32, i32), (i32, i32)) {
         y1 = y1.max(k.1);
         z0 = z0.min(k.2);
         z1 = z1.max(k.2);
+        w0 = w0.min(k.3);
+        w1 = w1.max(k.3);
     }
-    ((x0, x1), (y0, y1), (z0, z1))
+    ((x0, x1), (y0, y1), (z0, z1), (w0, w1))
 }
 
 fn dump(map: &Map) {
@@ -47,7 +51,7 @@ fn dump(map: &Map) {
         println!("z={}", z);
         for y in (dim.1.0)..=(dim.1.1) {
             for x in (dim.0.0)..=(dim.0.1) {
-                print!("{}", if map.contains(&(x, y, z)) { '#' } else { '.' });
+                print!("{}", if map.contains(&(x, y, z, 0)) { '#' } else { '.' });
             }
             println!();
         }
@@ -63,11 +67,13 @@ fn step(map: &Map, next: &mut Map) {
     for x in (dim.0.0 - 1)..=(dim.0.1 + 1) {
         for y in (dim.1.0 - 1)..=(dim.1.1 + 1) {
             for z in (dim.2.0 - 1)..=(dim.2.1 + 1) {
-                let state = update_cell(map, x, y, z);
-                if state {
-                    next.insert((x, y, z));
-                } else {
-                    next.remove(&(x, y, z));
+                for w in (dim.3.0 - 1)..=(dim.3.1 + 1) {
+                    let state = update_cell(map, x, y, z, w);
+                    if state {
+                        next.insert((x, y, z, w));
+                    } else {
+                        next.remove(&(x, y, z, w));
+                    }
                 }
             }
         }
@@ -78,29 +84,36 @@ fn score(map: &Map) -> usize {
     map.len()
 }
 
-fn animate(boot_state: &Vec<Vec<char>>, n: usize) -> usize {
+fn animate(boot_state: &Vec<Vec<char>>, n: usize, fourd: bool) -> usize {
     let mut map: Map = boot_state.iter()
         .enumerate().flat_map(|(y, row)| {
             row.iter().enumerate().filter_map(move |(x, &ch)| {
                 match ch {
-                    '#' => Some((x as i32, y as i32, 0)),
+                    '#' => Some((x as i32, y as i32, 0, 0)),
                     '.' => None,
                     _ => panic!()
                 }
             })
         }).collect();
 
-    println!("Before any cycles:");
-    println!();
-    dump(&map);
+    if false {
+        println!("Before any cycles:");
+        println!();
+        dump(&map);
+    }
 
     for i in 1..=n {
         let mut next: Map = Map::new();
         step(&map, &mut next);
         map = next;
-        println!("After {} cycles:", i);
-        println!();
-        dump(&map);
+        if !fourd {
+            map.retain(|&(_x, _y, _z, _w)| _w == 0);
+        }
+        if false {
+            println!("After {} cycles:", i);
+            println!();
+            dump(&map);
+        }
     }
 
     score(&map)
@@ -110,5 +123,6 @@ fn main() {
     let boot_state: Vec<Vec<char>> = io::stdin().lock().lines()
         .map(|line| line.unwrap().chars().collect())
         .collect();
-    println!("{}", animate(&boot_state, 6));
+    println!("{}", animate(&boot_state, 6, false));
+    println!("{}", animate(&boot_state, 6, true));
 }
