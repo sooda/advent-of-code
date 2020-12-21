6dc8ba1f2b56ed1b113f8cf7fc77b4cf43096dfd
Konsta Hölttä <sooda@iki.fi>
2020-12-21 08:31:00 +0200

    21b match ingredients and allergens
    The input is again well-behaving and the initial mapping from allergen
    to ingredients is small, and it contains at least one unique match to
    begin with. With this match recorded and removed from all other
    possibilities, another unique match (or more) appears. Loop this until
    no remaining unknowns remain, and print the ingredients in the correct
    order (by the allergen name).

--- 20/21.rs
+++ 20/21.rs
@@ -7,6 +7,27 @@ struct Food {
     allergens: HashSet<String>,
 }
 
+// which allergen is which ingredient
+fn final_mapping(mut plausible_ingredients: HashMap<String, HashSet<String>>) -> Vec<(String, String)> {
+    let mut out = Vec::new();
+    let unique = |(_k, v): &(&String, &HashSet<String>)| v.len() == 1;
+    let valued_k = |(k, _v): (&String, &HashSet<String>)| k.clone();
+
+    while let Some(allergen) = plausible_ingredients.iter().find(unique).map(valued_k) {
+        // ugh, I want a HashMap::remove that takes a closure and doesn't need to hash because we
+        // already have the entry from the iteration
+        let ingredient = plausible_ingredients.remove(&allergen).unwrap().drain().next().unwrap();
+        for (_alle, ings) in plausible_ingredients.iter_mut() {
+            ings.remove(&ingredient);
+        }
+        out.push((allergen, ingredient));
+    }
+
+    // no ambiguous ingredients should remain
+    assert!(plausible_ingredients.is_empty());
+    out
+}
+
 fn parse_food(line: &str) -> Food {
     let mut sp = line.strip_suffix(")").unwrap().split(" (contains ");
     let ingredients = sp.next().unwrap().split(' ').map(|s| s.to_owned()).collect();
@@ -62,10 +83,22 @@ fn main() {
 
     let safe_ingredients: HashSet<String> = all_ingredients.difference(&dangerous_ingredients)
         .cloned().collect();
-    println!("safe: {:?}", safe_ingredients);
 
     let safe_occurrences = foods.iter().map(|food| {
         food.ingredients.intersection(&safe_ingredients).count()
     }).sum::<usize>();
     println!("safe times: {}", safe_occurrences);
+
+    println!("initially plausible:");
+    for a in &plausible_ingred {
+        println!("{:?}", a);
+    }
+
+    let mut final_mapping: Vec<(String, String)> = final_mapping(plausible_ingred);
+    final_mapping.sort_unstable();
+    let ingredient_list: Vec<String> = final_mapping.into_iter().map(|(_k, v)| v).collect();
+    let canonical_dangerous_ingredient_list = ingredient_list.join(",");
+    println!("{}", canonical_dangerous_ingredient_list);
+
+    // dtb,zgk,pxr,cqnl,xkclg,xtzh,jpnv,lsvlx
 }
