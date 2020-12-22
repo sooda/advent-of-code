e93fd2f59f453eff1a2735ffd9a24079af06ae66
Konsta Hölttä <sooda@iki.fi>
2020-12-22 08:32:25 +0200

    22b defend raft captain honor recursively
    Incorporate the new rules of the game such that each game can spawn a
    sub-game. Each game stores a history of all previous game states as
    specified in the rules to look for cycles that result in the first
    player winning. The recursion is simple enough; just the mission spec
    was slightly confusing so this had initially a bug of not truncating the
    decks for the sub-game (and this bug wouldn't affect the outcome of the
    sample game!).

--- 20/22.rs
+++ 20/22.rs
@@ -1,4 +1,5 @@
 use std::io::{self, BufRead};
+use std::collections::HashSet;
 
 fn deck_score(deck: &[u32]) -> u32 {
     deck.iter().rev().enumerate().map(|(multiplier, card)| {
@@ -28,6 +29,41 @@ fn winning_score(mut deck0: Vec<u32>, mut deck1: Vec<u32>) -> u32 {
     unreachable!()
 }
 
+// ret: (score, winner is player 0)
+fn recursive_combat(mut deck0: Vec<u32>, mut deck1: Vec<u32>) -> (u32, bool) {
+    let mut previous_states: HashSet<(Vec<u32>, Vec<u32>)> = HashSet::new();
+    for _round in 1.. {
+        if !previous_states.insert((deck0.clone(), deck1.clone())) {
+            return (deck_score(&deck0), true);
+        }
+        let card0 = deck0.remove(0);
+        let card1 = deck1.remove(0);
+        assert!(card0 != card1);
+        let recurse = deck0.len() >= card0 as usize && deck1.len() >= card1 as usize;
+        let win_condition = if recurse {
+            recursive_combat(deck0[0..(card0 as usize)].to_vec(), deck1[0..(card1 as usize)].to_vec()).1
+        } else {
+            card0 > card1
+        };
+
+        let (windeck, wincard, losecard) = if win_condition {
+            (&mut deck0, card0, card1)
+        } else {
+            (&mut deck1, card1, card0)
+        };
+        windeck.push(wincard);
+        windeck.push(losecard);
+
+        if deck0.is_empty() {
+            return (deck_score(&deck1), false);
+        }
+        if deck1.is_empty() {
+            return (deck_score(&deck0), true);
+        }
+    }
+    unreachable!()
+}
+
 fn main() {
     let lines: Vec<_> = io::stdin().lock().lines()
         .map(|line| line.unwrap())
@@ -35,5 +71,6 @@ fn main() {
     let mut decks = lines.split(|x| x == "");
     let deck0: Vec<u32> = decks.next().unwrap().iter().skip(1).map(|x| x.parse().unwrap()).collect();
     let deck1: Vec<u32> = decks.next().unwrap().iter().skip(1).map(|x| x.parse().unwrap()).collect();
-    println!("{}", winning_score(deck0, deck1));
+    println!("{}", winning_score(deck0.clone(), deck1.clone()));
+    println!("{}", recursive_combat(deck0, deck1).0);
 }
