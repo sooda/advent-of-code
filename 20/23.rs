393fcc974726e7a354a934422fb3b71e10207cf4
Konsta Hölttä <sooda@iki.fi>
2020-12-23 12:29:20 +0200

    23b one million cups
    Not very fluent in crab. The number of cups is one million (our raft is
    big?) and the game runs for ten million moves. Now the handy vector
    rotations aren't feasible, so rewrite the game with a linked list
    instead. The list needs to be just singly linked and since the game
    consists of a nice list of consecutive integers, the list nodes can be
    indexed by node in a flat plain old vector.
    
    Curiously this was much (20x) slower with a hashmap initially; maybe its
    storage is less efficient for this purpose, although the access pattern
    seems so random that a linear vector shouldn't help that much.
    
    Spent quite some time looking for a cycle here. Long loops always seem
    like they'd require looking for cycles and matching to it, but ten
    million isn't actually a big number. A trillion would be.

--- 20/23.rs
+++ 20/23.rs
@@ -1,48 +1,87 @@
 use std::io::{self, BufRead};
 
+// a simple type to minimize accidental messing up with indices vs values
+#[derive(PartialEq, Clone, Copy)]
+struct Label(usize);
+
 // note!! 1-based indexing
-fn from_labeling(labeling: &str) -> Vec<u8> {
-    labeling.as_bytes().iter().map(|b| b - b'1').collect()
+fn from_labeling(labeling: &str) -> Vec<Label> {
+    labeling.as_bytes().iter().map(|&b| Label((b - b'1') as usize)).collect()
 }
 
-fn to_labeling(cups: &[u8]) -> String {
-    cups.iter().map(|c| (c + b'1') as char).collect()
+fn to_labeling(cups: &[Label]) -> String {
+    cups.iter().map(|&c| (c.0 as u8 + b'1') as char).collect()
 }
 
-fn simulate(labeling: &str, n: usize) -> String {
+fn simulate(cups: Vec<Label>, n: usize) -> Vec<Label> {
     // current cup always in the front
-    let mut cups = from_labeling(labeling);
-    let ncups = 9 as u8;
-    assert!(cups.len() == ncups as usize);
+    let ncups = cups.len();
+    // linked list from label to label
+    let mut successors: Vec<Label> = vec![Label(0); cups.len()];
+    for (&a, &b) in cups.iter().zip(cups.iter().cycle().skip(1)) {
+        successors[a.0] = b;
+    }
 
-    let find_destination = |mut value: u8, cups: &[u8]| {
+    let find_destination = |mut value: Label, removed_cups: (Label, Label, Label)| {
         loop {
             // current minus one, wrapping
-            value = (value + ncups - 1) % ncups;
-            // exists in the remaining cups?
-            if let Some(i) = cups.iter().position(|&c| c == value) {
-                return i;
+            value = Label((value.0 + ncups - 1) % ncups);
+            if value != removed_cups.0 && value != removed_cups.1 && value != removed_cups.2 {
+                return value;
             }
         }
     };
+    let remove_links = |map: &mut Vec<Label>, left_label: Label, last_label: Label| {
+        let right_label = map[last_label.0];
+        map[left_label.0] = right_label;
+        // (successor of last is left dangling)
+    };
+
+    let insert_links = |map: &mut Vec<Label>, left_label: Label, first_label: Label, last_label: Label| {
+        let right_label = map[left_label.0];
+        map[last_label.0] = right_label;
+        map[left_label.0] = first_label;
+    };
+
+    let mut current_label = cups[0];
     for _move in 1..=n {
-        let current = cups[0];
-        let pickup = (cups.remove(1), cups.remove(1), cups.remove(1));
-        let dest_idx = 1 + find_destination(current, &cups[1..]);
-        cups.insert(dest_idx + 1, pickup.2);
-        cups.insert(dest_idx + 1, pickup.1);
-        cups.insert(dest_idx + 1, pickup.0);
-        cups.rotate_left(1);
+        let first_label = successors[current_label.0];
+        let second_label = successors[first_label.0];
+        let third_label = successors[second_label.0];
+
+        remove_links(&mut successors, current_label, third_label);
+
+        let dest_label = find_destination(current_label, (first_label, second_label, third_label));
+        insert_links(&mut successors, dest_label, first_label, third_label);
+
+        current_label = successors[current_label.0];
     }
 
-    let one = cups.iter().position(|&c| c == 0).unwrap();
-    cups.rotate_left(one);
-    to_labeling(&cups[1..])
+    (0..cups.len()).scan(Label(0), |label, _| {
+        let value = *label;
+        *label = successors[label.0];
+        Some(value)
+    }).collect::<Vec<Label>>()
+}
+
+fn short_game(labeling: &str, n: usize) -> String {
+    let final_cups = simulate(from_labeling(labeling), n);
+    to_labeling(&final_cups[1..])
+}
+
+fn long_game(labeling: &str, n: usize, total_cups: usize) -> u64 {
+    let mut cups = from_labeling(labeling);
+    let init_size = cups.len();
+    cups.extend((init_size..total_cups).map(|x| Label(x)));
+    let final_cups = simulate(cups, n);
+    // 1-based values but they're 0-based internally
+    (final_cups[1].0 + 1) as u64 * (final_cups[2].0 + 1) as u64
 }
 
 fn main() {
     let input: String = io::stdin().lock().lines()
         .map(|line| line.unwrap().parse().unwrap())
         .next().unwrap();
-    println!("{}", simulate(&input, 100));
+    println!("{}", short_game(&input, 100));
+    println!("{}", long_game(&input, 10_000_000, 1_000_000));
 }
