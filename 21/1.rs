d6d73e78dcb191c4d7b621494ab576c3864d0a47
Konsta Hölttä <sooda@iki.fi>
2021-12-01 07:29:25 +0200

    1b denoise the data
    Average out the sea floor with a sliding window of three samples and do
    the depth check on that data. Although some numbers in the equation
    would cancel out, keeping the summation shows how this could be generic
    over any kind of convolution filter (although that too would somewhat
    cancel out on consecutive samples).

--- 21/1.rs
+++ 21/1.rs
@@ -6,9 +6,18 @@ fn increase_count(depths: &[u32]) -> usize {
         .count()
 }
 
+fn increase_count_windowed(depths: &[u32]) -> usize {
+    depths.windows(3).zip(depths.windows(3).skip(1))
+        .filter(|(prev, next)| {
+            next[0] + next[1] + next[2] > prev[0] + prev[1] + prev[2]
+        })
+        .count()
+}
+
 fn main() {
     let sonar_report: Vec<u32> = io::stdin().lock().lines()
         .map(|line| line.unwrap().parse().unwrap())
         .collect();
     println!("{}", increase_count(&sonar_report));
+    println!("{}", increase_count_windowed(&sonar_report));
 }
