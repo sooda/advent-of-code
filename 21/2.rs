2120b58f83b2cab2cc5bb28b3df4368bb0080d2a
Konsta Hölttä <sooda@iki.fi>
2021-12-02 07:44:45 +0200

    2b slightly more complicated
    The submarine is not an infinitesimal point with no rotation; its state
    includes also the aim, which the up and down instructions affect.
    Because it's getting complex with these three numbers, put them in a
    struct and impl some methods based on the new understanding.

--- 21/2.rs
+++ 21/2.rs
@@ -17,9 +17,58 @@ fn final_position(course_plan: &[String]) -> i32 {
     final_pos.0 * final_pos.1
 }
 
+struct Sub {
+    pos: (i32, i32),
+    aim: i32,
+}
+
+impl Sub {
+    fn new() -> Sub {
+        Sub { pos: (0, 0), aim: 0 }
+    }
+
+    fn forward(self, force: i32) -> Sub {
+        Sub {
+            pos: (self.pos.0 + force, self.pos.1 + self.aim * force),
+            aim: self.aim
+        }
+    }
+
+    fn steer(self, force: i32) -> Sub {
+        Sub {
+            pos: self.pos,
+            aim: self.aim + force
+        }
+    }
+
+    fn steer_aiming(self, step: &str) -> Sub {
+        let mut words = step.split(' ');
+        let direction = words.next().unwrap();
+        let amount = words.next().unwrap().parse::<i32>().unwrap();
+        match direction {
+            "forward" => self.forward(amount),
+            "down" => self.steer(amount),
+            "up" => self.steer(-amount),
+            _ => panic!()
+        }
+    }
+
+    fn position_score(&self) -> i32 {
+        self.pos.0 * self.pos.1
+    }
+}
+
+fn final_position_aiming(course_plan: &[String]) -> i32 {
+    let final_sub = course_plan.iter().fold(
+        Sub::new(), |sub, step| sub.steer_aiming(step)
+    );
+    final_sub.position_score()
+}
+
 fn main() {
     let course_plan: Vec<String> = io::stdin().lock().lines()
         .map(|line| line.unwrap())
         .collect();
     println!("{}", final_position(&course_plan));
+    println!("{}", final_position_aiming(&course_plan));
 }
