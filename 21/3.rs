7d6f93c4d9afca3dc1c6532b81b094a9dec08d1e
Konsta Hölttä <sooda@iki.fi>
2021-12-03 08:33:55 +0200

    3b life support rating
    Now reduce the report iteratively bit by bit until only one value
    remains filtered. The list of values is not actually changed but
    filtered every time to avoid memory allocations from further vecs using
    the desired top bits for the filtered values, although there's also
    partition_in_place() that might be even better.
    
    The mutually inverse logic for oxygen generator and co2 scrubber ratings
    calls for some cool trickery, but that's apparently not possible to
    realize just after waking up. Maybe later.

--- 21/3.rs
+++ 21/3.rs
@@ -14,6 +14,49 @@ fn power_consumption(diagnostic_report: &[u32], length: usize) -> u32 {
     gamma_rate * epsilon_rate
 }
 
+fn rating(diagnostic_report: &[u32], length: usize, bit_criteria: u32) -> u32 {
+    let mut desired_topbits = 0;
+    let mut topbits_mask = 0;
+    for bitpos in (0..length).rev() {
+        let mut filtered_report = diagnostic_report.iter()
+            .filter(move |&&x| (x & topbits_mask == desired_topbits));
+        let n_remaining = filtered_report.clone().count();
+        if n_remaining == 1 {
+            // ?
+            return *filtered_report.next().unwrap();
+        }
+        let ones = filtered_report.clone().filter(|&&x| (x & (1 << bitpos)) != 0).count();
+        desired_topbits |= if bit_criteria == 1 {
+            if ones >= n_remaining - ones {
+                1 << bitpos
+            } else {
+                0
+            }
+        } else {
+            if ones < n_remaining - ones {
+                1 << bitpos
+            } else {
+                0
+            }
+        };
+        topbits_mask |= 1 << bitpos;
+        let mut filtered_report = diagnostic_report.iter()
+            .filter(move |&&x| (x & topbits_mask == desired_topbits));
+        if filtered_report.clone().count() == 1 {
+            return *filtered_report.next().unwrap();
+        }
+    }
+    panic!()
+}
+
+fn life_support(diagnostic_report: &[u32], length: usize) -> u32 {
+    let len_mask = (1 << length) - 1;
+    let oxygen_generator_rating = rating(diagnostic_report, length, 1);
+    // todo: !
+    let co2_scrubber_rating = rating(diagnostic_report, length, 0) & len_mask;
+    oxygen_generator_rating * co2_scrubber_rating
+}
+
 fn main() {
     let mut length: usize = 0;
     let diagnostic_report: Vec<u32> = io::stdin().lock().lines()
@@ -24,4 +67,5 @@ fn main() {
         })
         .collect();
     println!("{}", power_consumption(&diagnostic_report, length));
+    println!("{}", life_support(&diagnostic_report, length));
 }
