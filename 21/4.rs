a9eb42f01a96d8aa5548a30e9ad3eea0a7cafde6
Konsta Hölttä <sooda@iki.fi>
2021-12-04 07:37:25 +0200

    4 giant squid
    Parse the the bingo configuration and play until one player gets one
    full row, because what vessel does not have a bingo subsystem??
    Curiously a squid _outside_ the vessel is considered a passenger, maybe
    it'll pay for the ticket. A board is defined by an array of numbers and
    corresponding flags on which numbers have been marked. The array is a
    flattened representation of five elements in each direction.
    
    The logic assumes that the game input is well-formed; only one player
    should win at a time, and some player must win before the numbers run
    out.
    
    Parsing this from the input is also surprisingly easy with just chunked
    sections, with the board lines flat_map'd to one series of numbers.

---
+++ 21/4.rs
@@ -0,0 +1,75 @@
+use std::io::{self, BufRead};
+
+struct Board {
+    numbers: [u32; 5 * 5],
+    marked: [bool; 5 * 5],
+}
+
+impl Board {
+    fn new(numbers: [u32; 5 * 5]) -> Self {
+        Board { numbers: numbers, marked: [false; 5 * 5] }
+    }
+
+    fn winning_sum(&self) -> u32 {
+        self.numbers.iter().zip(self.marked.iter())
+            .filter(|&(_n, m)| !m)
+            .map(|(n, _m)| n)
+            .sum()
+    }
+
+    fn row_complete(&self, i: usize) -> bool {
+        self.marked.iter().skip(5 * i).take(5).all(|&x| x)
+    }
+
+    fn col_complete(&self, i: usize) -> bool {
+        self.marked.chunks(5).map(|c| c[i]).all(|x| x)
+    }
+
+    fn one_complete(&self) -> bool {
+        (0..5).any(|i| self.row_complete(i) || self.col_complete(i))
+    }
+
+    fn play_round(&mut self, number: u32) -> Option<u32> {
+        if let Some(index) = self.numbers.iter().position(|&n| n == number) {
+            self.marked[index] = true;
+            if self.one_complete() {
+                Some(number * self.winning_sum())
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+
+}
+
+fn winning_score(boards: &mut [Board], drawn_numbers: &[u32]) -> u32 {
+    for &num in drawn_numbers {
+        for board in boards.iter_mut() {
+            if let Some(score) = board.play_round(num) {
+                return score;
+            }
+        }
+    }
+    panic!("nobody won");
+}
+
+fn parse_board(board_spec: &[String]) -> Board {
+    let mut numbers = [0; 5 * 5];
+    let input = board_spec.iter().flat_map(
+        |spec| spec.split_whitespace().map(|n| n.parse::<u32>().unwrap())
+    );
+    numbers.iter_mut().zip(input).for_each(|(a, i)| *a = i);
+    Board::new(numbers)
+}
+
+fn main() {
+    let lines: Vec<String> = io::stdin().lock().lines().map(|l| l.unwrap()).collect();
+    let drawn_numbers: Vec<u32> = lines[0].split(',')
+        .map(|num| num.parse().unwrap()).collect();
+    let mut boards: Vec<Board> = lines[1..].chunks(6)
+        .map(parse_board)
+        .collect();
+    println!("{}", winning_score(&mut boards, &drawn_numbers));
+}
