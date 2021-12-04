b9d563f0a6eeaf9c64239a1cf011287c8b2dac90
Konsta Hölttä <sooda@iki.fi>
2021-12-04 10:50:53 +0200

    unify 2a and 2b
    Turns out that the turning of the ship still keeps enough information
    for the score of the first puzzle, so delete the duplicate code that
    calculated the simple score. Run the sub only once, and figure out both
    scores at the end; the x coordinate is identical in both, and the y
    coordinate of part 1 is equivalent to the aim from the corrected
    instructions.
    
    A proper level of overengineering for this day would be to parse the
    instructions to some well typed enums, but come on there's just three.
    Or perhaps the instructions could define a microarchitecture for the sub
    that would be JITed?

--- 21/2.rs
+++ 21/2.rs
@@ -1,22 +1,5 @@
 use std::io::{self, BufRead};
 
-fn steer(pos: (i32, i32), step: &str) -> (i32, i32) {
-    let mut words = step.split(' ');
-    let direction = words.next().unwrap();
-    let amount = words.next().unwrap().parse::<i32>().unwrap();
-    match direction {
-        "forward" => (pos.0 + amount, pos.1),
-        "down" => (pos.0, pos.1 + amount),
-        "up" => (pos.0, pos.1 - amount),
-        _ => panic!()
-    }
-}
-
-fn final_position(course_plan: &[String]) -> i32 {
-    let final_pos = course_plan.iter().fold((0i32, 0i32), |acc, step| steer(acc, step));
-    final_pos.0 * final_pos.1
-}
-
 struct Sub {
     pos: (i32, i32),
     aim: i32,
@@ -56,19 +39,22 @@ impl Sub {
     fn position_score(&self) -> i32 {
         self.pos.0 * self.pos.1
     }
+
+    fn simple_position_score(&self) -> i32 {
+        self.pos.0 * self.aim
+    }
 }
 
-fn final_position_aiming(course_plan: &[String]) -> i32 {
+fn final_scores_aiming(course_plan: &[String]) -> (i32, i32) {
     let final_sub = course_plan.iter().fold(
         Sub::new(), |sub, step| sub.steer_aiming(step)
     );
-    final_sub.position_score()
+    (final_sub.simple_position_score(), final_sub.position_score())
 }
 
 fn main() {
     let course_plan: Vec<String> = io::stdin().lock().lines()
         .map(|line| line.unwrap())
         .collect();
-    println!("{}", final_position(&course_plan));
-    println!("{}", final_position_aiming(&course_plan));
+    println!("{:?}", final_scores_aiming(&course_plan));
 }
