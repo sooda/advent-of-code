690647d036a35dbaf7b9628fd007f7ccf6381064
Konsta Hölttä <sooda@iki.fi>
2021-12-04 13:17:58 +0200

    generalize bingo for any board size
    Just to practice the new const generics, make the bingo board generic
    over the board size. The squid game logic with its unusual rules is
    still enforced to use the size five.
    
    As instructed by the compiler, an empty-looking bound is required on the
    struct and then the impl: "where [(); N * N]:". Thanks compiler, have to
    go read about that "unconstrained generic constant".

--- 21/4.rs
+++ 21/4.rs
@@ -1,14 +1,25 @@
+#![feature(generic_const_exprs)]
+
 use std::io::{self, BufRead};
 
 #[derive(Clone)]
-struct Board {
-    numbers: [u32; 5 * 5],
-    marked: [bool; 5 * 5],
+struct Board<const N: usize> where [(); N * N]: {
+    numbers: [u32; N * N],
+    marked: [bool; N * N],
 }
 
-impl Board {
-    fn new(numbers: [u32; 5 * 5]) -> Self {
-        Board { numbers: numbers, marked: [false; 5 * 5] }
+impl<const N: usize> Board<N> where [(); N * N]: {
+    fn new(numbers: [u32; N * N]) -> Self {
+        Board { numbers: numbers, marked: [false; N * N] }
+    }
+
+    fn from_string_spec(board_spec: &[String]) -> Self {
+        let mut numbers = [0; N * N];
+        let input = board_spec.iter().flat_map(
+            |spec| spec.split_whitespace().map(|n| n.parse::<u32>().unwrap())
+            );
+        numbers.iter_mut().zip(input).for_each(|(a, i)| *a = i);
+        Board::new(numbers)
     }
 
     fn winning_sum(&self) -> u32 {
@@ -19,15 +30,15 @@ impl Board {
     }
 
     fn row_complete(&self, i: usize) -> bool {
-        self.marked.iter().skip(5 * i).take(5).all(|&x| x)
+        self.marked.iter().skip(N * i).take(N).all(|&x| x)
     }
 
     fn col_complete(&self, i: usize) -> bool {
-        self.marked.chunks(5).map(|c| c[i]).all(|x| x)
+        self.marked.chunks(N).map(|c| c[i]).all(|x| x)
     }
 
     fn one_complete(&self) -> bool {
-        (0..5).any(|i| self.row_complete(i) || self.col_complete(i))
+        (0..N).any(|i| self.row_complete(i) || self.col_complete(i))
     }
 
     fn play_round(&mut self, number: u32) -> Option<u32> {
@@ -45,7 +56,9 @@ impl Board {
 
 }
 
-fn winning_score(boards: &mut [Board], drawn_numbers: &[u32]) -> u32 {
+type SquidBoard = Board<5>;
+
+fn winning_score(boards: &mut [SquidBoard], drawn_numbers: &[u32]) -> u32 {
     for &num in drawn_numbers {
         for board in boards.iter_mut() {
             if let Some(score) = board.play_round(num) {
@@ -56,7 +69,7 @@ fn winning_score(boards: &mut [Board], drawn_numbers: &[u32]) -> u32 {
     panic!("nobody won");
 }
 
-fn squid_score(boards: &mut [Board], drawn_numbers: &[u32]) -> u32 {
+fn squid_score(boards: &mut [SquidBoard], drawn_numbers: &[u32]) -> u32 {
     let mut won_boards: Vec<bool> = boards.iter().map(|_b| false).collect();
     for &num in drawn_numbers {
         for (i, board) in boards.iter_mut().enumerate() {
@@ -71,21 +84,12 @@ fn squid_score(boards: &mut [Board], drawn_numbers: &[u32]) -> u32 {
     panic!("nobody won");
 }
 
-fn parse_board(board_spec: &[String]) -> Board {
-    let mut numbers = [0; 5 * 5];
-    let input = board_spec.iter().flat_map(
-        |spec| spec.split_whitespace().map(|n| n.parse::<u32>().unwrap())
-    );
-    numbers.iter_mut().zip(input).for_each(|(a, i)| *a = i);
-    Board::new(numbers)
-}
-
 fn main() {
     let lines: Vec<String> = io::stdin().lock().lines().map(|l| l.unwrap()).collect();
     let drawn_numbers: Vec<u32> = lines[0].split(',')
         .map(|num| num.parse().unwrap()).collect();
-    let mut boards: Vec<Board> = lines[1..].chunks(6)
-        .map(parse_board)
+    let mut boards: Vec<SquidBoard> = lines[1..].chunks(6)
+        .map(SquidBoard::from_string_spec)
         .collect();
     println!("{}", winning_score(&mut boards.clone(), &drawn_numbers));
     println!("{}", squid_score(&mut boards, &drawn_numbers));
