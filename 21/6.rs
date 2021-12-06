c7c583a42e07b326938a1b6f94be319a13f2c66c
Konsta Hölttä <sooda@iki.fi>
2021-12-06 08:05:52 +0200

    6 lanternfish
    As the spec hints about exponential growth, avoid keeping an entry for
    each fish because their count is going to explode. Because the fish
    lifetimes are integers and fairly limited in terms of growth logic,
    store the amount of fish for each age between 0 and 8 inclusive. During
    each iteration, move the ages - or the time until reproduction - back,
    and sum the reproduced count to the init age.
    
    The copying loop looks like copy_from_slice but something even more
    clever looks possible; this just was easy enough in the morning.

---
+++ 21/6.rs
@@ -0,0 +1,26 @@
+use std::io::{self, BufRead};
+
+fn fish_population(school: &[u64], sim_length: usize) -> u64 {
+    let mut current = [0u64; 9]; // count per age
+    for &age in school {
+        current[age as usize] += 1;
+    }
+    for _ in 0..sim_length {
+        let mut next = [0u64; 9];
+        for (i, &n) in current.iter().enumerate().skip(1) {
+            next[i - 1] = n;
+        }
+        next[8] = current[0];
+        next[6] += current[0];
+        current = next;
+    }
+    current.iter().sum()
+}
+
+fn main() {
+    let input: Vec<u64> = io::stdin().lock().lines()
+        .next().unwrap().unwrap().split(',')
+        .map(|n| n.parse().unwrap())
+        .collect();
+    println!("{}", fish_population(&input, 80));
+}
