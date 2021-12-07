ec60d52556e45f138deec782f7e27f3fab0b00c0
Konsta Hölttä <sooda@iki.fi>
2021-12-07 07:50:03 +0200

    7b crab engineering is not constant
    Do the same exhaustive search as in the first part but with a different
    fuel calculation: nth step costs n fuel, and these add up. The total
    movement cost for a crab is an arithmetic sequence whose sum is easily
    calculated, so that at least avoids one extra summation loop.

--- 21/7.rs
+++ 21/7.rs
@@ -1,5 +1,11 @@
 use std::io::{self, BufRead};
 
+fn arithmetic_seq(n: i32) -> i32 {
+    // series goes from 1 to n
+    let a = 1 + n;
+    (a * a - a) / 2
+}
+
 fn move_cost(crabs: &[i32], destination: i32) -> i32 {
     crabs.iter().map(|c| (c - destination).abs()).sum()
 }
@@ -10,10 +16,21 @@ fn min_fuel(crabs: &[i32]) -> i32 {
     (minx..=maxx).map(|pos| move_cost(crabs, pos)).min().unwrap()
 }
 
+fn move_cost_arith(crabs: &[i32], destination: i32) -> i32 {
+    crabs.iter().map(|c| arithmetic_seq((c - destination).abs())).sum()
+}
+
+fn min_fuel_arith(crabs: &[i32]) -> i32 {
+    let minx: i32 = crabs.iter().copied().min().unwrap();
+    let maxx: i32 = crabs.iter().copied().max().unwrap();
+    (minx..=maxx).map(|pos| move_cost_arith(crabs, pos)).min().unwrap()
+}
+
 fn main() {
     let input: Vec<i32> = io::stdin().lock().lines()
         .next().unwrap().unwrap().split(',')
         .map(|n| n.parse().unwrap())
         .collect();
     println!("{}", min_fuel(&input));
+    println!("{}", min_fuel_arith(&input));
 }
