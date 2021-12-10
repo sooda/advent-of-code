0b4d0611299d70e6b49e297940e3f3584cfc9288
Konsta Hölttä <sooda@iki.fi>
2021-12-10 07:54:10 +0200

    10b incomplete lines
    Repair the incomplete lines by looking up what is left in the line
    opener stack once the line has been consumed: build up a score based on
    what characters should be completed. Get the middle one out of all
    results of the incomplete lines.

--- 21/10.rs
+++ 21/10.rs
@@ -33,8 +33,44 @@ fn syntax_error_score(input: &str) -> u64 {
     0
 }
 
+fn incompleteness_score(input: &str) -> u64 {
+    let mut stack = Vec::new();
+    for ch in input.chars() {
+        match ch {
+            '(' | '[' | '{' | '<' => {
+                stack.push(ch);
+            },
+            ')' | ']' | '}' | '>' => {
+                // guaranteed to match
+                stack.pop().unwrap();
+            }
+            _ => panic!("bad line")
+        };
+    }
+    let score = stack.iter().rev().fold(0, |score, ch| {
+        5 * score + match ch {
+            '(' => 1,
+            '[' => 2,
+            '{' => 3,
+            '<' => 4,
+            _ => unreachable!("this was not pushed")
+        }
+    });
+    score
+}
+
 fn total_syntax_error_score(input: &[String]) -> u64 {
     input.iter().map(|i| syntax_error_score(i)).sum()
+}
+
+fn incompleteness_winner_score(input: &[String]) -> u64 {
+    let mut scores: Vec<_> = input.iter()
+        .filter(|i| syntax_error_score(i) == 0)
+        .map(|i| incompleteness_score(i))
+        .collect();
+    scores.sort_unstable();
+
+    scores[scores.len() / 2]
 }
 
 fn main() {
@@ -42,4 +78,5 @@ fn main() {
         .map(|line| line.unwrap())
         .collect();
     println!("{}", total_syntax_error_score(&lines));
+    println!("{}", incompleteness_winner_score(&lines));
 }
