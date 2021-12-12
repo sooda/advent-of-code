09d4c0b41cb6ba75d913bd3126a3ba8d100a93f0
Konsta Hölttä <sooda@iki.fi>
2021-12-12 23:26:49 +0200

    generalize cave search logic to avoid duplication
    The non-flexible search where there's no time for an extra visit is
    basically the same as the flexible one as if the double visit would have
    been done already. The previously visited node is not actually needed to
    keep, so keep just a boolean flag of whether there's time for one extra
    visit of a small cave. The two searches differ by just the starting
    value of this flag.

--- 21/12.rs
+++ 21/12.rs
@@ -29,51 +29,34 @@ impl Graph {
     }
 }
 
-fn traverse(sys: &Graph, from: &Node, mut visited: HashSet<Node>, paths: &mut usize, level: usize) {
+fn traverse_flexibly(sys: &Graph, from: &Node, mut visited: HashSet<Node>, mut time_for_another: bool, paths: &mut usize, level: usize) {
     //println!("{:-<width$}in {} paths {}", '-', from, paths, width=4*level);
     if from == "end" {
         *paths += 1;
         return;
     }
     if is_small_cave(from) && visited.contains(from) {
-        // visiting small caves more than once is not allowed
-        return;
-    }
-    visited.insert(from.to_owned());
-    for to in sys.edges.get(from).unwrap() {
-        traverse(sys, to, visited.clone(), paths, level + 1);
-    }
-}
-
-fn traverse_flexibly(sys: &Graph, from: &Node, mut visited: HashSet<Node>, mut twice_visited: Option<Node>, paths: &mut usize, level: usize) {
-    //println!("{:-<width$}in {} paths {}", '-', from, paths, width=4*level);
-    if from == "end" {
-        *paths += 1;
-        return;
-    }
-    if is_small_cave(from) && visited.contains(from) {
-        if twice_visited.is_some() || from == "start" {
-            // visiting just one small cave except start twice is allowed
+        if !time_for_another || from == "start" {
+            // visiting just one small cave except start twice may be allowed
             return;
-        } else {
-            twice_visited = Some(from.clone());
         }
+        time_for_another = false;
     }
     visited.insert(from.to_owned());
     for to in sys.edges.get(from).unwrap() {
-        traverse_flexibly(sys, to, visited.clone(), twice_visited.clone(), paths, level + 1);
+        traverse_flexibly(sys, to, visited.clone(), time_for_another, paths, level + 1);
     }
 }
 
 fn suitable_paths(sys: &Graph) -> usize {
     let mut paths = 0;
-    traverse(sys, &"start".to_string(), HashSet::new(), &mut paths, 0);
+    traverse_flexibly(sys, &"start".to_string(), HashSet::new(), false, &mut paths, 0);
     paths
 }
 
 fn suitable_paths_one_double(sys: &Graph) -> usize {
     let mut paths = 0;
-    traverse_flexibly(sys, &"start".to_string(), HashSet::new(), None, &mut paths, 0);
+    traverse_flexibly(sys, &"start".to_string(), HashSet::new(), true, &mut paths, 0);
     paths
 }
 
