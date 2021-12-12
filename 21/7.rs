ff1b44a33889b7c734cf3313d40b86d391eae99f
Konsta Hölttä <sooda@iki.fi>
2021-12-12 22:08:23 +0200

    generalize crab fuel search
    Tidy up the two attempts by moving common code to a generic function
    that takes the move cost computation function as an input.

--- 21/7.rs
+++ 21/7.rs
@@ -1,19 +1,24 @@
 use std::io::{self, BufRead};
 
-fn arithmetic_seq(n: i32) -> i32 {
-    // series goes from 1 to n
-    let a = 1 + n;
-    (a * a - a) / 2
+fn min_fuel_search<F>(crabs: &[i32], move_cost: F) -> i32
+where F: Fn(&[i32], i32) -> i32 {
+    let minx: i32 = crabs.iter().copied().min().unwrap();
+    let maxx: i32 = crabs.iter().copied().max().unwrap();
+    (minx..=maxx).map(|pos| move_cost(crabs, pos)).min().unwrap()
 }
 
-fn move_cost(crabs: &[i32], destination: i32) -> i32 {
+fn move_cost_simple(crabs: &[i32], destination: i32) -> i32 {
     crabs.iter().map(|c| (c - destination).abs()).sum()
 }
 
 fn min_fuel(crabs: &[i32]) -> i32 {
-    let minx: i32 = crabs.iter().copied().min().unwrap();
-    let maxx: i32 = crabs.iter().copied().max().unwrap();
-    (minx..=maxx).map(|pos| move_cost(crabs, pos)).min().unwrap()
+    min_fuel_search(crabs, move_cost_simple)
+}
+
+fn arithmetic_seq(n: i32) -> i32 {
+    // series goes from 1 to n
+    let a = 1 + n;
+    (a * a - a) / 2
 }
 
 fn move_cost_arith(crabs: &[i32], destination: i32) -> i32 {
@@ -21,9 +26,7 @@ fn move_cost_arith(crabs: &[i32], destination: i32) -> i32 {
 }
 
 fn min_fuel_arith(crabs: &[i32]) -> i32 {
-    let minx: i32 = crabs.iter().copied().min().unwrap();
-    let maxx: i32 = crabs.iter().copied().max().unwrap();
-    (minx..=maxx).map(|pos| move_cost_arith(crabs, pos)).min().unwrap()
+    min_fuel_search(crabs, move_cost_arith)
 }
 
 fn main() {
