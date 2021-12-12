fa7409b75f54294e1d8074c558353154caf387ef
Konsta Hölttä <sooda@iki.fi>
2021-12-12 10:57:27 +0200

    12b time to visit one twice
    Now a single small cave can be visited twice. Duplicate the search logic
    for these more flexible rules; keep an additional state of which node
    has been visited twice, if any.

--- 21/12.rs
+++ 21/12.rs
@@ -45,9 +45,35 @@ fn traverse(sys: &Graph, from: &Node, mut visited: HashSet<Node>, paths: &mut us
     }
 }
 
+fn traverse_flexibly(sys: &Graph, from: &Node, mut visited: HashSet<Node>, mut twice_visited: Option<Node>, paths: &mut usize, level: usize) {
+    //println!("{:-<width$}in {} paths {}", '-', from, paths, width=4*level);
+    if from == "end" {
+        *paths += 1;
+        return;
+    }
+    if is_small_cave(from) && visited.contains(from) {
+        if twice_visited.is_some() || from == "start" {
+            // visiting just one small cave except start twice is allowed
+            return;
+        } else {
+            twice_visited = Some(from.clone());
+        }
+    }
+    visited.insert(from.to_owned());
+    for to in sys.edges.get(from).unwrap() {
+        traverse_flexibly(sys, to, visited.clone(), twice_visited.clone(), paths, level + 1);
+    }
+}
+
 fn suitable_paths(sys: &Graph) -> usize {
     let mut paths = 0;
     traverse(sys, &"start".to_string(), HashSet::new(), &mut paths, 0);
+    paths
+}
+
+fn suitable_paths_one_double(sys: &Graph) -> usize {
+    let mut paths = 0;
+    traverse_flexibly(sys, &"start".to_string(), HashSet::new(), None, &mut paths, 0);
     paths
 }
 
@@ -57,4 +83,5 @@ fn main() {
         .collect();
     let sys = Graph::from_spec(&spec);
     println!("{:?}", suitable_paths(&sys));
+    println!("{:?}", suitable_paths_one_double(&sys));
 }
