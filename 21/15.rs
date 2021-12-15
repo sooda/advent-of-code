7c42a70cbe2bba541850625217f94e3a2084c804
Konsta Hölttä <sooda@iki.fi>
2021-12-15 08:33:54 +0200

    15b cave is five^2 times larger
    The original scanned area repeats in a consistent way, so build the real
    map from the scan by inserting the five by five coordinates from each
    scanned coordinate and then use the same search algorithm with this map.
    The decision to use a hashmap now really pays off because indexing a vec
    of vecs would again be rather unpleasant even though the map is dense
    and hashing dense coordinates is a bit silly.
    
    The four level flat_map is not the most readable when compared to plain
    nested for loops, but at least it's fun practice. Collecting a hashmap
    from an iterator is very convenient.

--- 21/15.rs
+++ 21/15.rs
@@ -43,9 +43,36 @@ fn total_risk(chitons: &[Vec<u8>]) -> i32 {
     chiton_dijkstra(&map, (0, 0), ((chitons[0].len() - 1) as i32, (chitons.len() - 1) as i32))
 }
 
+fn wrap_chiton_risk(tilex: i32, tiley: i32, risk: i32) -> i32 {
+    let internal_risk = risk - 1; // from 1..9 to 0..8
+    let wrapped = (internal_risk + tilex + tiley) % 9;
+    1 + wrapped // back to 1..9 again
+}
+
+fn total_risk_full(chitons: &[Vec<u8>]) -> i32 {
+    let w = chitons[0].len() as i32;
+    let h = chitons.len() as i32;
+    let map: HashMap<(i32, i32), i32> = chitons
+        .iter().enumerate().flat_map(|(y, row)| {
+            row.iter().enumerate().flat_map(move |(x, &ch)| {
+                (0..5).flat_map(move |tiley| {
+                    (0..5).map(move |tilex| {
+                        (
+                            (w * tilex + (x as i32), h * tiley + (y as i32)),
+                            wrap_chiton_risk(tilex, tiley, ch as i32)
+                        )
+                    })
+                })
+            })
+        })
+    .collect();
+    chiton_dijkstra(&map, (0, 0), (5 * w - 1, 5 * h - 1))
+}
+
 fn main() {
     let chitons: Vec<Vec<u8>> = io::stdin().lock().lines()
         .map(|line| line.unwrap().bytes().map(|b| b - b'0').collect())
         .collect();
     println!("{:?}", total_risk(&chitons));
+    println!("{:?}", total_risk_full(&chitons));
 }
