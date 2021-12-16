24f54025ea9ce7cd9575279842de4c5b2f1a86f0
Konsta Hölttä <sooda@iki.fi>
2021-12-16 09:04:03 +0200

    16b calculate the transmission expression
    Copy the recursion but now keep computing the value of the
    subexpressions. Fill in the operator kinds and execute them with the
    values collected from the subpackets.
    
    The subpacket values are collected in vectors which might be wasteful in
    allocations when an iterative computation might also work, but this is
    more ergonomic for now and perf does not really matter.
    
    This is complex enough to look like a reusable piece of a machine to be
    used in a later day like the VM stuff from previous years; fingers
    crossed, as that would be fun.

--- 21/16.rs
+++ 21/16.rs
@@ -56,11 +56,9 @@ impl<'a> Bitstream<'a> {
 fn version_sum_operator_bitnum(stream: &mut Bitstream) -> u64 {
     let bitnum = stream.extract_bits(15);
     let mut sum = 0;
-    {
-        let mut substream = stream.substream(bitnum as usize);
-        while substream.available() {
-            sum += version_sum_recursive(&mut substream);
-        }
+    let mut substream = stream.substream(bitnum as usize);
+    while substream.available() {
+        sum += version_sum_recursive(&mut substream);
     }
     sum
 }
@@ -106,10 +104,79 @@ fn version_sum(bits: &[u8]) -> u64 {
     version_sum_recursive(&mut stream)
 }
 
+fn values_operator_bitnum(stream: &mut Bitstream) -> Vec<u64> {
+    let bitnum = stream.extract_bits(15);
+    let mut values = Vec::new();
+    let mut substream = stream.substream(bitnum as usize);
+    while substream.available() {
+        values.push(compute_recursive(&mut substream));
+    }
+    values
+}
+
+fn values_operator_pktcount(stream: &mut Bitstream) -> Vec<u64> {
+    let pktcount = stream.extract_bits(11);
+    let mut values = Vec::new();
+    for _ in 0..pktcount {
+        values.push(compute_recursive(stream));
+    }
+    values
+}
+
+fn values_operator(stream: &mut Bitstream) -> Vec<u64> {
+    let length_type_id = stream.extract_bits(1);
+    match length_type_id {
+        0 => values_operator_bitnum(stream),
+        1 => values_operator_pktcount(stream),
+        _ => unreachable!()
+    }
+}
+
+fn value_literal(stream: &mut Bitstream) -> u64 {
+    let mut val = 0;
+    loop {
+        let group = stream.extract_bits(5);
+        val <<= 4;
+        val |= group & 0b1111;
+        if (group & 0b10000) == 0 {
+            break;
+        }
+    }
+    val
+}
+
+fn compute_recursive(stream: &mut Bitstream) -> u64 {
+    let _ver = stream.extract_bits(3);
+    let type_id = stream.extract_bits(3);
+    match type_id {
+        4 => value_literal(stream),
+        0 | 1 | 2 | 3 | 5 | 6 | 7 => {
+            let values = values_operator(stream);
+            match type_id {
+                0 => values.iter().sum(),
+                1 => values.iter().product(),
+                2 => *values.iter().min().unwrap(),
+                3 => *values.iter().max().unwrap(),
+                5 => (values[0] > values[1]) as u64,
+                6 => (values[0] < values[1]) as u64,
+                7 => (values[0] == values[1]) as u64,
+                _ => unreachable!()
+            }
+        },
+        _ => unreachable!()
+    }
+}
+
+fn compute(bits: &[u8]) -> u64 {
+    let mut stream = Bitstream::new(bits);
+    compute_recursive(&mut stream)
+}
+
 fn main() {
     let bits: Vec<u8> = io::stdin().lock().lines()
         .next().unwrap().unwrap()
         .chars().map(|b| "0123456789ABCDEF".chars().position(|c| c == b).unwrap() as u8) // or from_str_radix
         .collect();
     println!("{}", version_sum(&bits));
+    println!("{}", compute(&bits));
 }
