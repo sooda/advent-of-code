5537c4e1d0982e8db32c054d0ae4221680a30d9c
Konsta Hölttä <sooda@iki.fi>
2021-12-19 12:16:09 +0200

    19 beacon scanner
    The scanners are excellent beacon locators but do not locate themselves.
    Take the first scanner as the one true coordinate system and compare
    each of the remaining scanners to it, rotated in every possible
    orientation. When the orientation happens to match that of the master
    coordinate system, the points look identical but the set is just shifted
    somewhere based on where the new scanner is. When the scanners see a
    large enough shared subset of points, any of those shared points can be
    chosen as a common origin to which each other point can be referenced.
    The subset is not known in advance, so try every possible pair of origin
    beacons in the master set and the candidate neighbor set. When a match
    is found, transform the new points back to the master scanner.
    
    For now this is terribly inefficient because the results are not cached
    in any way and the same calculation needs to repeat over and over.

---
+++ 21/19.rs
@@ -0,0 +1,141 @@
+use std::io::{self, BufRead};
+use std::collections::HashSet;
+
+type Coord = (i32, i32, i32);
+
+fn csub(a: Coord, b: Coord) -> Coord {
+    (a.0 - b.0, a.1 - b.1, a.2 - b.2)
+}
+
+fn cadd(a: Coord, b: Coord) -> Coord {
+    (a.0 + b.0, a.1 + b.1, a.2 + b.2)
+}
+
+// a lot of technically possible combinations in total, but mirroring is not allowed
+fn orientation_permutations(report: &HashSet<Coord>) -> Vec<HashSet<Coord>> {
+    // rotation: ccw across z
+    let turn1 = |(x, y, z): (i32, i32, i32)| (-y,  x,  z); // roll left
+    let turn2 = |(x, y, z): (i32, i32, i32)| (-x, -y,  z); // roll upside down
+    let turn3 = |(x, y, z): (i32, i32, i32)| ( y, -x,  z); // roll right
+
+    // facing direction
+    let left  = |(x, y, z): (i32, i32, i32)| ( z,  y, -x); // pan left
+    let back  = |(x, y, z): (i32, i32, i32)| (-x,  y, -z); // behind you
+    let right = |(x, y, z): (i32, i32, i32)| (-z,  y,  x); // pan right
+    let up    = |(x, y, z): (i32, i32, i32)| ( x, -z,  y); // tilt up
+    let down  = |(x, y, z): (i32, i32, i32)| ( x,  z, -y); // tilt down
+    let permutators: &[Box<dyn Fn((i32, i32, i32)) -> (i32, i32, i32)>] = &[
+        Box::new(|p| p),
+        Box::new(|p| turn1(p)),
+        Box::new(|p| turn2(p)),
+        Box::new(|p| turn3(p)),
+
+        Box::new(|p| left(p)),
+        Box::new(|p| turn1(left(p))),
+        Box::new(|p| turn2(left(p))),
+        Box::new(|p| turn3(left(p))),
+
+        Box::new(|p| back(p)),
+        Box::new(|p| turn1(back(p))),
+        Box::new(|p| turn2(back(p))),
+        Box::new(|p| turn3(back(p))),
+
+        Box::new(|p| right(p)),
+        Box::new(|p| turn1(right(p))),
+        Box::new(|p| turn2(right(p))),
+        Box::new(|p| turn3(right(p))),
+
+        Box::new(|p| up(p)),
+        Box::new(|p| turn1(up(p))),
+        Box::new(|p| turn2(up(p))),
+        Box::new(|p| turn3(up(p))),
+
+        Box::new(|p| down(p)),
+        Box::new(|p| turn1(down(p))),
+        Box::new(|p| turn2(down(p))),
+        Box::new(|p| turn3(down(p))),
+    ];
+
+    permutators.iter().map(|f| {
+        report.iter().map(|&pt| f(pt)).collect()
+    }).collect()
+}
+
+fn relative_transform(report: &HashSet<Coord>, origin: Coord) -> HashSet<Coord> {
+    report.iter().map(|&c| csub(c, origin)).collect()
+}
+
+fn try_match(arep: &HashSet<Coord>, brep: &HashSet<Coord>) -> Option<HashSet<Coord>> {
+    // TODO: less repetition, cache all the transforms and then intersect all
+    let breps = orientation_permutations(brep);
+    for &attempted_origin_a in arep {
+        let arep_transformed = relative_transform(&arep, attempted_origin_a);
+        for brep in &breps {
+            for &attempted_origin_b in brep {
+                let brep_transformed = relative_transform(&brep, attempted_origin_b);
+                let shared_pts = arep_transformed.intersection(&brep_transformed).count();
+                if shared_pts >= 12 {
+                    let result = brep_transformed.into_iter().map(|p| cadd(attempted_origin_a, p)).collect();
+                    println!("{} pts. b must be {:?} btw", shared_pts, csub(attempted_origin_b, attempted_origin_a));
+                    return Some(result);
+                }
+            }
+        }
+    }
+    None
+}
+
+fn find_match(master: &HashSet<Coord>, reports: &Vec<HashSet<Coord>>) -> Option<(usize, HashSet<Coord>)> {
+    reports.iter()
+        .enumerate()
+        .filter_map(|(i, report)| {
+            try_match(master, report).map(|found| (i, found))
+        })
+        .next()
+}
+
+fn apply_match(master: &HashSet<Coord>, reports: &mut Vec<HashSet<Coord>>) -> Option<HashSet<Coord>> {
+    if let Some((i, found)) = find_match(master, reports) {
+        reports.remove(i);
+        Some(found)
+    } else {
+        None
+    }
+}
+
+fn total_beacon_count(mut reports: Vec<HashSet<Coord>>) -> usize {
+    let mut master_reference = reports.remove(0);
+    while let Some(found) = apply_match(&master_reference, &mut reports) {
+        master_reference.extend(found.into_iter());
+    }
+    assert!(reports.is_empty());
+    master_reference.len()
+}
+
+fn parse_reports(reports: &[String]) -> Vec<HashSet<Coord>> {
+    // --- scanner 0 ---
+    // -809,-750,623
+    // -853,-746,517
+    // ...
+    reports
+        .split(|l| l == "")
+        // ignore the header
+        .map(|report| report.iter().skip(1).map(|line| {
+            let mut pts = line.split(',');
+            (
+                pts.next().unwrap().parse().unwrap(),
+                pts.next().unwrap().parse().unwrap(),
+                pts.next().unwrap().parse().unwrap(),
+            )
+        }).collect())
+        .collect()
+}
+
+fn main() {
+    // TODO use iterator directly, not vec
+    let raw_reports: Vec<_> = io::stdin().lock().lines()
+        .map(|line| line.unwrap())
+        .collect();
+    let reports = parse_reports(&raw_reports);
+    println!("{:?}", total_beacon_count(reports));
+}
