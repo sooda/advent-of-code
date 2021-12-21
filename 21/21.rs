29b3a39a870e48f7f58f72f356faa89ca7642fd2
Konsta Hölttä <sooda@iki.fi>
2021-12-21 23:10:14 +0200

    21b play the real game
    Preparing for changes by keeping the game loop simple was not effective,
    as the game is now entirely different as each die roll triggers new
    universes and the puzzle calls for the win counts only.
    
    Keep the win counts memoized in a map of states: from the begin state
    with the players in some positions, new game states emerge until the
    game ends at a score of 21 on either player, which is achieved in just a
    few rounds. This forms a recursion that ends at the game end, and
    produces fibonacci-like repeating subsearches whose results can be
    memoized. The entire state is only 10 * 10 * 21 * 21 position and score
    combinations or so, and that loops through fairly quickly.
    
    The computation for each player is done separately which is a bit silly;
    indexing by player id would make this much more succinct.
    
    I spent ("wasted") some good amount of time analyzing how the universes
    could actually be enumerated such that each "universe ID" would encode
    the entire history of the game in base-3 trits of die rolls, so each
    universe knows what the game state is. The game history is not important
    for the result though, but that felt interesting to look at and it
    looked vaguely similar to z-order curves.

--- 21/21.rs
+++ 21/21.rs
@@ -1,4 +1,5 @@
 use std::io::{self, BufRead};
+use std::collections::HashMap;
 
 fn play_game(mut apos: i32, mut bpos: i32) -> i32 {
     // move to zero-based indexing
@@ -31,9 +32,68 @@ fn play_game(mut apos: i32, mut bpos: i32) -> i32 {
     unreachable!()
 }
 
+// positions and scores and player one turn
+type GameState = (i32, i32, i32, i32, bool);
+type GameStates = HashMap::<GameState, (usize, usize)>;
+
+fn dirac_recurse(start_state: GameState, states: &mut GameStates) -> (usize, usize) {
+    if let Some(x) = states.get(&start_state) {
+        return *x;
+    }
+    let mut wins_a = 0;
+    let mut wins_b = 0;
+    for d0 in 1..=3 {
+        for d1 in 1..=3 {
+            for d2 in 1..=3 {
+                let mut state = start_state;
+                let die = d0 + d1 + d2;
+                if state.4 {
+                    let pos = (state.0 + die) % 10;
+                    let score = state.2 + 1 + pos;
+                    if score >= 21 {
+                        wins_a += 1;
+                    } else {
+                        state.0 = pos;
+                        state.2 = score;
+                        state.4 = !state.4;
+                        let subwins = dirac_recurse(state, states);
+                        wins_a += subwins.0;
+                        wins_b += subwins.1;
+                    }
+                } else {
+                    let pos = (state.1 + die) % 10;
+                    let score = state.3 + 1 + pos;
+                    if score >= 21 {
+                        wins_b += 1;
+                    } else {
+                        state.1 = pos;
+                        state.3 = score;
+                        state.4 = !state.4;
+                        let subwins = dirac_recurse(state, states);
+                        wins_a += subwins.0;
+                        wins_b += subwins.1;
+                    }
+                }
+            }
+        }
+    }
+    states.insert(start_state, (wins_a, wins_b));
+    (wins_a, wins_b)
+}
+
+
+fn play_dirac_game(apos: i32, bpos: i32) -> usize {
+    // (apos, bpos, ascore, bscore, aturn)
+    // max lut size of 10 * 10 * 21 * 21 = 44100 elements
+    let mut mem = GameStates::new();
+    let wins = dirac_recurse((apos - 1, bpos - 1, 0, 0, true), &mut mem);
+    wins.0.max(wins.1)
+}
+
 fn main() {
     let positions: Vec<i32> = io::stdin().lock().lines()
         .map(|line| line.unwrap().split("position: ").skip(1).next().unwrap().parse().unwrap())
         .collect();
     println!("{:?}", play_game(positions[0], positions[1]));
+    println!("{:?}", play_dirac_game(positions[0], positions[1]));
 }
