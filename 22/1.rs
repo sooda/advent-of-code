de7ad17124a9a1997a982a2b17f470a601b2cf3a
Konsta Hölttä <sooda@iki.fi>
2022-12-01 07:28:44 +0200

    1b the most snacky ones run out first
    With some odd elf math, the top three food carriers might run out. Maybe
    those elves are just Obelix. Sort the food sums to find the top three
    and sum them.

--- 22/1.rs
+++ 22/1.rs
@@ -6,9 +6,19 @@ fn max_cals(calories_spec: &[String]) -> usize {
     }).max().unwrap()
 }
 
+fn tot_top_three_cals(calories_spec: &[String]) -> usize {
+    let mut tot_cals: Vec<usize> = calories_spec.split(|cs| cs == "").map(|cals| {
+        cals.iter().map(|c| c.parse::<usize>().unwrap()).sum()
+    }).collect();
+    tot_cals.sort_unstable_by(|a, b| b.cmp(a));
+
+    tot_cals[0] + tot_cals[1] + tot_cals[2]
+}
+
 fn main() {
     let calories_spec: Vec<String> = io::stdin().lock().lines()
         .map(|line| line.unwrap())
         .collect();
     println!("{}", max_cals(&calories_spec));
+    println!("{}", tot_top_three_cals(&calories_spec));
 }
