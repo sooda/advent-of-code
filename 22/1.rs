cd2772321917059c41d618a46e01c7e9196dbcde
Konsta Hölttä <sooda@iki.fi>
2022-12-01 08:13:34 +0200

    1: precollect total calories
    The individual calorie counts of foodstuffs is not important in either
    part of the puzzle.
    
    The split_iter crate could be useful here, btw.
    
    Another interesting way to handle the input would be to parse() them to
    Option<usize> in main(), and then split by None for totals.

--- 22/1.rs
+++ 22/1.rs
@@ -1,24 +1,26 @@
 use std::io::{self, BufRead};
 
-fn max_cals(calories_spec: &[String]) -> usize {
+fn calorie_totals(calories_spec: &[String]) -> Vec<usize> {
     calories_spec.split(|cs| cs == "").map(|cals| {
         cals.iter().map(|c| c.parse::<usize>().unwrap()).sum()
-    }).max().unwrap()
+    }).collect()
 }
 
-fn tot_top_three_cals(calories_spec: &[String]) -> usize {
-    let mut tot_cals: Vec<usize> = calories_spec.split(|cs| cs == "").map(|cals| {
-        cals.iter().map(|c| c.parse::<usize>().unwrap()).sum()
-    }).collect();
-    tot_cals.sort_unstable_by(|a, b| b.cmp(a));
+fn max_cals(cal_totals: &[usize]) -> usize {
+    *cal_totals.iter().max().unwrap()
+}
 
-    tot_cals[0] + tot_cals[1] + tot_cals[2]
+fn tot_top_three_cals(mut cal_totals: Vec<usize>) -> usize {
+    cal_totals.sort_unstable_by(|a, b| b.cmp(a));
+
+    cal_totals[0] + cal_totals[1] + cal_totals[2]
 }
 
 fn main() {
     let calories_spec: Vec<String> = io::stdin().lock().lines()
         .map(|line| line.unwrap())
         .collect();
-    println!("{}", max_cals(&calories_spec));
-    println!("{}", tot_top_three_cals(&calories_spec));
+    let elf_totals = calorie_totals(&calories_spec);
+    println!("{}", max_cals(&elf_totals));
+    println!("{}", tot_top_three_cals(elf_totals));
 }
