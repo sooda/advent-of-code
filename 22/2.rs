cce5428943aa1da1e5eadc375112ab92f209fb6f
Konsta Hölttä <sooda@iki.fi>
2022-12-02 08:28:28 +0200

    2: get clever with modulos
    The game can end in three ways and the modulo by three gets us just that
    with no branches, with the right offset applied. The first part outcome
    is the difference between two hands, and the second part hand is the
    other hand plus the outcome. Keep the old code as explicit documentation
    of what happens.

--- 22/2.rs
+++ 22/2.rs
@@ -3,15 +3,19 @@ use std::io::{self, BufRead};
 type Rps = i32;
 
 fn play_game_exactly(a: Rps, b: Rps) -> i32 {
-    let shape = b + 1;
-    // this could be branchless more modulo magic
-    let outcome = match (b - a + 3) % 3 {
-        2 => 0, // lost
-        0 => 3, // equal
-        1 => 6, // won
-        _ => panic!()
-    };
-    shape + outcome
+    let shape_score = b + 1;
+    if false {
+        let _outcome = match (b - a + 3) % 3 {
+            2 => 0, // lost
+            0 => 3, // equal
+            1 => 6, // won
+            _ => panic!()
+        };
+    }
+    // rotate the above by 1
+    let outcome = (b - a + 1 + 3) % 3;
+    let outcome_score = 3 * outcome;
+    shape_score + outcome_score
 }
 
 fn play_games_exactly(games: &[(Rps, Rps)]) -> i32 {
@@ -19,13 +23,19 @@ fn play_games_exactly(games: &[(Rps, Rps)]) -> i32 {
 }
 
 fn play_game_by_plan(a: Rps, b: Rps) -> i32 {
-    let (outcome, my_shape) = match b {
-        0 => (0, (a + 2) % 3), // lost
-        1 => (3, a), // equal
-        2 => (6, (a + 1) % 3), // won
-        _ => panic!()
-    };
-    1 + my_shape + outcome
+    let outcome_score = 3 * b;
+    if false {
+        let (_outcome, _my_shape) = match b {
+            0 => (0, (a + 2) % 3), // lost
+            1 => (3, a), // equal
+            2 => (6, (a + 1) % 3), // won
+            _ => panic!()
+        };
+    }
+    // rotate the above by 2
+    let my_shape = (a + b + 2 + 3) % 3;
+    let shape_score = my_shape + 1;
+    shape_score + outcome_score
 }
 
 fn play_games_by_plan(games: &[(Rps, Rps)]) -> i32 {
@@ -33,6 +43,7 @@ fn play_games_by_plan(games: &[(Rps, Rps)]) -> i32 {
 }
 
 // 0 for rock, 1 for paper, 2 for scissors
+// or 0 for lose, 1 for draw, 2 for win
 fn decode_rps(ch: u8) -> Rps {
     Rps::from(match ch {
         b'A'..=b'C' => ch - b'A',
