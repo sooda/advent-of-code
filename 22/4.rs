b6ee8cc7ab88690ddcf9ef7fd6fc4f11e58c1371
Konsta Hölttä <sooda@iki.fi>
2022-12-04 07:26:24 +0200

    4 camp cleanup
    Count how many pairs of integer ranges fully overlap, i.e., one of the
    pair contains the other range entirely.

---
+++ 22/4.rs
@@ -0,0 +1,28 @@
+use std::io::{self, BufRead};
+
+type Sects = (i32, i32);
+type Assignment = (Sects, Sects);
+
+// a contains b
+fn fully_contains(a: Sects, b: Sects) -> bool {
+    return b.0 >= a.0 && b.1 <= a.1
+}
+
+fn num_fully_contained(sectss: &[Assignment]) -> usize {
+    sectss.iter().filter(|sects| {
+        fully_contains(sects.0, sects.1) || fully_contains(sects.1, sects.0)
+    }).count()
+}
+
+fn parse_assignments(line: &str) -> Assignment {
+    let mut sp = line.split(&['-', ',']).map(|x| x.parse().unwrap());
+    ((sp.next().unwrap(), sp.next().unwrap()),
+     (sp.next().unwrap(), sp.next().unwrap()))
+}
+
+fn main() {
+    let section_assignments: Vec<_> = io::stdin().lock().lines()
+        .map(|line| parse_assignments(&line.unwrap()))
+        .collect();
+    println!("{}", num_fully_contained(&section_assignments));
+}
