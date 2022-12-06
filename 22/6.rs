e5e8d408b3beb300b62f0f98d05418ccf7d8874c
Konsta Hölttä <sooda@iki.fi>
2022-12-06 07:13:12 +0200

    6b also look for messages
    Packet start marker was just four, but now a start of message is marked
    by fourteen unique characters. That's as easy in source code as before,
    just make the marker length a parameter.
    
    This would be not very efficient for long messages and long markers
    though, as the hashset is rebuilt from almost identical input for each
    step. A better way could be to scan(), maintaining a (hash)map that is
    popped and pushed for the exiting and entering characters.

--- 22/6.rs
+++ 22/6.rs
@@ -1,13 +1,14 @@
 use std::io::{self, BufRead};
 use std::collections::HashSet;
 
-fn marker_offset(datastream: &[u8]) -> usize {
-    datastream.windows(4).position(|w| {
-        w.iter().copied().collect::<HashSet<_>>().len() == 4
-    }).unwrap() + 4
+fn marker_offset(datastream: &[u8], marker_len: usize) -> usize {
+    datastream.windows(marker_len).position(|w| {
+        w.iter().copied().collect::<HashSet<_>>().len() == marker_len
+    }).unwrap() + marker_len
 }
 
 fn main() {
     let datastream: String = io::stdin().lock().lines().next().unwrap().unwrap();
-    println!("{}", marker_offset(datastream.as_bytes()));
+    println!("{}", marker_offset(datastream.as_bytes(), 4));
+    println!("{}", marker_offset(datastream.as_bytes(), 14));
 }
