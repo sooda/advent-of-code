5a4efcdd259ba81e251258e42ee8f4ef5d364af3
Konsta Hölttä <sooda@iki.fi>
2022-12-07 10:38:19 +0200

    7b choose a directory to delete
    The original tree traversal isn't that good anymore so make a more
    generic one in postorder to look at sizes. Compute the total size and
    the size of each directory of suitable size (this time with no nested
    duplicates) and then look up the smallest of the suitable deletion
    candidates.
    
    This also got redesigned somewhat while being implemented, so it'll need
    another cleanup iteration later on, but at least the result is correct.

--- 22/7.rs
+++ 22/7.rs
@@ -27,6 +27,42 @@ fn simple_compute_dirs(entry: &Entry, max: usize) -> usize {
     total
 }
 
+fn walk_fs_tree<F: FnMut(&Entry, usize)>(entry: &Entry, visit: &mut F) -> usize {
+    let size = match entry {
+        Entry::Dir(_, contents) => {
+            // contents.borrow().iter().map(&mut |e| walk_fs_tree(e, visit)).sum(),
+            let mut s = 0;
+            for e in contents.borrow().iter() {
+                s += walk_fs_tree(e, visit);
+            }
+            s
+        }
+        Entry::File(size) => *size,
+    };
+    visit(entry, size);
+    size
+}
+
+fn smallest_to_delete(root: &Entry, capacity: usize, need: usize) -> usize {
+    let mut space_used = 0;
+    // an iterator over the tree would be better though but it's harder to implement
+    walk_fs_tree(root, &mut |entry, _| {
+        match entry {
+            Entry::File(sz) => space_used += sz,
+            _ => (),
+        }
+    });
+    let free_space = capacity - space_used;
+    let mut deletion_candidate_sizes = Vec::new();
+    walk_fs_tree(root, &mut |entry, size| {
+        match entry {
+            Entry::Dir(..) => if free_space + size >= need { deletion_candidate_sizes.push(size) },
+            _ => (),
+        }
+    });
+    *deletion_candidate_sizes.iter().min().unwrap()
+}
+
 fn parse_listing(lines: &[String]) -> Entry {
     let root_dir = Rc::new(RefCell::new(Vec::new()));
     let mut visitstack = Vec::new();
@@ -81,4 +117,5 @@ fn main() {
         .collect();
     let fs = parse_listing(&terminal_listing);
     println!("{}", simple_compute_dirs(&fs, 100000));
+    println!("{}", smallest_to_delete(&fs, 70000000, 30000000));
 }
