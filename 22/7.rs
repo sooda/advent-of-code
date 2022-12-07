69519199f829973b1afc2b206a52b8df6174b7b7
Konsta Hölttä <sooda@iki.fi>
2022-12-07 22:52:58 +0200

    7: practice tree traversal iterators
    While the visit pattern is easy to see and recurse with, it needs that
    clumsy outer state for size sums. Implement post-order tree traversal
    with the Iterator trait mainly for fun and technical practice. This
    iterator yields directory sizes from files and subdirectories within.
    
    The iterator is implemented with an internal stack. Three types of
    entries mark proceeding and completed sub-size calculation operations
    and sentinels for propagating a completed size into the iterator
    interface.
    
    The stack unfortunately does mean various allocations during traversal.
    Recursion involves an implicit traversal stack too, but that happens on
    the thread stack which is simpler than heap reallocation.
    
    With some further work, this could be made into a more generic tree
    traversal "library"; for now the size operations are hardcoded.
    Generality might be achieved cleanly e.g. by using an internal scan or
    fold iterator provided as a type parameter, or just with some closures.

--- 22/7.rs
+++ 22/7.rs
@@ -1,3 +1,5 @@
+#![feature(cell_leak)]
+
 use std::io::{self, BufRead};
 use std::cell::RefCell;
 use std::rc::Rc;
@@ -10,7 +12,92 @@ enum Entry {
     File(usize),
 }
 
-// an iterator over the tree would be better though but it's harder to implement
+// a tree structure is "flattened" into these
+#[derive(Clone, Copy)]
+enum EntryVisit<'a> {
+    // marker: a dir entry has been traversed when this pops
+    Visiting(usize),
+    // an entry to be traversed
+    Incoming(&'a Entry),
+    // one result from traversing an entry (dir or file) contributing to parent visit
+    ExitResult(usize),
+}
+
+// just directory entry size iter though, file entries are skipped
+struct EntrySizeIter<'a> {
+    // O(log n) space, and storage reused for next subtrees
+    stack: Vec<EntryVisit<'a>>,
+}
+
+impl Entry {
+    fn iter_dir_sizes<'a>(&'a self) -> EntrySizeIter<'a> {
+        match self {
+            Entry::Dir(..) => {
+                EntrySizeIter {
+                    // no preceding Visiting here to avoid a spurious root^2
+                    stack: vec![ EntryVisit::Incoming(self) ],
+                }
+            },
+            _ => {
+                // could also return empty iterator for files,
+                // but that would be dead code here
+                panic!("only dirs can be traversed");
+            }
+        }
+    }
+}
+
+impl<'a> Iterator for EntrySizeIter<'a> {
+    type Item = usize;
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            match self.stack.pop() {
+                Some(EntryVisit::Visiting(size)) => {
+                    // for parent directory, if any
+                    self.stack.push(EntryVisit::ExitResult(size));
+                    return Some(size);
+                },
+                Some(EntryVisit::Incoming(&Entry::File(size))) => {
+                    self.stack.push(EntryVisit::ExitResult(size));
+                },
+                Some(res @ EntryVisit::ExitResult(size)) => {
+                    let earlier = self.stack.pop();
+                    match earlier {
+                        Some(EntryVisit::ExitResult(esize)) => {
+                            self.stack.push(EntryVisit::ExitResult(size + esize));
+                        },
+                        // vsize should be 0 though, could return here already?
+                        Some(EntryVisit::Visiting(vsize)) => {
+                            // returned in next iteration
+                            self.stack.push(EntryVisit::Visiting(size + vsize));
+                        },
+                        Some(incoming @ EntryVisit::Incoming(_)) => {
+                            // flip the order to process the next one.
+                            // guaranteed to have two exitresults then
+                            self.stack.push(res);
+                            self.stack.push(incoming);
+                        },
+                        None => {
+                            // result of already visiting root directory
+                            return None;
+                        }
+                    };
+                },
+                Some(EntryVisit::Incoming(Entry::Dir(_, contents))) => {
+                    self.stack.push(EntryVisit::Visiting(0));
+                    let evec: std::cell::Ref<'a, _> = contents.borrow();
+                    // FIXME don't leak
+                    let evec = std::cell::Ref::leak(evec);
+                    for ent in evec.iter() {
+                        self.stack.push(EntryVisit::Incoming(ent));
+                    }
+                },
+                None => return None // hmmm, should end at exitresult
+            }
+        }
+    }
+}
+
 fn walk_fs_sizes<F: FnMut(usize)>(entry: &Entry, visit: &mut F) -> usize {
     match entry {
         Entry::Dir(_, contents) => {
@@ -26,25 +113,42 @@ fn walk_fs_sizes<F: FnMut(usize)>(entry: &Entry, visit: &mut F) -> usize {
 }
 
 fn simple_compute_dirs(root: &Entry, max: usize) -> usize {
-    let mut total = 0;
+    let total_iter = root.iter_dir_sizes()
+        .filter(|&size| size <= max)
+        .sum::<usize>();
+
+    // backup check with ground truth
+    let mut total_recurse = 0;
     walk_fs_sizes(root, &mut |size| {
         if size <= max {
-            total += size;
+            total_recurse += size;
         }
     });
-    total
+
+    assert!(total_iter == total_recurse);
+    total_recurse
 }
 
 fn smallest_to_delete(root: &Entry, capacity: usize, need: usize) -> usize {
-    let space_used = walk_fs_sizes(root, &mut |_| { });
-    let free_space = capacity - space_used;
-    let mut smallest = space_used;
+    let space_used_iter = root.iter_dir_sizes().last().unwrap();
+    let space_used_recu = walk_fs_sizes(root, &mut |_| { });
+    assert!(space_used_iter == space_used_recu);
+
+    let free_space = capacity - space_used_iter;
+
+    let smallest_iter = root.iter_dir_sizes()
+        .filter(|&size| free_space + size >= need)
+        .min()
+        .unwrap();
+    let mut smallest_recu = space_used_recu;
     walk_fs_sizes(root, &mut |size| {
         if free_space + size >= need {
-            smallest = smallest.min(size);
+            smallest_recu = smallest_recu.min(size);
         }
     });
-    smallest
+    assert!(smallest_iter == smallest_recu);
+
+    smallest_iter
 }
 
 fn parse_listing(lines: &[String]) -> Entry {
