b0015ff552155c455ae0549b7358eb43b7a0ce8f
Konsta Hölttä <sooda@iki.fi>
2022-12-08 08:49:18 +0200

    8b the best spot to build the tree house
    Scan the cartesian directions of each tree to find how many are
    reachable; this finds a good tall tree in the middle of a wide low area.
    
    This would probably look better without skipping over some of the
    instructions first as there's no real need to track the tallest tree so
    far; take_while() gives one too few though, but adding one to the edge
    trees wouldn't work either. Seems like one of those cases where sentinel
    elements at the edges could be useful.

--- 22/8.rs
+++ 22/8.rs
@@ -1,4 +1,5 @@
 use std::io::{self, BufRead};
+use std::iter::repeat;
 
 type Trees = [Vec<u8>];
 
@@ -19,10 +20,38 @@ fn visible_outside(trees: &Trees) -> usize {
     }).count()
 }
 
+fn scenic_score(x: usize, y: usize, tree: u8, trees: &Trees) -> usize {
+    let h = trees.len();
+    let w = trees[0].len();
+    let scan_until_occluded = |tallest: &mut bool, (xi, yi): (usize, usize)| {
+        if *tallest {
+            None
+        } else if trees[yi][xi] >= tree {
+            *tallest = true;
+            Some(())
+        } else {
+            Some(())
+        }
+    };
+    let left_visible = (0..x).rev().zip(repeat(y)).scan(false, scan_until_occluded).count();
+    let top_visible = repeat(x).zip((0..y).rev()).scan(false, scan_until_occluded).count();
+    let right_visible = (x+1..w).zip(repeat(y)).scan(false, scan_until_occluded).count();
+    let bottom_visible = repeat(x).zip(y+1..h).scan(false, scan_until_occluded).count();
+
+    left_visible * top_visible * right_visible * bottom_visible
+}
+
+fn best_scenic_score(trees: &Trees) -> usize {
+    trees.iter().enumerate().flat_map(|(y, row)| {
+        row.iter().enumerate().map(move |(x, tree)| scenic_score(x, y, *tree, trees))
+    }).max().unwrap()
+}
+
 fn main() {
     let trees: Vec<_> = io::stdin().lock().lines()
         .map(|line| line.unwrap().as_bytes().to_vec())
         .collect();
     println!("{}", visible_outside(&trees));
+    println!("{}", best_scenic_score(&trees));
 }
 
