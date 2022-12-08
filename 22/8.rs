1ec8f23d3e385d4bf8c6e19b7e829099f7ca0dc8
Konsta Hölttä <sooda@iki.fi>
2022-12-08 21:50:07 +0200

    overengineer 8 more generic
    Adjust both parts of day 8 to use the same underlying scanning algorithm
    to illustrate how they're similar and how they're different.

--- 22/8.rs
+++ 22/8.rs
@@ -1,49 +1,70 @@
 use std::io::{self, BufRead};
 use std::iter::repeat;
 
-type Trees = [Vec<u8>];
+type Tree = u8;
+type Trees = [Vec<Tree>];
 
-fn visible(x: usize, y: usize, tree: u8, trees: &Trees) -> bool {
+// is it better to have F copy or to pass around a ref to it?
+fn visibility<F: Fn(&mut bool, Tree) -> Option<()> + Copy>(x: usize, y: usize, trees: &Trees, f: F)
+-> (usize, usize, usize, usize) {
     let h = trees.len();
     let w = trees[0].len();
-    let left_blocking = (0..x).any(|xi| trees[y][xi] >= tree);
-    let top_blocking = (0..y).any(|yi| trees[yi][x] >= tree);
-    let right_blocking = (x+1..w).any(|xi| trees[y][xi] >= tree);
-    let bottom_blocking = (y+1..h).any(|yi| trees[yi][x] >= tree);
 
-    !(left_blocking && top_blocking && right_blocking && bottom_blocking)
+    // this just shortens copypasta, hopefully the compiler doesn't dyn all the things
+    let compute = |it: &mut dyn Iterator<Item = (usize, usize)>| {
+        it.map(|(xi, yi)| trees[yi][xi]).scan(false, f).count()
+    };
+
+    let left = compute(&mut (0..x).rev().zip(repeat(y)));
+    let top = compute(&mut repeat(x).zip((0..y).rev()));
+    let right = compute(&mut (x+1..w).zip(repeat(y)));
+    let bottom = compute(&mut repeat(x).zip(y+1..h));
+
+    (left, top, right, bottom)
 }
 
-fn visible_outside(trees: &Trees) -> usize {
-    trees.iter().enumerate().flat_map(|(y, row)| {
-        row.iter().enumerate().filter(move |(x, tree)| visible(*x, y, **tree, trees))
-    }).count()
-}
-
-fn scenic_score(x: usize, y: usize, tree: u8, trees: &Trees) -> usize {
+fn visible(x: usize, y: usize, trees: &Trees) -> bool {
     let h = trees.len();
     let w = trees[0].len();
-    let scan_until_occluded = |tallest: &mut bool, (xi, yi): (usize, usize)| {
-        if *tallest {
+    let tree_at_origin = trees[y][x];
+    // like take_while
+    let f = |_: &mut bool, candidate: Tree| {
+        if candidate >= tree_at_origin {
             None
-        } else if trees[yi][xi] >= tree {
-            *tallest = true;
-            Some(())
         } else {
             Some(())
         }
     };
-    let left_visible = (0..x).rev().zip(repeat(y)).scan(false, scan_until_occluded).count();
-    let top_visible = repeat(x).zip((0..y).rev()).scan(false, scan_until_occluded).count();
-    let right_visible = (x+1..w).zip(repeat(y)).scan(false, scan_until_occluded).count();
-    let bottom_visible = repeat(x).zip(y+1..h).scan(false, scan_until_occluded).count();
+    let vis = visibility(x, y, trees, f);
 
-    left_visible * top_visible * right_visible * bottom_visible
+    vis.0 == x || vis.1 == y || vis.2 == w - 1 - x || vis.3 == h - 1 - y
+}
+
+fn visible_outside(trees: &Trees) -> usize {
+    trees.iter().enumerate().flat_map(|(y, row)| {
+        row.iter().enumerate().filter(move |(x, _)| visible(*x, y, trees))
+    }).count()
+}
+
+fn scenic_score(x: usize, y: usize, trees: &Trees) -> usize {
+    let tree_at_origin = trees[y][x];
+    // take_while would have off-by-1 issues
+    let f = |tallest: &mut bool, candidate: Tree| {
+        if *tallest {
+            None
+        } else {
+            *tallest = candidate >= tree_at_origin;
+            Some(())
+        }
+    };
+    let vis = visibility(x, y, trees, f);
+
+    vis.0 * vis.1 * vis.2 * vis.3
 }
 
 fn best_scenic_score(trees: &Trees) -> usize {
     trees.iter().enumerate().flat_map(|(y, row)| {
-        row.iter().enumerate().map(move |(x, tree)| scenic_score(x, y, *tree, trees))
+        row.iter().enumerate().map(move |(x, _)| scenic_score(x, y, trees))
     }).max().unwrap()
 }
 
@@ -54,4 +75,3 @@ fn main() {
     println!("{}", visible_outside(&trees));
     println!("{}", best_scenic_score(&trees));
 }
-
