21c472890996b0a1f6e964d96e84f2e1faab5a03
Konsta Hölttä <sooda@iki.fi>
2022-12-09 10:13:25 +0200

    9b ropes whipping as you fall
    Extend the simulation to support longer ropes. The movement logic is the
    same for each pair of knots, so behave as if one is the head and the
    other the tail of a planckian rope for each pair.
    
    This got the right results but is rather overcomplicated. Also, a simple
    hashset would have been enough after all, but at least we could now
    compute the intersections for fun.

--- 22/9.rs
+++ 22/9.rs
@@ -1,39 +1,81 @@
 use std::io::{self, BufRead};
 use std::collections::HashMap;
+use std::iter::once;
 
 type Move = (char, i32);
 
-fn dump_visits(visits: &HashMap::<(i32, i32), usize>) {
-    let minx = visits.keys().map(|&(x, _)| x).min().unwrap();
-    let maxx = visits.keys().map(|&(x, _)| x).max().unwrap();
-    let miny = visits.keys().map(|&(_, y)| y).min().unwrap();
-    let maxy = visits.keys().map(|&(_, y)| y).max().unwrap();
+fn dump_visits(visits: &HashMap::<Pt, usize>, head: Pt, tail: &[Pt]) {
+    let it = visits.keys().chain(once(&head)).chain(tail.iter());
+    let minx = it.clone().map(|&(x, _)| x).min().unwrap();
+    let maxx = it.clone().map(|&(x, _)| x).max().unwrap();
+    let miny = it.clone().map(|&(_, y)| y).min().unwrap();
+    let maxy = it.clone().map(|&(_, y)| y).max().unwrap();
+
     for y in (miny..=maxy).rev() {
         for x in minx..=maxx {
+            let mut ch = '.';
             if visits.contains_key(&(x, y)) {
-                print!("#");
-            } else {
-                print!(".");
+                ch = '#';
             }
+            if (x, y) == (0, 0) {
+                ch = 's';
+            }
+            for (ti, &t) in tail.iter().enumerate().rev() {
+                if (x, y) == t {
+                    ch = (b'1' + ti as u8) as char;
+                }
+            }
+            if (x, y) == head {
+                ch = 'H';
+            }
+            print!("{}", ch);
         }
         println!();
     }
 }
 
-fn tail_visits_once(moves: &[Move]) -> usize {
-    let mut visits = HashMap::<(i32, i32), usize>::new();
+type Pt = (i32, i32);
+
+fn delta(h: Pt, t: Pt) -> Pt {
+    (h.0 - t.0, h.1 - t.1)
+}
+
+fn knotty_move(head: Pt, mut tail: Pt) -> Pt {
+    let d = delta(head, tail);
+    if d.0 == 0 || d.1 == 0 {
+        // directly in line
+        if d.0.abs() > 1 || d.1.abs() > 1 {
+            if false {
+                println!("direct {:?}", d);
+            }
+            tail = (tail.0 + d.0.signum(), tail.1 + d.1.signum());
+        }
+    } else {
+        // diagonal move
+        if d.0.abs() > 1 || d.1.abs() > 1 {
+            if false {
+                println!("diag {:?}", d);
+            }
+            tail = (tail.0 + d.0.signum(), tail.1 + d.1.signum());
+        }
+    }
+    tail
+}
+
+fn tail_visits_once(moves: &[Move], ntails: usize) -> usize {
+    let mut visits = HashMap::<Pt, usize>::new();
     let mut head = (0, 0);
-    let mut tail = (0, 0);
-    visits.insert(tail, 1);
-    let delta = |h: (_, _), t: (_, _)| (h.0 - t.0, h.1 - t.1);
+    // tails[0] moves first, tails.last() is the end of the rope
+    let mut tails = vec![(0, 0); ntails];
+    visits.insert((0, 0), 1);
     for &(dir, length) in moves {
         if false {
             println!("looks like:");
-            dump_visits(&visits);
+            dump_visits(&visits, head, &tails);
             println!("then {} {}", dir, length);
         }
         // x axis points to the right, y up
-        let (dx, dy) = match dir {
+        let dhead = match dir {
             'L' => (-1, 0),
             'R' => (1, 0),
             'U' => (0, 1),
@@ -41,25 +83,41 @@ fn tail_visits_once(moves: &[Move]) -> usize {
             _ => panic!("bad dir"),
         };
         for i in 0..length {
-            head = (head.0 + dx, head.1 + dy);
-            let d = delta(head, tail);
             if false {
-                println!("{}: tail {:?}, head {:?}, d {:?}", i, tail, head, d);
-                dump_visits(&visits);
+                println!();
+                println!("move #{} from:", i);
+                dump_visits(&visits, head, &tails);
+                println!();
             }
-            if d.0 == 0 || d.1 == 0 {
-                // directly in line
-                if d.0.abs() > 1 || d.1.abs() > 1 {
-                    tail = (tail.0 + dx, tail.1 + dy);
-                }
-            } else {
-                // diagonal move
-                if d.0.abs() > 1 || d.1.abs() > 1 {
-                    tail = (tail.0 + d.0.signum(), tail.1 + d.1.signum());
-                }
+            head = (head.0 + dhead.0, head.1 + dhead.1);
+            if false {
+                println!("for idx 0");
             }
-            visits.entry(tail).and_modify(|n| *n += 1).or_insert(1);
+            let t = knotty_move(head, tails[0]);
+            if false {
+                println!("after that move:");
+                dump_visits(&visits, head, &tails);
+            }
+            tails[0] = t;
+            for ti in 1..ntails {
+                let (src, dest) = (ti - 1, ti);
+                if false {
+                    println!("for idx {}", ti);
+                }
+                let t = knotty_move(tails[src], tails[dest]);
+                if false {
+                    println!("after that move:");
+                    dump_visits(&visits, head, &tails);
+                }
+                tails[dest] = t;
+            }
+
+            visits.entry(*tails.last().unwrap()).and_modify(|n| *n += 1).or_insert(1);
         }
+    }
+    if false {
+        println!("end looks like:");
+        dump_visits(&visits, head, &tails);
     }
     visits.values().count()
 }
@@ -76,5 +134,6 @@ fn main() {
     let moves: Vec<_> = io::stdin().lock().lines()
         .map(|line| parse_move(&line.unwrap()))
         .collect();
-    println!("{}", tail_visits_once(&moves));
+    println!("{}", tail_visits_once(&moves, 1));
+    println!("{}", tail_visits_once(&moves, 9));
 }
