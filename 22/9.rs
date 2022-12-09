030683aad196a25455c3dc01b8ae4f56724f6d1a
Konsta Hölttä <sooda@iki.fi>
2022-12-09 12:14:48 +0200

    cleanup 9b
    Brain went off while coding. Turns out the moves generalize well and the
    head can be just part of a knot list.

--- 22/9.rs
+++ 22/9.rs
@@ -1,11 +1,11 @@
 use std::io::{self, BufRead};
 use std::collections::HashMap;
-use std::iter::once;
 
 type Move = (char, i32);
+type Pt = (i32, i32);
 
-fn dump_visits(visits: &HashMap::<Pt, usize>, head: Pt, tail: &[Pt]) {
-    let it = visits.keys().chain(once(&head)).chain(tail.iter());
+fn dump_visits(visits: &HashMap::<Pt, usize>, knots: &[Pt]) {
+    let it = visits.keys().chain(knots.iter());
     let minx = it.clone().map(|&(x, _)| x).min().unwrap();
     let maxx = it.clone().map(|&(x, _)| x).max().unwrap();
     let miny = it.clone().map(|&(_, y)| y).min().unwrap();
@@ -20,12 +20,12 @@ fn dump_visits(visits: &HashMap::<Pt, usize>, head: Pt, tail: &[Pt]) {
             if (x, y) == (0, 0) {
                 ch = 's';
             }
-            for (ti, &t) in tail.iter().enumerate().rev() {
+            for (ki, &t) in knots.iter().enumerate().skip(1).rev() {
                 if (x, y) == t {
-                    ch = (b'1' + ti as u8) as char;
+                    ch = (b'0' + ki as u8) as char;
                 }
             }
-            if (x, y) == head {
+            if (x, y) == knots[0] {
                 ch = 'H';
             }
             print!("{}", ch);
@@ -34,45 +34,33 @@ fn dump_visits(visits: &HashMap::<Pt, usize>, head: Pt, tail: &[Pt]) {
     }
 }
 
-type Pt = (i32, i32);
-
 fn delta(h: Pt, t: Pt) -> Pt {
     (h.0 - t.0, h.1 - t.1)
 }
 
-fn knotty_move(head: Pt, mut tail: Pt) -> Pt {
-    let d = delta(head, tail);
-    if d.0 == 0 || d.1 == 0 {
-        // directly in line
-        if d.0.abs() > 1 || d.1.abs() > 1 {
-            if false {
-                println!("direct {:?}", d);
-            }
-            tail = (tail.0 + d.0.signum(), tail.1 + d.1.signum());
-        }
-    } else {
-        // diagonal move
-        if d.0.abs() > 1 || d.1.abs() > 1 {
-            if false {
-                println!("diag {:?}", d);
-            }
-            tail = (tail.0 + d.0.signum(), tail.1 + d.1.signum());
-        }
-    }
-    tail
+fn sum(a: Pt, b: Pt) -> Pt {
+    (a.0 + b.0, a.1 + b.1)
 }
 
-fn tail_visits_once(moves: &[Move], ntails: usize) -> usize {
+fn knotty_move(head: Pt, tail: Pt) -> Pt {
+    let d = delta(head, tail);
+    if d.0.abs() > 1 || d.1.abs() > 1 {
+        sum(tail, (d.0.signum(), d.1.signum()))
+    } else {
+        tail
+    }
+}
+
+fn tail_visits_once(moves: &[Move], nknots: usize) -> usize {
     let mut visits = HashMap::<Pt, usize>::new();
-    let mut head = (0, 0);
-    // tails[0] moves first, tails.last() is the end of the rope
-    let mut tails = vec![(0, 0); ntails];
+    // knots[0] moves first, knots.last() is the end of the rope
+    let mut knots = vec![(0, 0); nknots];
     visits.insert((0, 0), 1);
+
     for &(dir, length) in moves {
         if false {
-            println!("looks like:");
-            dump_visits(&visits, head, &tails);
-            println!("then {} {}", dir, length);
+            println!("before {} {}:", dir, length);
+            dump_visits(&visits, &knots);
         }
         // x axis points to the right, y up
         let dhead = match dir {
@@ -82,44 +70,23 @@ fn tail_visits_once(moves: &[Move], ntails: usize) -> usize {
             'D' => (0, -1),
             _ => panic!("bad dir"),
         };
-        for i in 0..length {
-            if false {
-                println!();
-                println!("move #{} from:", i);
-                dump_visits(&visits, head, &tails);
-                println!();
-            }
-            head = (head.0 + dhead.0, head.1 + dhead.1);
-            if false {
-                println!("for idx 0");
-            }
-            let t = knotty_move(head, tails[0]);
-            if false {
-                println!("after that move:");
-                dump_visits(&visits, head, &tails);
-            }
-            tails[0] = t;
-            for ti in 1..ntails {
-                let (src, dest) = (ti - 1, ti);
-                if false {
-                    println!("for idx {}", ti);
-                }
-                let t = knotty_move(tails[src], tails[dest]);
-                if false {
-                    println!("after that move:");
-                    dump_visits(&visits, head, &tails);
-                }
-                tails[dest] = t;
+        for _ in 0..length {
+            knots[0] = sum(knots[0], dhead);
+            for ki in 1..nknots {
+                let (src, dest) = (ki - 1, ki);
+                knots[dest] = knotty_move(knots[src], knots[dest]);
             }
 
-            visits.entry(*tails.last().unwrap()).and_modify(|n| *n += 1).or_insert(1);
+            visits.entry(knots[nknots - 1]).and_modify(|n| *n += 1).or_insert(1);
         }
     }
+
     if false {
         println!("end looks like:");
-        dump_visits(&visits, head, &tails);
+        dump_visits(&visits, &knots);
     }
-    visits.values().count()
+
+    visits.len()
 }
 
 fn parse_move(line: &str) -> Move {
@@ -134,6 +101,6 @@ fn main() {
     let moves: Vec<_> = io::stdin().lock().lines()
         .map(|line| parse_move(&line.unwrap()))
         .collect();
-    println!("{}", tail_visits_once(&moves, 1));
-    println!("{}", tail_visits_once(&moves, 9));
+    println!("{}", tail_visits_once(&moves, 2));
+    println!("{}", tail_visits_once(&moves, 10));
 }
