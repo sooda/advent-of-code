f286c981478d1cc1b336991a01772872c7a1b6bc
Konsta Hölttä <sooda@iki.fi>
2022-12-15 09:12:31 +0200

    15: proper structure for part b
    Split out the task to find those spans per row so that the result can be
    returned to the caller instead of debug printing in the middle.

--- 22/15.rs
+++ 22/15.rs
@@ -31,24 +31,14 @@ fn try_merge_any(spans: &[Span], next: Span) -> Option<(usize, Span)> {
         .next()
 }
 
-fn positions(sensors: &[Sensor], row: i64) -> usize {
+fn visible_spans(sensors: &[Sensor], row: i64) -> Vec<Span> {
     let mut spans: Vec<Span> = Vec::new();
     for sensor in sensors {
         let view = (sensor.0.0 - sensor.1.0).abs() + (sensor.0.1 - sensor.1.1).abs();
-        if false {
-            println!("sens at {:?} for {:?} view {} sees {:?}",
-                     sensor.0, sensor.1, view, (sensor.0.1 - row).abs() <= view);
-        }
         let dy = (row - sensor.0.1).abs();
         if dy <= view {
             let dx = view - dy;
-            let min = sensor.0.0 - dx;
-            let max = sensor.0.0 + dx;
-            if false {
-                println!("dy {}  sees this much {} its min max = {} {}",
-                         dy, dx, min, max);
-            }
-            let mut this_span = (min, max);
+            let mut this_span = (sensor.0.0 - dx, sensor.0.0 + dx);
             while let Some((i, combined)) = try_merge_any(&spans, this_span) {
                 this_span = combined;
                 spans.remove(i);
@@ -57,16 +47,12 @@ fn positions(sensors: &[Sensor], row: i64) -> usize {
         }
     }
 
-    if spans.len() == 2 {
-        spans.sort_unstable();
-        let (l, r) = (spans[0], spans[1]);
-        if l.0 <= 0 && r.1 >= 20 && l.1 + 2 == r.0 {
-            let x = l.1 + 1;
-            println!("ahoy x {} y {} score {}", x, row, x * 4000000 + row);
-        }
-    }
+    spans
+}
 
-    // multiples are ok
+fn positions(sensors: &[Sensor], row: i64) -> usize {
+    let spans = visible_spans(sensors, row);
+
     let beacons: HashSet<_> = sensors.iter().map(|&(_, b)| b).collect();
     let visible = beacons.iter().filter(|&b| {
         b.1 == row && spans.iter().any(|sp| b.0 >= sp.0 && b.0 <= sp.1)
@@ -75,17 +61,31 @@ fn positions(sensors: &[Sensor], row: i64) -> usize {
     spans.iter().map(|sp| sp.1 - sp.0 + 1).sum::<i64>() as usize - visible
 }
 
-fn tuning_freq(sensors: &[Sensor], maxsize: i64) -> i64 {
+fn tuning_freq(sensors: &[Sensor], maxsize: i64) -> Option<i64> {
+    let mut ret = None;
     for y in 0..=maxsize {
-        positions(sensors, y);
+        let mut spans = visible_spans(sensors, y);
+        if spans.len() == 2 {
+            spans.sort_unstable();
+            let (l, r) = (spans[0], spans[1]);
+            if l.0 <= 0 && r.1 >= maxsize && l.1 + 2 == r.0 {
+                if ret.is_some() {
+                    panic!("multiple solutions");
+                } else {
+                    ret = Some(4000000 * (l.1 + 1) + y);
+                }
+            }
+        }
     }
-    0
+
+    ret
 }
 
 fn parse_sensor(line: &str) -> Sensor {
     // Sensor at x=2, y=18: closest beacon is at x=-2, y=15
     let re = Regex::new(r"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)").unwrap();
     let cap = re.captures(line).unwrap();
+    // ughh
     let sx = cap.get(1).unwrap().as_str().parse().unwrap();
     let sy = cap.get(2).unwrap().as_str().parse().unwrap();
     let bx = cap.get(3).unwrap().as_str().parse().unwrap();
@@ -100,6 +100,6 @@ fn main() {
         .collect();
     println!("sample {}", positions(&sensors, 10));
     println!("{}", positions(&sensors, 2000000));
-    println!("sample {}", tuning_freq(&sensors, 20));
-    println!("{}", tuning_freq(&sensors, 4000000));
+    println!("sample {:?}", tuning_freq(&sensors, 20));
+    println!("{:?}", tuning_freq(&sensors, 4000000));
 }
