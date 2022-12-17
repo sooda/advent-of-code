2e5645b93eb1ff70520d43cc9cd076afbb9fbe4b
Konsta Hölttä <sooda@iki.fi>
2022-12-17 18:12:49 +0200

    16b one of the elephants to help
    Extend the state node with the valve index that the big animal friend is
    currently vacating. Add a new separate search to make use of it.
    
    Now there are much more edges between search space nodes:
    
    (stay, open, or move) x (elephant stays, elephant opens, elephant moves)
    
    Otherwise the search is mostly the same as before.
    
    The A*-like heuristic of not even trying an edge that could not possibly
    lead to a better pressure result than the best so far seems to be
    necessary to prune the choices enough to make this run even in that one
    and half minutes, which still is very long; the search space should be
    cut much more with some trick. It's perhaps not a great idea to hold the
    time left in the search space node? Why did that even exist again?
    
    One possible optimization could be to jump over the non-steaming valves
    that are just tunnel parts, but time tracking is a bit tricky with that
    if both of the travelers need to end up at some very specific nodes that
    are different steps apart from current. The tunnel cells probably
    contribute to a lot of back-and-forth walking. Maybe going to a tunnel
    just for a u-turn is avoided with the visit scores, but just jumping
    over the tunnel parts would make that worry unnecessary.
    
    What if each traveler had a short-term plan to go to some particular
    closed valve, rather than just trying what happens by visiting the
    nearest neighbors? For now it's a lot of shuffling around for nothing.
    In previous years' searches, "teleporting" right over to important nodes
    by constructing a complete graph has caused success.

--- 22/16.rs
+++ 22/16.rs
@@ -1,6 +1,4 @@
 use std::io::{self, BufRead};
-// HashSet<String> isn't hashable so it wouldn't fit in another hashset
-// (not a problem anymore?)
 use std::collections::{HashMap, hash_map::Entry, BinaryHeap};
 
 extern crate regex;
@@ -43,17 +41,14 @@ impl OpenMap {
     fn and(self, other: Self) -> Self {
         Self(self.0 & other.0)
     }
-
-    fn all(self, other: Self) -> bool {
-        self.and(other) == other
-    }
 }
 
 impl std::fmt::Debug for OpenMap {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        let alphas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".as_bytes();
         for i in 0..63 {
             if self.is_open(i) {
-                write!(f, "X")?;
+                write!(f, "{}", alphas[i % 26] as char)?;
             } else {
                 write!(f, "_")?;
             }
@@ -73,12 +68,13 @@ impl std::fmt::Debug for OpenMap {
 struct Score {
     timeleft: i64,
     nopened: usize,
-    steamed: i64,
+    steamed: i64, // not including steam from this node
 }
 
 #[derive(Clone, Copy, Eq, Ord, PartialEq, PartialOrd, Hash)]
 struct Node {
     vidx: usize,
+    elephant: usize,
     vstates: OpenMap,
     timeleft: i64,
 }
@@ -99,7 +95,7 @@ fn vent(valves: &[Valve], opened: OpenMap) -> i64 {
 fn dijkstra(valves: &[Valve], startidx: usize) -> i64 {
     let debug = false;
     let startscore = Score { steamed: 0, nopened: 0, timeleft: 30 };
-    let startnode = Node { vidx: startidx, vstates: OpenMap::new(), timeleft: 30 };
+    let startnode = Node { vidx: startidx, elephant: 0, vstates: OpenMap::new(), timeleft: 30 };
     let mut heap: BinaryHeap<State> = BinaryHeap::new();
     let mut visited = HashMap::new();
     heap.push(State { score: startscore, node: startnode });
@@ -111,8 +107,13 @@ fn dijkstra(valves: &[Valve], startidx: usize) -> i64 {
         .filter(|&(_, v)| v.rate > 0)
         .fold(OpenMap::new(), |map, (i, _)| map.open(i));
 
-    let mut push = |h: &mut BinaryHeap<_>, state: State| {
+    let mut push = |h: &mut BinaryHeap<_>, state: State, best| {
         let (score, node) = (state.score, state.node);
+        let max_possible_vent = score.timeleft * good_valves.len() as i64;
+
+        if score.steamed + max_possible_vent <= best {
+            return;
+        }
 
         let nent = visited.entry(node);
         match nent {
@@ -128,8 +129,6 @@ fn dijkstra(valves: &[Valve], startidx: usize) -> i64 {
             Entry::Occupied(mut e) => {
                 let oldscore = e.get();
                 let more_steam = score.steamed > oldscore.steamed;
-                let more_time = score.timeleft > oldscore.timeleft;
-                // (nopened is part of state, cached for score)
                 if more_steam {
                     if debug {
                         println!("   to {} {:?} steam {} nopen {} time {} (better)",
@@ -170,10 +169,11 @@ fn dijkstra(valves: &[Valve], startidx: usize) -> i64 {
                     },
                     node: Node {
                         vidx: node.vidx,
+                        elephant: 0,
                         vstates: node.vstates.open(node.vidx),
                         timeleft: newtime,
                     }
-                });
+                }, steam_possible);
             }
             for nname in &posvalve.neighs {
                 let nidx = geti(valves, &nname);
@@ -185,10 +185,11 @@ fn dijkstra(valves: &[Valve], startidx: usize) -> i64 {
                     },
                     node: Node {
                         vidx: nidx,
+                        elephant: 0,
                         vstates: node.vstates,
                         timeleft: newtime,
                     }
-                });
+                }, steam_possible);
             }
         }
     }
@@ -198,6 +199,176 @@ fn dijkstra(valves: &[Valve], startidx: usize) -> i64 {
 
 fn most_pressure(valves: &[Valve]) -> i64 {
     dijkstra(valves, geti(valves, "AA"))
+}
+
+fn dijkstra_with_elephant(valves: &[Valve], startidx: usize) -> i64 {
+    let debug = false;
+    let startscore = Score { steamed: 0, nopened: 0, timeleft: 26 };
+    let startnode = Node { vidx: startidx, elephant: startidx, vstates: OpenMap::new(), timeleft: 26 };
+    let mut heap: BinaryHeap<State> = BinaryHeap::new();
+    let mut visited = HashMap::new();
+    heap.push(State { score: startscore, node: startnode });
+    // those two tuples backwards
+    // from state to score
+    visited.insert(startnode, startscore);
+
+    let good_valves = valves.iter().enumerate()
+        .filter(|&(_, v)| v.rate > 0)
+        .fold(OpenMap::new(), |map, (i, _)| map.open(i));
+
+    let mut steam_possible = 0;
+
+    let mut push = |h: &mut BinaryHeap<_>, state: State, best| {
+        let (score, node) = (state.score, state.node);
+        let max_possible_vent = score.timeleft * good_valves.len() as i64;
+
+        if score.steamed + max_possible_vent <= best {
+            return;
+        }
+
+        let nent = visited.entry(node);
+        match nent {
+            Entry::Vacant(e) => {
+                if debug {
+                    println!("   to {} {} {:?} steam {} nopen {} time {} (new)",
+                             valves[node.vidx].name, valves[node.elephant].name, node.vstates,
+                             score.steamed, score.nopened, 27-score.timeleft);
+                }
+                e.insert(score);
+                h.push(state);
+            },
+            Entry::Occupied(mut e) => {
+                let oldscore = e.get();
+                let more_steam = score.steamed > oldscore.steamed;
+                if more_steam {
+                    if debug {
+                        println!("   to {} {} {:?} steam {} nopen {} time {} (better)",
+                                 valves[node.vidx].name, valves[node.elephant].name, node.vstates,
+                                 score.steamed, score.nopened, 27-score.timeleft);
+                    }
+                    e.insert(score);
+                    h.push(state);
+                }
+            },
+        }
+    };
+
+    while let Some(state) = heap.pop() {
+        let (score, node) = (state.score, state.node);
+        if debug {
+            println!("visit {} {} {}left {:?} steam {} nopen {} time {}",
+                     valves[node.vidx].name, valves[node.elephant].name, node.timeleft, node.vstates,
+                     score.steamed, score.nopened, 27-score.timeleft);
+        }
+
+        if score.steamed > steam_possible {
+            println!("{}", score.steamed);
+        }
+        steam_possible = steam_possible.max(score.steamed);
+
+        let release = vent(valves, node.vstates);
+        let steamed = score.steamed + release;
+        let timeleft = score.timeleft - 1;
+
+        if timeleft >= 0 {
+            // can not move and not open, but cannot move and open in a single round
+            for &(self_move, self_open, elep_move, elep_open) in &[
+                // both do nothing is not a useful condition
+                /* (false, false, false, false), */
+                (false, true, false, false),
+                (true, false, false, false),
+
+                (false, false, false, true),
+                (false, true, false, true),
+                (true, false, false, true),
+
+                (false, false, true, false),
+                (false, true, true, false),
+                (true, false, true, false),
+            ] {
+                let self_open = self_open && good_valves.is_open(node.vidx);
+                let elep_open = elep_open && good_valves.is_open(node.elephant);
+                let mut vstates = node.vstates;
+                if self_open {
+                    vstates = vstates.open(node.vidx);
+                }
+                if elep_open {
+                    vstates = vstates.open(node.elephant);
+                }
+                let nopened = vstates.and(good_valves).len();
+                let score = Score {
+                    steamed,
+                    nopened,
+                    timeleft,
+                };
+                if !self_move && !elep_move {
+                    // steam this ham in particular
+                    push(&mut heap, State {
+                        score,
+                        node: Node {
+                            vidx: node.vidx,
+                            elephant: node.elephant,
+                            vstates,
+                            timeleft,
+                        }
+                    }, steam_possible);
+                }
+
+                // ok to use the above score, as move and open cannot happen in parallel
+
+                if self_move && !elep_move {
+                    for nname in &valves[node.vidx].neighs {
+                        let nidx = geti(valves, &nname);
+                        push(&mut heap, State {
+                            score,
+                            node: Node {
+                                vidx: nidx,
+                                elephant: node.elephant,
+                                vstates,
+                                timeleft,
+                            }
+                        }, steam_possible);
+                    }
+                }
+
+                if elep_move && !self_move {
+                    for nname in &valves[node.elephant].neighs {
+                        let nidx = geti(valves, &nname);
+                        push(&mut heap, State {
+                            score,
+                            node: Node {
+                                vidx: node.vidx,
+                                elephant: nidx,
+                                vstates,
+                                timeleft,
+                            }
+                        }, steam_possible);
+                    }
+                }
+                if self_move && elep_move {
+                    for selfname in &valves[node.vidx].neighs {
+                        for elepname in &valves[node.elephant].neighs {
+                            push(&mut heap, State {
+                                score,
+                                node: Node {
+                                    vidx: geti(valves, &selfname),
+                                    elephant: geti(valves, &elepname),
+                                    vstates,
+                                    timeleft,
+                                }
+                            }, steam_possible);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    steam_possible
+}
+
+fn most_pressure_with_elephant(valves: &[Valve]) -> i64 {
+    dijkstra_with_elephant(valves, geti(valves, "AA"))
 }
 
 fn dump_map(valves: &[Valve]) {
@@ -233,5 +404,6 @@ fn main() {
         dump_map(&valves);
     } else {
         println!("{}", most_pressure(&valves));
+        println!("{}", most_pressure_with_elephant(&valves));
     }
 }
