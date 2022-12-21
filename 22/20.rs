69cbeb9adbd072d53934b6f2763c183f2952f7be
Konsta Hölttä <sooda@iki.fi>
2022-12-21 11:19:45 +0200

    20: shrink those usize links
    u16 is sufficient, and s/usize/u16/ for list links makes the entire
    problem fit in my L1. Runtime is only halved though.

--- 22/20.rs
+++ 22/20.rs
@@ -1,50 +1,61 @@
 use std::io::{self, BufRead};
 
-fn unlink(links: &mut [(usize, usize)], pos: usize) {
-    let (left, right) = links[pos];
-    links[left].1 = right;
-    links[right].0 = left;
+type Lidx = u16;
+type Link = (Lidx, Lidx);
+
+fn at(links: &[Link], pos: Lidx) -> Link {
+    links[pos as usize]
 }
 
-fn insert_after(links: &mut [(usize, usize)], pos: usize, link: usize) {
-    let (left, right) = (pos, links[pos].1);
-    links[left].1 = link;
-    links[right].0 = link;
-    links[link] = (left, right);
+fn at_mut(links: &mut [Link], pos: Lidx) -> &mut Link {
+    &mut links[pos as usize]
 }
 
-fn insert_before(links: &mut [(usize, usize)], pos: usize, link: usize) {
-    insert_after(links, links[pos].0, link);
+fn unlink(links: &mut [Link], pos: Lidx) {
+    let (left, right) = at(links, pos);
+    at_mut(links, left).1 = right;
+    at_mut(links, right).0 = left;
 }
 
-fn list_nth(links: &[(usize, usize)], mut link: usize, steps: usize) -> usize {
+fn insert_after(links: &mut [Link], pos: Lidx, link: Lidx) {
+    let (left, right) = (pos, at(links, pos).1);
+    at_mut(links, left).1 = link;
+    at_mut(links, right).0 = link;
+    *at_mut(links, link) = (left, right);
+}
+
+fn insert_before(links: &mut [Link], pos: Lidx, link: Lidx) {
+    insert_after(links, at(links, pos).0, link);
+}
+
+fn list_nth(links: &[Link], mut link: Lidx, steps: Lidx) -> Lidx {
     for _ in 0..steps {
-        link = links[link].1;
+        link = at(links, link).1;
     }
     link
 }
 
-fn move_right(links: &mut [(usize, usize)], src: usize, steps: usize) {
+fn move_right(links: &mut [Link], src: Lidx, steps: Lidx) {
     let after = list_nth(links, src, steps);
-    let before = links[after].1; // manage also steps == 0 branchless
+    let before = at(links, after).1; // manage also steps == 0 branchless
     unlink(links, src);
     insert_before(links, before, src);
 }
 
-fn print_list(file: &[i64], links: &[(usize, usize)]) {
+fn print_list(file: &[i64], links: &[Link]) {
     let mut p = 0;
     // not drawn as in the examples, but it's a loop and thus equivalent
     for _j in 0..file.len() {
-        print!("{} ", file[p]);
-        p = links[p].1;
+        print!("{} ", file[p as usize]);
+        p = at(links, p).1;
     }
     println!();
 }
 
-fn basic_links(n: usize) -> Vec<(usize, usize)> {
-    let mut links = vec![(0usize, 0usize); n];
+fn basic_links(n: Lidx) -> Vec<Link> {
+    let mut links = vec![(0, 0); n as usize];
     for i in 0..n {
-        links[i] = (
+        links[i as usize] = (
             (i + n - 1) % n,
             (i + 1) % n
         );
@@ -52,7 +63,7 @@ fn basic_links(n: usize) -> Vec<(usize, usize)> {
     links
 }
 
-fn mix(file: &[i64], mut links: Vec<(usize, usize)>) -> Vec<(usize, usize)> {
+fn mix(file: &[i64], mut links: Vec<Link>) -> Vec<Link> {
     /*
      *           ->
      * 4, 5, 6, 1, 7, 8, 9
@@ -73,14 +84,14 @@ fn mix(file: &[i64], mut links: Vec<(usize, usize)>) -> Vec<(usize, usize)> {
 
     for i in 0..file.len() {
         let right = if file[i] >= 0 {
-            (file[i] as usize) % (file.len() - 1)
+            (file[i] % (file.len() as i64 - 1)) as Lidx
         } else {
-            let left = ((-file[i]) as usize) % (file.len() - 1);
+            let left = (((-file[i])) % (file.len() as i64 - 1)) as Lidx;
             // cyclic, and moving n-1 moves a full cycle etc
             // e.g. len six: 0 1 _2_ 3 4 5 two left = three right: _2_ 0 1 3 4 5
-            file.len() - left - 1
+            file.len() as Lidx - left - 1
         };
-        move_right(&mut links, i, right);
+        move_right(&mut links, i as Lidx, right);
 
         if false {
             print_list(file, &links);
@@ -90,18 +101,18 @@ fn mix(file: &[i64], mut links: Vec<(usize, usize)>) -> Vec<(usize, usize)> {
     links
 }
 
-fn mix_result(file: &[i64], mixes: usize) -> i64 {
-    let mut links = basic_links(file.len());
+fn mix_result(file: &[i64], mixes: Lidx) -> i64 {
+    let mut links = basic_links(file.len() as Lidx);
     for _ in 0..mixes {
         let next_links = mix(file, links);
         links = next_links;
     }
 
-    let mut pos = file.iter().position(|&n| n == 0).unwrap();
+    let mut pos = file.iter().position(|&n| n == 0).unwrap() as Lidx;
     let mut ret = 0;
     for _ in 0..3 {
         pos = list_nth(&links, pos, 1000);
-        ret += file[pos];
+        ret += file[pos as usize];
     }
 
     ret
