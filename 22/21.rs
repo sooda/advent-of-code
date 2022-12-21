4ed5392660d572c7356d8d310a16869cde8216a7
Konsta Hölttä <sooda@iki.fi>
2022-12-21 09:44:20 +0200

    21b misunderstood key details
    The root monkey tests for equality and the humn monkey is human input,
    not the number in it. Find where x is, and solve the equation around it
    one step at a time by bringing the outermost computation to the other
    side that contains math without x.

--- 22/21.rs
+++ 22/21.rs
@@ -9,20 +9,25 @@ enum Op {
     Div,
 }
 
+#[derive(Clone)]
 enum Yell {
     MathResult(String, String, Op),
     Number(i64),
 }
+use Yell::*;
 
+#[derive(Clone)]
 struct Monkey {
     name: String,
     yell: Yell,
 }
 
-fn obtain(monkeys: &HashMap<&str, &Monkey>, name: &str) -> i64 {
+type Monkeys = HashMap<String, Monkey>;
+
+fn obtain(monkeys: &Monkeys, name: &str) -> i64 {
     match &monkeys.get(name).unwrap().yell {
-        Yell::Number(n) => *n,
-        Yell::MathResult(left, right, op) => {
+        Number(n) => *n,
+        MathResult(left, right, op) => {
             let left_num = obtain(monkeys, &left);
             let right_num = obtain(monkeys, &right);
             match op {
@@ -35,8 +40,80 @@ fn obtain(monkeys: &HashMap<&str, &Monkey>, name: &str) -> i64 {
     }
 }
 
-fn root_number(monkeys: &HashMap<&str, &Monkey>) -> i64 {
+fn root_number(monkeys: &Monkeys) -> i64 {
     obtain(monkeys, "root")
+}
+
+fn find_leaf_monkey(monkeys: &Monkeys, node: &str, find: &str) -> bool {
+    match &monkeys.get(node).unwrap().yell {
+        Number(_) => node == find,
+        MathResult(left, right, _) => {
+            find_leaf_monkey(monkeys, left, find) || find_leaf_monkey(monkeys, right, find)
+        }
+    }
+}
+
+fn order_x<'a, 'b>(monkeys: &'a Monkeys, left: &'b str, right: &'b str) -> (&'b str, &'b str) {
+    let found = (
+        find_leaf_monkey(monkeys, &left, "humn"),
+        find_leaf_monkey(monkeys, &right, "humn")
+    );
+    match found {
+        (true, false) => (left, right),
+        (false, true) => (right, left),
+        (false, false) => panic!("no x"),
+        (true, true) => panic!("both x??"),
+    }
+}
+
+/*
+
+((4+(2*(x-3)))/4) = ((32-2)*5)
+ (4+(2*(x-3)))    = (((32-2)*5)*4)
+    (2*(x-3))     = ((((32-2)*5)*4)-4)
+       (x-3)      = (((((32-2)*5)*4)-4)/2)
+        x         = (((((32-2)*5)*4)-4)/2)+3
+*/
+fn find_x(monkeys: &mut Monkeys, xside: &str, mathside: &str) -> i64 {
+    match &monkeys.get(xside).unwrap().yell {
+        Number(_) => obtain(monkeys, mathside),
+        MathResult(left, right, op) => {
+            // FIXME keep the path in topmost search to avoid searching again?
+            let (x, other) = order_x(monkeys, left, right);
+            let (x, other, mathside) = (x.to_string(), other.to_string(), mathside.to_string());
+
+            // wrap the current math side and the part split from x in a new monkey
+            let new_name = mathside.clone() + "_for_x";
+
+            let yell = match (x == *left, op) {
+                // (x + other, x - other, x / other, x * other) = mathside
+                (true, Op::Plus)   => MathResult(mathside, other, Op::Minus),
+                (true, Op::Minus)  => MathResult(mathside, other, Op::Plus),
+                (true, Op::Mul)    => MathResult(mathside, other, Op::Div),
+                (true, Op::Div)    => MathResult(mathside, other, Op::Mul),
+                // (other + x, other - x, other / x, other * x) = mathside
+                (false, Op::Plus)  => MathResult(mathside, other, Op::Minus),
+                (false, Op::Minus) => MathResult(other, mathside, Op::Minus),
+                (false, Op::Mul)   => MathResult(mathside, other, Op::Div),
+                (false, Op::Div)   => MathResult(other, mathside, Op::Div),
+            };
+
+            let new_m = Monkey { name: new_name.clone(), yell };
+            monkeys.insert(new_name.clone(), new_m);
+
+            find_x(monkeys, &x, &new_name)
+        }
+    }
+}
+
+fn root_equality_test(mut monkeys: Monkeys) -> i64 {
+    let (left, right) = match &monkeys.get("root").unwrap().yell {
+        MathResult(left, right, _) => (left.clone(), right.clone()),
+        _ => panic!()
+    };
+    let (x, math) = order_x(&monkeys, &left, &right);
+
+    find_x(&mut monkeys, x, math)
 }
 
 fn parse_monkey(input: &str) -> Monkey {
@@ -44,7 +121,7 @@ fn parse_monkey(input: &str) -> Monkey {
     let name = sp.next().unwrap().to_string();
     let yell = sp.next().unwrap();
     let yell = if let Ok(num) = yell.parse::<i64>() {
-        Yell::Number(num)
+        Number(num)
     } else {
         let mut sp = yell.split(' ');
         let left = sp.next().unwrap().to_string();
@@ -57,7 +134,7 @@ fn parse_monkey(input: &str) -> Monkey {
             "/" => Op::Div,
             _ => panic!()
         };
-        Yell::MathResult(left, right, op)
+        MathResult(left, right, op)
     };
     Monkey { name, yell }
 }
@@ -66,6 +143,7 @@ fn main() {
     let monkeys: Vec<_> = io::stdin().lock().lines()
         .map(|line| parse_monkey(&line.unwrap()))
         .collect();
-    let monkeys_map: HashMap<_, _> = monkeys.iter().map(|m| (&m.name as &str, m)).collect();
+    let monkeys_map: Monkeys = monkeys.iter().map(|m| (m.name.clone(), m.clone())).collect();
     println!("{}", root_number(&monkeys_map));
+    println!("{}", root_equality_test(monkeys_map));
 }
