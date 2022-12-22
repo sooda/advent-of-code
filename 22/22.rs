381bd2ff758122b0df77093bc49a57cccfeee4bd
Konsta Hölttä <sooda@iki.fi>
2022-12-22 13:29:53 +0200

    22b.0 input device isn't what the monkeys drew
    The input device isn't that map but the faces of a cube folded flat, so
    the wrapping rules are quite different.
    
    How the edge pairs are mapped is not quite trivial to derive so the
    mapping is hardcoded for now, and only done for the sample input in this
    patch. The algorithm seems to be fine though:
    
    For each open edge not immediately connected to another face in the map
    data, encode the equivalent edge on the other face connected to it. The
    equivalent edge is the face index (as x, y), how to turn the axis that
    runs along the source edge, and where the origin of the axis lies in the
    pair edge. The source edge axis is known from the direction the edge is
    headed at; the axis direction is the same as with major axes, and the
    origin is determined by that rule.
    
    This brings the position to the right spot on the neighbor face on the
    edge and also flips the heading with the same transform as the edge axis
    vector is rotated.
    
    It's probably possible to write these in more concise manner (such as "U
    turn to the right") but for now the abstraction level is low.
    
    Figuring out the edge pairs from the input data could be tricky, so
    another plan for automatic edge discovery would be to think the cube as
    a known fold-out shape with well defined edge warps and then to
    transform the source faces into this "canonical form". To get the final
    coordinate in map reality, the coordinate would be transformed back from
    the virtual reality in the canonical form.

--- 22/22.rs
+++ 22/22.rs
@@ -24,10 +24,12 @@ use Rotation::*;
 type Route = Vec<(Rotation, i64)>;
 type Notes = (Map, Route);
 
+// like ccw
 fn left(dir: Coords) -> Coords {
     (dir.1, -dir.0)
 }
 
+// like cw
 fn right(dir: Coords) -> Coords {
     (-dir.1, dir.0)
 }
@@ -36,17 +38,131 @@ fn add(a: Coords, b: Coords) -> Coords {
     (a.0 + b.0, a.1 + b.1)
 }
 
-fn wrap(map: &Map, pos: Coords, dir: Coords) -> Coords {
-    *match dir {
-        (1,  0) => map.keys().filter(|&&(_, y)| y == pos.1).min_by_key(|&(x, _)| x).unwrap(),
-        (-1, 0) => map.keys().filter(|&&(_, y)| y == pos.1).max_by_key(|&(x, _)| x).unwrap(),
-        (0,  1) => map.keys().filter(|&&(x, _)| x == pos.0).min_by_key(|&(_, y)| y).unwrap(),
-        (0, -1) => map.keys().filter(|&&(x, _)| x == pos.0).max_by_key(|&(_, y)| y).unwrap(),
-        _ => panic!()
-    }
+fn sub(a: Coords, b: Coords) -> Coords {
+    (a.0 - b.0, a.1 - b.1)
 }
 
-fn final_pos(notes: &Notes) -> (Coords, Coords) {
+fn scale(a: Coords, b: Coords) -> Coords {
+    (a.0 * b.0, a.1 * b.1)
+}
+
+fn wrap_flat(map: &Map, pos: Coords, dir: Coords) -> (Coords, Coords) {
+    (*match dir {
+        (1,  0) => map.keys().filter(|&&(_, y)| y == pos.1).min_by_key(|&(x, _)| x),
+        (-1, 0) => map.keys().filter(|&&(_, y)| y == pos.1).max_by_key(|&(x, _)| x),
+        (0,  1) => map.keys().filter(|&&(x, _)| x == pos.0).min_by_key(|&(_, y)| y),
+        (0, -1) => map.keys().filter(|&&(x, _)| x == pos.0).max_by_key(|&(_, y)| y),
+        _ => panic!()
+    }.unwrap(), dir)
+}
+
+// row-major order
+type Mat = (Coords, Coords);
+
+fn matmul(mat: Mat, v: Coords) -> Coords {
+    let ((a, b), (c, d)) = mat;
+    let (v0, v1) = v;
+    ((a * v0 + b * v1), (c * v0 + d * v1))
+}
+
+fn wrap_cube(map: &Map, pos: Coords, dir: Coords) -> (Coords, Coords) {
+    // remember, y grows down. these aoc coordinate systems...
+    let _flip_y = ((1, 0), (0, -1));
+    let _flip_x = ((-1, 0), (0, 1));
+    let fullturn = ((-1, 0), (0, -1));
+    let turn_ccw = ((0, 1), (-1, 0)); // prev y becomes x, prev x becomes -y
+    let turn_cw = ((0, -1), (1, 0)); // prev x becomes y, prev y becomes -x
+    // Tform: (dest face index, edge axis rot matrix, offset of origin of new axis)
+    // (could also use homogeneous coordinates for just one matrix...)
+    // newpos = dest_face_origin + axis_rotate(src_faceoff - src_ax_offset) + dest_ax_offset
+    // src_ax_offset derived from exit direction to avoid redundancy
+    type Tform = Option<(Coords, Mat, Coords)>;
+    let facesize = match map.len() {
+        96 /* 6*4*4 */ => 4,
+        15000 /* 6*50*50 */ => 50,
+        _ => panic!("unusual input"),
+    };
+    // TODO: make these into lang symbols like U left, U right, upside-U left, just-turn-axis-right
+    // TODO: derive dest_ax_offset from edge transition
+    let faceend = facesize - 1;
+    // (dest face, (right left bottom top))
+    let transform_map: Vec<(Coords, (Tform, Tform, Tform, Tform))>
+        = match map.len() {
+        96 /* 6*4*4 */ => vec![
+            ((2, 0), (
+                Some(((3, 2), fullturn, (faceend, faceend))),
+                Some(((1, 1), turn_ccw, (0, 0))),
+                None,
+                Some(((0, 1), fullturn, (0, 0))),
+            )),
+            ((0, 1), (
+                None,
+                Some(((3, 2), turn_cw, (faceend, faceend))),
+                Some(((2, 2), fullturn, (faceend, faceend))),
+                Some(((2, 0), fullturn, (faceend, 0))),
+            )),
+            ((1, 1), (
+                None,
+                None,
+                Some(((2, 2), turn_ccw, (0, 0))),
+                Some(((2, 0), turn_cw, (0, 0))),
+            )),
+            ((2, 1), (
+                Some(((3, 2), turn_cw, (faceend, 0))),
+                None,
+                None,
+                None,
+            )),
+            ((2, 2), (
+                None,
+                Some(((1, 1), turn_cw, (faceend, 0))),
+                Some(((0, 1), fullturn, (faceend, faceend))),
+                None,
+            )),
+            ((3, 2), (
+                Some(((2, 0), fullturn, (faceend, faceend))),
+                None,
+                Some(((0, 1), turn_ccw, (0, faceend))),
+                Some(((2, 1), turn_ccw, (0, faceend))),
+            )),
+        ],
+        15000 /* 6*50*50 */ => vec![],
+        _ => panic!("unusual input"),
+    };
+    // note: x,y at the edge, not necessarily overlapping major axes of the face (at 0,0)
+    // so src edge vectors also managed in the transform
+
+    let src_faceidx = (pos.0 / facesize, pos.1 / facesize);
+    let src_faceoff = (pos.0 % facesize, pos.1 % facesize);
+
+    let transforms = transform_map.iter().find(|t| t.0 == src_faceidx).expect("this face does not flip").1;
+    let (tform, src_ax_offset) = match dir {
+        (1,  0) => (transforms.0.expect("flip on contig edge?"), (faceend, 0)),
+        (-1, 0) => (transforms.1.expect("flip on contig edge?"), (0, 0)),
+        (0,  1) => (transforms.2.expect("flip on contig edge?"), (0, faceend)),
+        (0, -1) => (transforms.3.expect("flip on contig edge?"), (0, 0)),
+        _ => panic!()
+    };
+    // make src coords run along an axis on the edge so the rotation just works
+    let src_edgeoff = sub(src_faceoff, src_ax_offset);
+
+    let (dest_faceidx, axisrotate, dest_ax_offset) = tform;
+    let dest_face_origin = scale(dest_faceidx, (facesize, facesize));
+
+    let dest_faceoff = add(matmul(axisrotate, src_edgeoff), dest_ax_offset);
+
+    let finalpos = add(dest_face_origin, dest_faceoff);
+    let finaldir = matmul(axisrotate, dir);
+    if false {
+        println!("transformed {:?} (off {:?}) into {:?} (off {:?})",
+            pos, src_faceoff,
+            add(dest_face_origin, dest_faceoff), dest_faceoff);
+    }
+
+    (finalpos, finaldir)
+}
+
+fn final_pos<F: Fn(&Map, Coords, Coords) -> (Coords, Coords)>(notes: &Notes, wrap: F) -> (Coords, Coords) {
     let mut pos: Coords = *notes.0.keys().filter(|&&(_, y)| y == 0).min_by_key(|&(x, _)| x).unwrap();
     let mut dir = (0, -1); // up
     for &(rot, steps) in &notes.1 {
@@ -56,25 +172,28 @@ fn final_pos(notes: &Notes) -> (Coords, Coords) {
         };
         for _ in 0..steps {
             let next = add(pos, dir);
-            let (next, tile) = match notes.0.get(&next) {
-                Some(tile) => (next, tile),
+            let (next, ndir, tile) = match notes.0.get(&next) {
+                Some(tile) => (next, dir, tile),
                 None => {
-                    let next = wrap(&notes.0, pos, dir);
-                    (next, notes.0.get(&next).unwrap())
+                    if false {
+                        println!("found nothin at from {:?} to {:?}", pos, next);
+                    }
+                    let (next, nextdir) = wrap(&notes.0, pos, dir);
+                    (next, nextdir, notes.0.get(&next).unwrap())
                 }
             };
-            let newpos = match tile {
-                Open => next,
-                Wall => pos,
+            let (newpos, newdir) = match tile {
+                Open => (next, ndir),
+                Wall => (pos, dir),
             };
             pos = newpos;
+            dir = newdir;
         }
     }
     (pos, dir)
 }
 
-fn final_password(notes: &Notes) -> i64 {
-    let (pos, dir) = final_pos(notes);
+fn password((pos, dir): (Coords, Coords)) -> i64 {
     let facing = match dir {
         (1,  0) => 0,
         (0,  1) => 1,
@@ -83,6 +202,27 @@ fn final_password(notes: &Notes) -> i64 {
         _ => panic!()
     };
     1000 * (pos.1 + 1) + 4 * (pos.0 + 1) + facing
+}
+
+fn final_password_flat(notes: &Notes) -> i64 {
+    password(final_pos(notes, wrap_flat))
+}
+
+/*
+ * Map into this form, depending on if the input is taller than wide or not:
+ * .F.  ..G.
+ * .E.  FEAD
+ * BAG  ..B.
+ * .D.
+ */
+fn canonical_cube(map: Map) -> Map {
+    // FIXME: non-hardcoded face mapping
+    // - perhaps map only the wrapping rules, not the data
+    map
+}
+
+fn final_password_cube(notes: Notes) -> i64 {
+    password(final_pos(&(canonical_cube(notes.0), notes.1), wrap_cube))
 }
 
 fn parse_map(note: &[String]) -> Map {
@@ -129,5 +269,6 @@ fn main() {
         .map(|line| line.unwrap())
         .collect();
     let notes = parse_notes(&data);
-    println!("{}", final_password(&notes));
+    println!("{}", final_password_flat(&notes));
+    println!("{}", final_password_cube(notes));
 }
