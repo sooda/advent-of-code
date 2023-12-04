e44d0c7b4d083d06a6366bba3d2e9f6ef95ee7fb
Konsta Hölttä <sooda@iki.fi>
2023-12-04 09:10:20 +0200

    4b win more scratchcards
    The only way to win is to play more? Each match gets us more cards from
    the list that follows the win. Instead of the given algorithm, count the
    winning cards recursively. This does a whole lot of repetition and is as
    stupid as doing fibonacci recursively, but the input size is small
    enough to run in a second which is good enough for now and the code is
    nice and readable like this.

--- 23/4.rs
+++ 23/4.rs
@@ -1,12 +1,17 @@
 use std::io::{self, BufRead};
 use std::collections::HashSet;
 
+struct Card {
+    winning: HashSet<u32>,
+    hand: HashSet<u32>,
+}
+
 fn parse_list(s: &str) -> HashSet<u32> {
     // filter_map because "" isn't a number, so skip them
     s.split(' ').filter_map(|n| n.parse::<u32>().ok()).collect()
 }
 
-fn parse_for_points(card_spec: &str) -> u32 {
+fn parse_card(card_spec: &str) -> Card {
     let mut sp = card_spec.split(" | ");
     let left = sp.next().unwrap();
     let hand_str = sp.next().unwrap();
@@ -15,15 +20,30 @@ fn parse_for_points(card_spec: &str) -> u32 {
     let winning = parse_list(winning_str);
     let hand = parse_list(hand_str);
 
-    let n = hand.intersection(&winning).count();
+    Card { winning, hand }
+}
+
+fn points(card: &Card) -> u32 {
+    let n = card.hand.intersection(&card.winning).count();
 
     if n == 0 { 0 } else { 1 << (n - 1) }
 }
 
-fn main() {
-    let points: Vec<u32> = io::stdin().lock().lines()
-        .map(|line| parse_for_points(&line.unwrap()))
-        .collect();
-    println!("{}", points.iter().sum::<u32>());
+fn won_cards(cards: &[Card], play: usize) -> u32 {
+    let card = &cards[play];
+    let n = card.hand.intersection(&card.winning).count();
+
+    1 + (1..=n).map(|i| won_cards(cards, play + i)).sum::<u32>()
 }
 
+fn game(cards: &[Card]) -> u32 {
+    (0..cards.len()).map(|i| won_cards(cards, i)).sum::<u32>()
+}
+
+fn main() {
+    let cards: Vec<Card> = io::stdin().lock().lines()
+        .map(|line| parse_card(&line.unwrap()))
+        .collect();
+    println!("{}", cards.iter().map(points).sum::<u32>());
+    println!("{}", game(&cards));
+}
