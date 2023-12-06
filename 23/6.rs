c38843f2ef8ed7685cb0821043fa6d521d7cb2f6
Konsta Hölttä <sooda@iki.fi>
2023-12-06 09:25:22 +0200

    6b very bad keming
    The playtime is more and distance too, actually. Combine the parsed
    numbers together because that's more fun than parsing again with
    whitespace deleted; 123 45 becomes 12345 etc.
    
    The search is still just brute force.
    
    So disappointed that the puzzle links to wikipedia instead of that one
    xkcd though.

--- 23/6.rs
+++ 23/6.rs
@@ -1,23 +1,31 @@
 use std::io::{self, Read};
 
-fn beats(time: i32, best_distance: i32) -> i32 {
+fn beats(time: i64, best_distance: i64) -> i64 {
     (1..time).filter(|button_press| {
         let travel = (time - button_press) * button_press;
         travel > best_distance
-    }).count() as i32
+    }).count() as i64
 }
 
-fn ways_to_beat(records: &[(i32, i32)]) -> i32 {
+fn ways_to_beat(records: &[(i64, i64)]) -> i64 {
     records.iter().map(|r| beats(r.0, r.1)).fold(1, |acc, x| acc * x)
 }
 
-fn parse(file: &str) -> Vec<(i32, i32)> {
+fn join(total: i64, next: i64) -> i64 {
+    total * 10i64.pow(next.ilog10() + 1) as i64 + next
+}
+
+fn fix_kerning(records: &[(i64, i64)]) -> (i64, i64) {
+    records.iter().fold((0, 0), |acc, x| (join(acc.0, x.0), join(acc.1, x.1)))
+}
+
+fn parse(file: &str) -> Vec<(i64, i64)> {
     let mut l = file.lines();
     let times = l.next().unwrap();
     let distances = l.next().unwrap();
-    times.split(' ').flat_map(|s| s.parse::<i32>().ok())
+    times.split(' ').flat_map(|s| s.parse::<i64>().ok())
         .zip(
-            distances.split(' ').flat_map(|s| s.parse::<i32>().ok())
+            distances.split(' ').flat_map(|s| s.parse::<i64>().ok())
         )
         .collect()
 }
@@ -25,5 +33,8 @@ fn parse(file: &str) -> Vec<(i32, i32)> {
 fn main() {
     let mut file = String::new();
     io::stdin().read_to_string(&mut file).unwrap();
-    println!("{:?}", ways_to_beat(&parse(&file)));
+    let paper = parse(&file);
+    println!("{:}", ways_to_beat(&paper));
+    let fixed = fix_kerning(&paper);
+    println!("{:}", beats(fixed.0, fixed.1));
 }
