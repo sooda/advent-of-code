d438e9dabaa66c9d2f9c8214077d81c013cc254c
Konsta Hölttä <sooda@iki.fi>
2023-12-08 08:15:40 +0200

    8b map is for ghosts
    Now a lot of network traversals happen simultaneously. Turns out that
    the route loops and the directions are formulated such that just least
    common multiple of all A-Z lengths is sufficient to find how long it
    takes until all ghosts dance to the end exactly; no CRT needed.

--- 23/8.rs
+++ 23/8.rs
@@ -3,16 +3,37 @@ use std::collections::HashMap;
 
 type Map = HashMap<String, (String, String)>;
 
-fn steps(dirs: &[bool], map: &Map, begin: &str, end: &str) -> usize {
+fn steps<F>(dirs: &[bool], map: &Map, begin: &str, end: F) -> usize
+where F: Fn(&str) -> bool
+{
     let mut current = begin;
     let mut ways = dirs.iter().cycle();
     let mut n = 0;
-    while current != end {
+    while !end(current) {
         let pair = map.get(current).unwrap();
         current = if *ways.next().unwrap() { &pair.1 } else { &pair.0 };
         n += 1;
     }
     n
+}
+
+fn gcd(mut a: usize, mut b: usize) -> usize {
+    while a != 0 {
+        let c = b % a;
+        b = a;
+        a = c;
+    }
+    b
+}
+
+fn lcm(a: usize, b: usize) -> usize {
+    a * b / gcd(a, b)
+}
+
+fn steps_ghost(dirs: &[bool], map: &Map) -> usize {
+    let startnodes = map.keys().filter(|k| k.ends_with('A')).map(|s| s as &str).collect::<Vec<_>>();
+    let counts = startnodes.iter().map(|begin| steps(dirs, map, begin, |s| s.ends_with('Z'))).collect::<Vec<_>>();
+    counts.iter().fold(1, |acc, &x| lcm(acc, x))
 }
 
 fn parse(file: &str) -> (Vec<bool>, Map) {
@@ -37,5 +58,6 @@ fn main() {
     let mut file = String::new();
     io::stdin().read_to_string(&mut file).unwrap();
     let (dirs, map) = parse(&file);
-    println!("{}", steps(&dirs, &map, "AAA", "ZZZ"));
+    println!("{}", steps(&dirs, &map, "AAA", |x| x == "ZZZ"));
+    println!("{}", steps_ghost(&dirs, &map));
 }
