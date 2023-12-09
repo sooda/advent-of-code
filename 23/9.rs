5c03d2ff3406bbbeff6fce56d4bdc405df9d6a60
Konsta Hölttä <sooda@iki.fi>
2023-12-09 08:23:13 +0200

    9b extrapolate backwards
    Integrate the deltas also in the front of the number series, not just
    the back.

--- 23/9.rs
+++ 23/9.rs
@@ -1,19 +1,23 @@
 use std::io::{self, BufRead};
 
-fn find_next(history: &[i32]) -> i32 {
+fn find_prev_next(history: &[i32]) -> (i32, i32) {
     if history.iter().all(|&x| x == 0) {
-        0
+        (0, 0)
     } else {
         let deltas = history.iter()
             .zip(history.iter().skip(1))
             .map(|(x0, x1)| x1 - x0)
             .collect::<Vec<i32>>();
-        history.last().unwrap() + find_next(&deltas)
+        let next = find_prev_next(&deltas);
+        (history.first().unwrap() - next.0,
+         history.last().unwrap() + next.1)
     }
 }
 
-fn next_sum(histories: &[Vec<i32>]) -> i32 {
-    histories.iter().map(|h| find_next(h)).sum::<i32>()
+fn prev_next_sum(histories: &[Vec<i32>]) -> (i32, i32) {
+    histories.iter()
+        .map(|h| find_prev_next(h))
+        .fold((0, 0), |acc, x| (acc.0 + x.0, acc.1 + x.1))
 }
 
 fn parse_numbers(line: &str) -> Vec<i32> {
@@ -24,5 +28,7 @@ fn main() {
     let histories: Vec<Vec<i32>> = io::stdin().lock().lines()
         .map(|line| parse_numbers(&line.unwrap()))
         .collect();
-    println!("{}", next_sum(&histories));
+    let pn = prev_next_sum(&histories);
+    println!("{}", pn.1);
+    println!("{}", pn.0);
 }
