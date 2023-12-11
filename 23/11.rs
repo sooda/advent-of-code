37766be75e2017059994e788e1425eaaf8efacb5
Konsta Hölttä <sooda@iki.fi>
2023-12-11 08:05:31 +0200

    11b much older than initially estimated
    Instead of moving the galaxies by 1 when expanding, move them by some
    configurable amount, which for part 2 is one million.

--- 23/11.rs
+++ 23/11.rs
@@ -1,23 +1,23 @@
 use std::io::{self, BufRead};
 
-type Map = Vec<(i32, i32)>;
+type Map = Vec<(i64, i64)>;
 
-fn expand(map: &mut Map) {
+fn expand(map: &mut Map, n: i64) {
     let maxx = map.iter().map(|&(x, _)| x).max().unwrap();
     let maxy = map.iter().map(|&(_, y)| y).max().unwrap();
     for x in (0..=maxx).rev() {
         if !map.iter().any(|&(xi, _)| xi == x) {
-            map.iter_mut().filter(|(xi, _)| *xi > x).for_each(|p| p.0 += 1);
+            map.iter_mut().filter(|(xi, _)| *xi > x).for_each(|p| p.0 += n);
         }
     }
     for y in (0..=maxy).rev() {
         if !map.iter().any(|&(_, yi)| yi == y) {
-            map.iter_mut().filter(|(_, yi)| *yi > y).for_each(|p| p.1 += 1);
+            map.iter_mut().filter(|(_, yi)| *yi > y).for_each(|p| p.1 += n);
         }
     }
 }
 
-fn short_paths(map: &Map) -> i32 {
+fn short_paths(map: &Map) -> i64 {
     let mut sum = 0;
     for (i, a) in map.iter().enumerate() {
         for b in map.iter().skip(i + 1) {
@@ -33,11 +33,17 @@ fn main() {
         .fold(Map::new(), |mut map, (y, line)| {
             line.unwrap().chars().enumerate().for_each(|(x, ch)| {
                 if ch == '#' {
-                    map.push((x as i32, y as i32));
+                    map.push((x as i64, y as i64));
                 }
             });
             map
         });
-    expand(&mut map);
+    let mut map2 = map.clone();
+    let mut map3 = map.clone();
+    expand(&mut map, 1);
     println!("{}", short_paths(&map));
+    expand(&mut map2, 9);
+    println!("10x: {}", short_paths(&map2));
+    expand(&mut map3, 1_000_000 - 1);
+    println!("{}", short_paths(&map3));
 }
