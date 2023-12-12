bcbc209b4e8adff9681c8ada3052ce35a42587ad
Konsta Hölttä <sooda@iki.fi>
2023-12-12 10:32:06 +0200

    12b the records were folded up this whole time
    The search for part 1 is easily made faster and much more suitable for
    longer sequences by just caching. Ah, the solution to all DP problems,
    simply memoize it. The cache stores slices so this got a bit nasty with
    explicit lifetimes. Some sort of generic "cache wrapper" could be
    useful for AoCs in general, if possible.

--- 23/12.rs
+++ 23/12.rs
@@ -1,8 +1,11 @@
 use std::io::{self, BufRead};
+use std::collections::HashMap;
 
 type Spec = (Vec<char>, Vec<usize>);
 
-fn try_broken_sequence(springs: &[char], counts: &[usize]) -> usize {
+type Mem<'a> = HashMap<(&'a [char], &'a [usize]), usize>;
+
+fn try_broken_sequence<'a>(springs: &'a [char], counts: &'a [usize], mem: &mut Mem<'a>) -> usize {
     let seq_len = counts[0];
     let pattern_fits = springs.iter().take_while(|&&ch| ch == '?' || ch == '#').count() >= seq_len;
     let fits_exactly = seq_len == springs.len() && counts.len() == 1;
@@ -12,49 +15,73 @@ fn try_broken_sequence(springs: &[char], counts: &[usize]) -> usize {
         1
     } else if pattern_fits && no_mismatch {
         // consume this sequence and force the next one operational
-        arrangements(&springs[seq_len + 1..], &counts[1..])
+        arrangements(&springs[seq_len + 1..], &counts[1..], mem)
     } else {
         // didn't work, prune
         0
     }
 }
 
-fn try_operational(springs: &[char], counts: &[usize]) -> usize {
+fn try_operational<'a>(springs: &'a [char], counts: &'a [usize], mem: &mut Mem<'a>) -> usize {
     match springs.first() {
         None => 0,
         Some('#') => 0,
         Some('.') => {
             // trivial case
-            arrangements(&springs[1..], counts)
+            arrangements(&springs[1..], counts, mem)
         },
         Some('?') => {
             // try to fit one operational spring here
-            arrangements(&springs[1..], counts)
+            arrangements(&springs[1..], counts, mem)
         }
         _ => panic!()
     }
 }
 
-fn try_consume(springs: &[char], counts: &[usize]) -> usize {
+fn try_consume<'a>(springs: &'a [char], counts: &'a [usize], mem: &mut Mem<'a>) -> usize {
     if counts.len() == 0 {
-        try_operational(springs, counts)
+        try_operational(springs, counts, mem)
     } else {
-        try_broken_sequence(springs, counts) + try_operational(springs, counts)
+        try_broken_sequence(springs, counts, mem) + try_operational(springs, counts, mem)
     }
 }
 
-fn arrangements(springs: &[char], counts: &[usize]) -> usize {
-    match springs.first() {
+fn arrangements<'a>(springs: &'a [char], counts: &'a [usize], mem: &mut Mem<'a>) -> usize {
+    if let Some(x) = mem.get(&(springs, counts)) {
+        return *x;
+    }
+    let x = match springs.first() {
         None if counts.len() == 0 => 1, // happy end of a recursion
         None => 0,
-        Some('.') => arrangements(&springs[1..], counts),
-        Some('#') | Some('?') => try_consume(springs, counts),
+        Some('.') => arrangements(&springs[1..], counts, mem),
+        Some('#') | Some('?') => try_consume(springs, counts, mem),
         _ => panic!()
-    }
+    };
+    mem.insert((springs, counts), x);
+    x
 }
 
 fn sum_arrangements(rows: &[Spec]) -> usize {
-    rows.iter().map(|r| arrangements(&r.0, &r.1)).sum::<usize>()
+    rows.iter().map(|r| arrangements(&r.0, &r.1, &mut Mem::new())).sum::<usize>()
+}
+
+fn unfold(springs: &[char], counts: &[usize]) -> Spec {
+    let springs = springs.iter()
+        .copied()
+        .chain(std::iter::once('?'))
+        .cycle()
+        .take(5 * springs.len() + 4)
+        .collect::<Vec<_>>();
+    let counts = counts.iter()
+        .copied()
+        .cycle()
+        .take(5 * counts.len())
+        .collect();
+    (springs, counts)
+}
+
+fn unfold_rows(rows: &[Spec]) -> Vec<Spec> {
+    rows.iter().map(|r| unfold(&r.0, &r.1)).collect()
 }
 
 // .??..??...?##. 1,1,3
@@ -70,4 +97,5 @@ fn main() {
         .map(|row| parse_row(&row.unwrap()))
         .collect::<Vec<_>>();
     println!("{}", sum_arrangements(&rows));
+    println!("{}", sum_arrangements(&unfold_rows(&rows)));
 }
