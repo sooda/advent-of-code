8b3dcdfb5c0c59a7514123c68b0d1b40df1460e1
Konsta Hölttä <sooda@iki.fi>
2024-12-03 07:57:00 +0200

    3b handle conditional statements
    Revise the regex to also match dos and don'ts. As an (awful|clever)
    hack, shape the new conditional matching to have the same number of
    state such that the same c.extract() still works conveniently.

--- 24/3.rs
+++ 24/3.rs
@@ -3,11 +3,18 @@ use std::io::{self, Read};
 extern crate regex;
 use regex::Regex;
 
-fn multiplications(program: &str) -> i32 {
-    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+fn multiplications(program: &str, use_do: bool) -> i32 {
+    let re = Regex::new(r"(?:(mul)\((\d+),(\d+)\))|((d)(o)\(\))|((d)(o)n't\(\))").unwrap();
     let mut total_sum = 0;
-    for (_, [a, b]) in re.captures_iter(program).map(|c| c.extract()) {
-        total_sum += a.parse::<i32>().unwrap() * b.parse::<i32>().unwrap();
+    let mut enabled = true;
+    for (_, [op, a, b]) in re.captures_iter(program).map(|c| c.extract()) {
+        match op {
+            "do()" => enabled = true,
+            "don't()" => enabled = false || !use_do,
+            "mul" if enabled => total_sum += a.parse::<i32>().unwrap() * b.parse::<i32>().unwrap(),
+            "mul" if !enabled => (),
+            _ => panic!("unhandled op")
+        }
     }
     total_sum
 }
@@ -15,6 +22,6 @@ fn multiplications(program: &str) -> i32 {
 fn main() {
     let mut file = String::new();
     io::stdin().read_to_string(&mut file).unwrap();
-    println!("{}", multiplications(&file));
+    println!("{}", multiplications(&file, false));
+    println!("{}", multiplications(&file, true));
 }
-
