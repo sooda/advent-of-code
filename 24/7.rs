8d9094037116fe5d7999160c1bcdfc08c86022a2
Konsta Hölttä <sooda@iki.fi>
2024-12-07 07:31:49 +0200

    7b well-hidden elephants
    Implement the new concatenation operator and just add it to the search;
    carrying the "this is part b" flag along is not pretty but is good
    enough for today. Certainly the limited selection of operators is chosen
    to give a hint to short-circuit the search early as the number can only
    grow (and become too big), but we're still fast enough.

--- 24/7.rs
+++ 24/7.rs
@@ -3,21 +3,26 @@ use std::io::{self, BufRead};
 // result and test values
 type Equation = (i64, Vec<i64>);
 
-fn try_resolve(result: i64, current: i64, remaining: &[i64]) -> bool {
+fn concat(a: i64, b: i64) -> i64 {
+    a * 10i64.pow(b.ilog10() + 1) + b
+}
+
+fn try_resolve(result: i64, current: i64, remaining: &[i64], also_concat: bool) -> bool {
     if remaining.len() == 0 {
         current == result
     } else {
-        try_resolve(result, current + remaining[0], &remaining[1..]) ||
-            try_resolve(result, current * remaining[0], &remaining[1..])
+        try_resolve(result, current + remaining[0], &remaining[1..], also_concat) ||
+            try_resolve(result, current * remaining[0], &remaining[1..], also_concat) ||
+            also_concat && try_resolve(result, concat(current, remaining[0]), &remaining[1..], also_concat)
     }
 }
 
-fn possibly_true(eq: &Equation) -> bool {
-    try_resolve(eq.0, eq.1[0], &eq.1[1..])
+fn possibly_true(eq: &Equation, also_concat: bool) -> bool {
+    try_resolve(eq.0, eq.1[0], &eq.1[1..], also_concat)
 }
 
-fn total_calibration_result(eqs: &[Equation]) -> i64 {
-    eqs.iter().filter(|&e| possibly_true(e)).map(|e| e.0).sum()
+fn total_calibration_result(eqs: &[Equation], also_concat: bool) -> i64 {
+    eqs.iter().filter(|&e| possibly_true(e, also_concat)).map(|e| e.0).sum()
 }
 
 fn parse(line: &str) -> Equation {
@@ -31,6 +36,6 @@ fn main() {
     let equations: Vec<Equation> = io::stdin().lock().lines()
         .map(|line| parse(&line.unwrap())
             ).collect();
-    println!("{}", total_calibration_result(&equations));
+    println!("{}", total_calibration_result(&equations, false));
+    println!("{}", total_calibration_result(&equations, true));
 }
-
