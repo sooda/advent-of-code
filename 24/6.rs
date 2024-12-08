eb02b40e65a43577cdcafbd3ee4e25f0e27427e8
Konsta Hölttä <sooda@iki.fi>
2024-12-08 10:42:56 +0200

    Optimize 6 for under 0.4 seconds
    First, only consider the positions the guard actually visits; this
    already cuts the search space to about 30% of the map (~two seconds).
    The key trick though is to record the path of guard poses and loop it
    backwards, trying each position for the obstruction and simulating the
    route again from the pose just prior to that one. Looping backwards
    allows to iteratively clear the visit pose map so that it matches the
    path until that point and can be reused.
    
    However, there's one corner case: some positions are visited more than
    once because the walk paths can cross. If a position is visited earlier
    with another heading, don't try obstructing it and simulating later
    because that later would not happen anymore.
    
    (The vec^2 for the map is becoming real annoying. Gotta make a toolkit.)

--- 24/6.rs
+++ 24/6.rs
@@ -1,7 +1,12 @@
+#![feature(let_chains)]
+
 use std::io::{self, BufRead};
-use std::collections::HashSet;
+use std::collections::HashMap;
 
 type Map = Vec<Vec<char>>;
+type Pos = (i32, i32);
+type Visits = HashMap<(Pos, Pos), usize>;
+type Path = Vec<(Pos, Pos)>;
 
 fn start_pos(map: &Map) -> (i32, i32) {
     let (x, y, _) = map.iter()
@@ -12,18 +17,25 @@ fn start_pos(map: &Map) -> (i32, i32) {
     (x as i32, y as i32)
 }
 
-fn route_positions(map: &Map) -> Option<usize> {
-    let (mut x, mut y) = start_pos(map);
-    let (mut dx, mut dy) = (0, -1); // up
+fn right(p: Pos) -> Pos {
+    (-p.1, p.0)
+}
+
+fn route_positions(map: &Map, start: (Pos, Pos), mut time: usize, mut visits: Visits) -> Option<(usize, Visits, Path)> {
+    let ((mut x, mut y), (mut dx, mut dy)) = start;
     let w = map[0].len() as i32;
     let h = map.len() as i32;
     let mut positions = vec![vec![false; map[0].len()]; map.len()];
-    let mut visits = HashSet::new();
+    let mut path = Path::new();
+
     loop {
         positions[y as usize][x as usize] = true;
-        if !visits.insert(((x, y), (dx, dy))) {
-            // cycle, can't get to the end
-            return None;
+        path.push(((x, y), (dx, dy)));
+        if let Some(existing_time) = visits.insert(((x, y), (dx, dy)), time) {
+            if existing_time < time {
+                // cycle, can't get to the end
+                return None;
+            }
         }
         if (x == 0 && dx == -1) ||
                 (x == w-1 && dx == 1) ||
@@ -32,31 +44,51 @@ fn route_positions(map: &Map) -> Option<usize> {
             break;
         }
         while map[(y + dy) as usize][(x + dx) as usize] == '#' {
-            (dx, dy) = (-dy, dx);
+            (dx, dy) = right((dx, dy));
         }
         x += dx;
         y += dy;
+        time += 1;
     }
-    Some(positions.into_iter()
+
+    let count = positions.into_iter()
         .map(|row| row.into_iter().filter(|&x| x).count())
-        .sum())
+        .sum();
+    Some((count, visits, path))
 }
 
-fn possible_obstruction(map: &Map, x: usize, y: usize) -> bool {
-    if map[y][x] != '.' {
-        false
-    } else {
-        let mut map = map.clone();
-        map[y][x] = '#';
-        route_positions(&map).is_none()
+fn possible_obstructions(mut map: Map, mut visits: Visits, path: &Path) -> usize {
+    let mut count = 0;
+    for ((time, &player), &obstru) in path.iter().enumerate().zip(path.iter().skip(1)).rev() {
+        // can't get here like this because the path would get obstructed earlier?
+        let this_time = visits.remove(&obstru).unwrap();
+        if let Some(&time) = visits.get(
+                &(obstru.0, right(obstru.1))) && time < this_time {
+            continue;
+        }
+        if let Some(&time) = visits.get(
+                &(obstru.0, right(right(obstru.1)))) && time < this_time {
+            continue;
+        }
+        if let Some(&time) = visits.get(
+                &(obstru.0, right(right(right(obstru.1))))) && time < this_time {
+            continue;
+        }
+
+        let orig_cell = std::mem::replace(&mut map[obstru.0.1 as usize][obstru.0.0 as usize], '#');
+        let cycled = route_positions(&map, player, time, visits.clone()).is_none();
+        if false { // flip this to double check
+            let gold = route_positions(&map, (start_pos(&map), (0, -1)), 0, Visits::new()).is_none();
+            if cycled != gold {
+                panic!("mismatch at {:?} cycled {} gold {}", (player, obstru), cycled, gold);
+            }
+        }
+        map[obstru.0.1 as usize][obstru.0.0 as usize] = orig_cell;
+        if cycled {
+            count += 1;
+        }
     }
-}
-
-fn possible_obstructions(map: &Map) -> usize {
-    (0..map.len())
-        .flat_map(|y| (0..map[0].len()).map(move |x| (x, y)))
-        .filter(|&(x, y)| possible_obstruction(&map, x, y))
-        .count()
+    count
 }
 
 fn main() {
@@ -64,6 +96,8 @@ fn main() {
         .map(|line| line.unwrap()
              .chars().collect()
             ).collect();
-    println!("{}", route_positions(&map).unwrap());
-    println!("{}", possible_obstructions(&map));
+    let (counts, visits, path) = route_positions(
+        &map, (start_pos(&map), (0, -1)), 0, Visits::new()).unwrap();
+    println!("{}", counts);
+    println!("{}", possible_obstructions(map, visits, &path));
 }
