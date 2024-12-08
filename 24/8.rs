a5013b7b6030cd34a63d332950ab9a7c86a2c4fd
Konsta Hölttä <sooda@iki.fi>
2024-12-08 07:51:11 +0200

    8b resonant harmonics
    Now instead of just two points, loop all possible points in the map and
    place antinodes at each. The vector placement direction is also flipped
    because both antennas also get an antinode.

--- 24/8.rs
+++ 24/8.rs
@@ -3,15 +3,8 @@ use std::collections::HashMap;
 
 type Map = Vec<Vec<char>>;
 
-fn unique_antinodes(map: &Map) -> usize {
-    let mut antinodes = vec![vec![false; map[0].len()]; map.len()];
-    let mut antennas = HashMap::<char, Vec<(i32, i32)>>::new();
-    let mut put = |x, y| {
-        if x >= 0 && x < antinodes[0].len() as i32 && y >= 0 && y < antinodes.len() as i32 {
-            antinodes[y as usize][x as usize] = true;
-        }
-    };
-
+fn find_antennas(map: &Map) -> HashMap<char, Vec<(i32, i32)>> {
+    let mut antennas = HashMap::new();
     for (y, row) in map.iter().enumerate() {
         for (x, &ch) in row.iter().enumerate() {
             if ch != '.' {
@@ -19,13 +12,51 @@ fn unique_antinodes(map: &Map) -> usize {
             }
         }
     }
+    antennas
+}
 
-    for (_kind, locations) in &antennas {
+fn put(antinodes: &mut Vec<Vec<bool>>, x: i32, y: i32) -> bool {
+    if x >= 0 && x < antinodes[0].len() as i32 && y >= 0 && y < antinodes.len() as i32 {
+        antinodes[y as usize][x as usize] = true;
+        true
+    } else {
+        false
+    }
+}
+
+fn unique_antinodes(map: &Map) -> usize {
+    let mut antinodes = vec![vec![false; map[0].len()]; map.len()];
+
+    for (_kind, locations) in find_antennas(map) {
         for (i, a) in locations.iter().enumerate() {
             for b in locations.iter().skip(i + 1) {
                 let (dx, dy) = (b.0 - a.0, b.1 - a.1);
-                put(a.0 - dx, a.1 - dy);
-                put(b.0 + dx, b.1 + dy);
+                put(&mut antinodes, a.0 - dx, a.1 - dy);
+                put(&mut antinodes, b.0 + dx, b.1 + dy);
+            }
+        }
+    }
+
+    antinodes.into_iter()
+        .map(|row| row.into_iter().filter(|&x| x).count())
+        .sum()
+}
+
+fn unique_resonant_antinodes(map: &Map) -> usize {
+    let mut antinodes = vec![vec![false; map[0].len()]; map.len()];
+
+    for (_kind, locations) in find_antennas(map) {
+        for (i, a) in locations.iter().enumerate() {
+            for b in locations.iter().skip(i + 1) {
+                let (dx, dy) = (b.0 - a.0, b.1 - a.1);
+                for mult in 1.. {
+                    let (ddx, ddy) = (mult * dx, mult * dy);
+                    let a_ok = put(&mut antinodes, a.0 + ddx, a.1 + ddy);
+                    let b_ok = put(&mut antinodes, b.0 - ddx, b.1 - ddy);
+                    if !a_ok && !b_ok {
+                        break;
+                    }
+                }
             }
         }
     }
@@ -41,4 +72,5 @@ fn main() {
              .chars().collect()
             ).collect();
     println!("{}", unique_antinodes(&map));
+    println!("{}", unique_resonant_antinodes(&map));
 }
