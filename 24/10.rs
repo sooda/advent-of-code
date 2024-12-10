f1288d51e0676cd034539d15ff63b18be0f50679
Konsta Hölttä <sooda@iki.fi>
2024-12-10 07:59:41 +0200

    10b a second way to measure a trailhead
    Implement the rating calculation; it's exactly the same as for part 1
    with just the visit map removed. This gets the number of paths instead
    of the number of reached high spots.
    
    That copypasta should be cleaned up at once, and even got into some
    trouble by accidentally calling search instead of search2.

--- 24/10.rs
+++ 24/10.rs
@@ -64,10 +64,48 @@ fn trailhead_scores(map: &Map) -> i32 {
     total
 }
 
+fn search2(map: &Map, pos: Pos, target: i32, visits: &mut HashSet<Pos>) -> i32 {
+    if let Some(h) = map.at(pos) {
+        if h == 9 {
+            1
+        } else {
+            let (x, y) = pos;
+            [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)].into_iter()
+                .map(|neigh| {
+                    if map.at(neigh) == Some(target) {
+                        search2(map, neigh, target + 1, visits)
+                    } else {
+                        0
+                    }
+                })
+            .sum()
+        }
+    } else {
+        0
+    }
+}
+
+fn rating(map: &Map, p: Pos) -> i32 {
+    search2(map, p, 1, &mut HashSet::new())
+}
+
+fn trailhead_ratings(map: &Map) -> i32 {
+    let mut total = 0;
+    for (y, row) in map.0.iter().enumerate() {
+        for (x, &h) in row.iter().enumerate() {
+            if h == 0 {
+                total += rating(map, (x as i32, y as i32));
+            }
+        }
+    }
+    total
+}
+
 fn main() {
     let map = Map::new(io::stdin().lock().lines()
         .map(|line| line.unwrap()
              .bytes().map(|c| (c - b'0') as i32).collect::<Vec<_>>()
             ).collect::<Vec<_>>());
     println!("{}", trailhead_scores(&map));
+    println!("{}", trailhead_ratings(&map));
 }
