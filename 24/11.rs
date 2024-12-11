c808aa8870a45f238a82254808a3e7ef89b318ff
Konsta Hölttä <sooda@iki.fi>
2024-12-11 08:06:55 +0200

    11b the infinite corridors are very large
    The stone order in the list does not matter actually and there are a lot
    of duplicates, so group the stones by the number engraved on them. This
    makes blinking much faster: the number of changes is now just the number
    of different engravements. That doesn't increase exponentially.

--- 24/11.rs
+++ 24/11.rs
@@ -1,4 +1,5 @@
 use std::io::{self, BufRead};
+use std::collections::HashMap;
 
 fn blink(mut stones: Vec<u128>, blinks: usize) -> usize {
     for _ in 0..blinks {
@@ -26,11 +27,49 @@ fn blink(mut stones: Vec<u128>, blinks: usize) -> usize {
     stones.len()
 }
 
+fn blink_faster(stones: &[u128], blinks: usize) -> usize {
+    let mut expansion = HashMap::<u128, (u128, Option<u128>)>::new();
+    expansion.insert(0, (1, None));
+    let mut collection = HashMap::<u128, usize>::new();
+    let mut next_collection = HashMap::<u128, usize>::new();
+    for &s in stones {
+        *collection.entry(s).or_insert(0) += 1;
+    }
+    let inc = |hm: &mut HashMap<_, _>, k, v| *hm.entry(k).or_insert(0) += v;
+    for _ in 0..blinks {
+        next_collection.clear();
+        for (&stone, &count) in &collection {
+            if let Some(data) = expansion.get(&stone) {
+                inc(&mut next_collection, data.0, count);
+                if let Some(s) = data.1 {
+                    inc(&mut next_collection, s, count);
+                }
+            } else {
+                let digits = stone.ilog10() + 1;
+                if digits % 2 == 0 {
+                    let base = 10u128.pow(digits / 2);
+                    let (a, b) = (stone / base, stone % base);
+                    expansion.insert(stone, (a, Some(b)));
+                    inc(&mut next_collection, a, count);
+                    inc(&mut next_collection, b, count);
+                } else {
+                    expansion.insert(stone, (stone * 2024, None));
+                    inc(&mut next_collection, stone * 2024, count);
+                }
+            }
+        }
+        std::mem::swap(&mut collection, &mut next_collection);
+    }
+    collection.values().sum()
+}
+
 fn main() {
     let stones: Vec<_> = io::stdin().lock().lines()
         .next().unwrap().unwrap()
         .split(' ').map(|s| s.parse().unwrap())
         .collect();
     println!("{}", blink(stones.clone(), 6));
-    println!("{}", blink(stones, 25));
+    println!("{} (backup)", blink(stones.clone(), 25));
+    println!("{}", blink_faster(&stones, 25));
+    println!("{}", blink_faster(&stones, 75));
 }
