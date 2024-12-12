e655cda885051dda1ca723e6f443efeb057e3de0
Konsta Hölttä <sooda@iki.fi>
2024-12-12 08:58:39 +0200

    12b bulk discount
    Now the fence price is not the perimeter but the number of sides of a
    shape being fenced. Build the regions in the primary search into a
    per-region visit map each time. DFS the edges of this visit map: a cell
    has a top edge if there's nothing above it; recurse such cells left and
    right. Then repeat three more times with the map rotated to keep the
    logic just for top edges.
    
    Another perhaps simpler way would have been to count the corners. For
    these closed loop regions, each side has a corner pair.

--- 24/12.rs
+++ 24/12.rs
@@ -36,10 +36,16 @@ fn add(a: Pos, b: Pos) -> Pos {
     (a.0 + b.0, a.1 + b.1)
 }
 
-fn search(map: &Map, p: Pos, c: char, visits: &mut HashSet<Pos>) -> (usize, usize) {
+fn right(p: Pos) -> Pos {
+    (-p.1, p.0)
+}
+
+fn search(map: &Map, p: Pos, c: char, visits: &mut HashSet<Pos>, region: &mut HashSet<Pos>) -> (usize, usize) {
     if !visits.insert(p) {
         return (0, 0);
     }
+
+    region.insert(p);
 
     let mut area = 1;
     let mut perimeter = 4;
@@ -47,7 +53,7 @@ fn search(map: &Map, p: Pos, c: char, visits: &mut HashSet<Pos>) -> (usize, usiz
         let neighpos = add(p, delta);
         if map.at(neighpos) == Some(c) {
             perimeter -= 1;
-            let (next_area, next_perimeter) = search(map, neighpos, c, visits);
+            let (next_area, next_perimeter) = search(map, neighpos, c, visits, region);
             area += next_area;
             perimeter += next_perimeter;
         }
@@ -58,8 +64,73 @@ fn search(map: &Map, p: Pos, c: char, visits: &mut HashSet<Pos>) -> (usize, usiz
 fn total_fencing_price(map: &Map) -> usize {
     let mut visits = HashSet::new();
     map.iter()
-        .map(|(p, c)| search(map, p, c, &mut visits))
+        .map(|(p, c)| search(map, p, c, &mut visits, &mut HashSet::new()))
         .map(|(area, perimeter)| area * perimeter)
+        .sum()
+}
+
+// "flood fill" a horizontal line over each cell that doesn't have anything above it
+fn count_top(pos: Pos, region: &HashSet<Pos>, visited: &mut HashSet<Pos>) -> usize {
+    if !region.contains(&pos) {
+        return 0;
+    } else if !visited.insert(pos) {
+        return 0;
+    } else if region.contains(&add(pos, (0, -1))) {
+        0
+    } else {
+        count_top(add(pos, (-1, 0)), &region, visited);
+        count_top(add(pos, ( 1, 0)), &region, visited);
+        1
+    }
+}
+
+fn printregion(r: &HashSet<Pos>) {
+    if r.is_empty() {
+        return;
+    }
+    let minx = r.iter().map(|p| p.0).min().unwrap();
+    let maxx = r.iter().map(|p| p.0).max().unwrap();
+    let miny = r.iter().map(|p| p.1).min().unwrap();
+    let maxy = r.iter().map(|p| p.1).max().unwrap();
+    for y in miny..=maxy {
+        for x in minx..=maxx {
+            if r.contains(&(x, y)) {
+                print!("#");
+            } else {
+                print!(".");
+            }
+        }
+        println!();
+    }
+}
+
+fn rotate(region: HashSet<Pos>) -> HashSet<Pos> {
+    region.into_iter().map(right).collect()
+}
+
+fn apply_discount(mut region: HashSet<Pos>) -> usize {
+    let mut total = 0;
+    for i in 0..4 {
+        if i != 0 {
+            region = rotate(region);
+        }
+        if false { printregion(&region); }
+        let mut visited = HashSet::new();
+        total += region.iter()
+            .map(|&pos| count_top(pos, &region, &mut visited))
+            .sum::<usize>();
+    }
+    total
+}
+
+fn total_fencing_price_discounted(map: &Map) -> usize {
+    let mut visits = HashSet::new();
+    map.iter()
+        .map(|(pos, c)| {
+            let mut region = HashSet::new();
+            let (area, _) = search(map, pos, c, &mut visits, &mut region);
+            area * apply_discount(region)
+        })
         .sum()
 }
 
@@ -69,4 +140,5 @@ fn main() {
              .chars().collect::<Vec<_>>()
             ).collect::<Vec<_>>());
     println!("{}", total_fencing_price(&map));
+    println!("{}", total_fencing_price_discounted(&map));
 }
