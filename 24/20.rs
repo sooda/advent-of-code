ecbca25477ef32c793ec15daef0582c3d9e8fbdf
Konsta Hölttä <sooda@iki.fi>
2024-12-20 23:19:47 +0200

    20b programs seem perplexed
    Try cheats of at most 20 picoseconds, not just two. After some silly
    attempts at trying to apply the cheat in dijkstra code to make it aware
    of those warps through walls, take the distance map from the initial
    search and use it to see how many steps would be saved if a direct
    manhattan distance would be taken for any two points along the road.
    
    This is still only slightly adapted from the first part; it would be
    better to try the nodes along the path found in the search instead of
    the map cells which is wasteful and suffers from seeing each path twice
    as mirrored pairs. 2.5 seconds for both parts is not too bad though from
    the previous multi-minute hack.

--- 24/20.rs
+++ 24/20.rs
@@ -64,7 +64,7 @@ type Distances = HashMap<State, usize>;
 // backwards to start
 type Edges = HashMap<State, HashSet<State>>;
 
-fn dijkstra(map: &Map, start: Pos, end: Pos) -> (Distances, Edges) {
+fn dijkstra(map: &Map, start: Pos) -> (Distances, Edges) {
     let mut heap: BinaryHeap::<(Reverse<usize>, State)> = BinaryHeap::new();
     let mut distances = Distances::new();
     let mut edges = Edges::new();
@@ -94,64 +94,63 @@ fn dijkstra(map: &Map, start: Pos, end: Pos) -> (Distances, Edges) {
     (distances, edges)
 }
 
-fn saving_cheats(map: &Map) -> usize {
+fn saving_cheats(map: &Map, maxlen: i32) -> usize {
     let start = map.iter().find(|&(_, ch)| ch == 'S').unwrap().0;
-    let end = map.iter().find(|&(_, ch)| ch == 'E').unwrap().0;
-    let (distances, _) = dijkstra(&map, start, end);
-    let fastest = distances[&State(end)];
+    let (distances, _) = dijkstra(&map, start);
     assert_eq!(map.w(), map.h());
     let mut saves = HashMap::new(); // to match with the example
     let mut good_cheat_count = 0;
-
     for cheety in 1..(map.h()-1) {
-        for cheetx in 1..(map.w()-2) {
+        for cheetx in 1..(map.w()-1) {
 
             let mut trycheat = |p1: Pos, p2: Pos| {
-                if !distances.contains_key(&State(p1)) && !distances.contains_key(&State(p2)) {
-                    // at least one of these must be on the path for the cheat to be valid
-                    return;
-                }
-                let mut map2 = map.clone();
-                map2[p1] = '.';
-                map2[p2] = '.';
-                let (distances2, _) = dijkstra(&map2, start, end);
-                if !distances2.contains_key(&State(p1)) || !distances2.contains_key(&State(p2)) {
-                    panic!();
+                if map.at(p1).is_none() || map.at(p2).is_none() {
                     return;
                 }
 
-                // if not traversing from empty node to unblocked wall, this did not help
-                if distances2[&State(p1)] > distances2[&State(p2)] && map[p1] == '#' {
-                    return;
-                }
-                if distances2[&State(p2)] > distances2[&State(p1)] && map[p2] == '#' {
+                if map[p1] == '#' || map[p2] == '#' {
                     return;
                 }
 
-                let fastest2 = distances2[&State(end)];
-                if fastest2 < fastest {
-                    let saved = fastest - fastest2;
+                // nodes are not walls, must have visited both
+                let d1 = distances[&State(p1)] as i32;
+                let d2 = distances[&State(p2)] as i32;
+
+                let manh = (p1.0 - p2.0).abs() + (p1.1 - p2.1).abs();
+                let saved = (d1 - d2).abs() - manh;
+                if saved > 0 {
                     *saves.entry(saved).or_insert(0) += 1;
                     if saved >= 100 {
                         good_cheat_count += 1;
-                    }
-
-                    if false {
-                        dump(&map2, &[p1, p2].into_iter().collect());
+                        if false {
+                            dump(&map, &[p1, p2].into_iter().collect());
+                        }
                     }
                 }
             };
 
-            // horiz pair
-            trycheat((cheetx, cheety), (cheetx + 1, cheety));
-
-            // vert pair
-            trycheat((cheety, cheetx), (cheety, cheetx + 1));
+            for qcheety in 1..(map.h()-1) {
+                for qcheetx in 1..(map.w()-1) {
+                    let dx = (cheetx - qcheetx).abs();
+                    let dy = (cheety - qcheety).abs();
+                    if dx + dy > maxlen {
+                        continue;
+                    }
+                    trycheat((cheetx, cheety), (qcheetx, qcheety));
+                }
+            }
         }
-        println!("{cheety} ...");
     }
 
-    good_cheat_count
+    if map.w() == 15 { // sample input
+        let mut saves = saves.iter().collect::<Vec<_>>();
+        saves.sort();
+        for s in saves {
+            println!("{} cheats that save {} ps", s.1 / 2, s.0);
+        }
+    }
+
+    good_cheat_count / 2 // symmetric search (FIXME, extra work)
 }
 
 fn dump(map: &Map, tiles: &HashSet<Pos>) {
@@ -168,5 +167,7 @@ fn main() {
     let map = Map(io::stdin().lock().lines()
         .map(|line| line.unwrap().chars().collect())
         .collect());
-    println!("{:?}", saving_cheats(&map));
+
+    println!("{:?}", saving_cheats(&map, 2));
+    println!("{:?}", saving_cheats(&map, 20));
 }
