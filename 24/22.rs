2b351b6409726aaa946237395e5ec75a7a0382b8
Konsta Hölttä <sooda@iki.fi>
2024-12-22 09:24:58 +0200

    22b just the ones digit
    Find the changes in price, and for each sequence of four changes, add
    the possible price matching the sequence to the seq price if that's the
    first of such sequence.
    
    Single digit diffs can be in range from 0-9=-9 to 9-0=9 so the [diff; 4]
    that in a way makes up a base-19 number is easily a space small enough
    to have as a hashmap key.

--- 24/22.rs
+++ 24/22.rs
@@ -1,21 +1,46 @@
-use std::io::{self, BufRead};
+#![feature(array_windows)]
 
-fn simulate(mut secret: u64, n: usize) -> u64 {
+use std::io::{self, BufRead};
+use std::collections::{HashMap, HashSet};
+
+fn simulate(mut secret: i64, n: usize) -> Vec<i64> {
+    let mut output = Vec::new();
+    output.push(secret);
     for _ in 0..n {
         secret = ((secret * 64) ^ secret) % 16777216;
         secret = ((secret / 32) ^ secret) % 16777216;
         secret = ((secret * 2048) ^ secret) % 16777216;
+        output.push(secret);
     }
-    secret
+    output
 }
 
-fn secret_sums(secrets: &[u64], n: usize) -> u64 {
-    secrets.iter().map(|&s| simulate(s, n)).sum()
+fn secret_sums(secrets: &[i64]) -> i64 {
+    secrets.iter().map(|&s| *simulate(s, 2000).last().unwrap()).sum()
+}
+
+fn most_bananas(secrets: &[i64]) -> i64 {
+    let mut seq_scores = HashMap::<[i8; 4], i64>::new();
+    for series in secrets.iter().map(|&s| simulate(s, 2000)) {
+        let deltas = series.iter()
+            .zip(series.iter().skip(1))
+            .map(|(a, b)| (b % 10 - a % 10) as i8).collect::<Vec<_>>();
+
+        let mut seen = HashSet::new();
+        for (secret, diffwin) in series.iter().skip(4).zip(deltas.array_windows::<4>()) {
+            if seen.insert(diffwin) {
+                *seq_scores.entry(*diffwin).or_insert(0) += *secret % 10;
+            }
+        }
+    }
+
+    *seq_scores.values().max().unwrap()
 }
 
 fn main() {
     let secrets: Vec<_> = io::stdin().lock().lines()
-        .map(|line| line.unwrap().parse::<u64>().unwrap())
+        .map(|line| line.unwrap().parse::<i64>().unwrap())
         .collect();
-    println!("{}", secret_sums(&secrets, 2000));
+    println!("{}", secret_sums(&secrets));
+    println!("{}", most_bananas(&secrets));
 }
