6bb8692b29a8c7c2d693827fcf3bbc827e481941
Konsta Hölttä <sooda@iki.fi>
2024-12-23 09:58:27 +0200

    23b go there yourself
    Find the largest complete subgraph of the whole graph to identify the
    LAN party.
    
    Looking at the sample input and the quite consistent number of neighbors
    in the real input, the LAN party probably has as many computers as those
    nodes have neighbors, so cheat a bit with a solution that only works for
    this easily-formed input: for each node N, for each neighbor M, observe
    a subgraph of N and all its friends except M. Add one to the count of
    that subgraph. Then after going through all nodes, the subgraph with the
    largest count is the LAN party.
    
    For a more general solution done this way we'd have to look at subgraphs
    of not just one neighbor removed but all possible combinations; this
    would be rather excessive and a better solution could be in order.
    
    A picture of this case for demonstration, with example max size 4:
    
         ...    ...
           \    /
            +--+
            |\/|
            |/\|
            +--+
           /    \
         ...    ...

--- 24/23.rs
+++ 24/23.rs
@@ -1,7 +1,10 @@
 use std::io::{self, BufRead};
 use std::collections::{HashMap, HashSet};
 
-fn three_sets_t(connections: &[(String, String)]) -> usize {
+type Nodes<'a> = HashSet<&'a str>;
+type Edges<'a> = HashMap<&'a str, Vec<&'a str>>;
+
+fn graphize(connections: &[(String, String)]) -> (Nodes, Edges) {
     let mut nodes = HashSet::<&str>::new();
     let mut edges = HashMap::<&str, Vec<&str>>::new();
     for (a, b) in connections {
@@ -10,10 +13,13 @@ fn three_sets_t(connections: &[(String, String)]) -> usize {
         edges.entry(a).or_insert(Vec::new()).push(b);
         edges.entry(b).or_insert(Vec::new()).push(a);
     }
+    (nodes, edges)
+}
 
+fn three_sets_t(nodes: &Nodes, edges: &Edges) -> usize {
     let mut sets = HashSet::<[&str; 3]>::new();
     // for each x, find prev-x-next so that also prev-next is a link
-    for &x in &nodes {
+    for &x in nodes {
         for prev in &edges[x] {
             for next in &edges[x] {
                 if edges[prev].contains(&next) {
@@ -29,6 +35,24 @@ fn three_sets_t(connections: &[(String, String)]) -> usize {
     sets.len()
 }
 
+fn password(nodes: &Nodes, edges: &Edges) -> String {
+    let mut subgraphs = HashMap::new();
+    for n in nodes {
+        for i in 0..edges[n].len() {
+            let mut subgraph = edges[n].clone();
+            subgraph.remove(i);
+            subgraph.push(n);
+            subgraph.sort();
+            *subgraphs.entry(subgraph).or_insert(0) += 1;
+        }
+    }
+
+    subgraphs.into_iter()
+        .max_by_key(|(_graph, count)| *count)
+        .unwrap().0
+        .join(",")
+}
+
 fn parse(line: &str) -> (String, String) {
     let mut sp = line.split('-');
     let a = sp.next().unwrap().to_string();
@@ -40,5 +64,7 @@ fn main() {
     let connections: Vec<_> = io::stdin().lock().lines()
         .map(|line| parse(&line.unwrap()))
         .collect();
-    println!("{}", three_sets_t(&connections));
+    let (nodes, edges) = graphize(&connections);
+    println!("{}", three_sets_t(&nodes, &edges));
+    println!("{}", password(&nodes, &edges));
 }
