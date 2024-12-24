90f6cc7315cdc70504dc5f501028f0ed326a243c
Konsta Hölttä <sooda@iki.fi>
2024-12-24 15:20:49 +0200

    24b inspect the monitoring device more closely
    The circuit is a series of 1-bit adders but four pairs of gates have
    their wires swapped. Trying each possible pair combination would take
    forever, so let's instead look at the circuit and observe how things can
    go wrong. Use the following heuristics to find bad wires and collect
    them into the final list; the pair information is not necessary.
    
    - a Z has to be an output of a XOR
    - an AND must go to OR, except for the first X and Y
    - an OR has to be an output of an AND
    - an XOR inputting X or Y must output to a XOR that outputs to a Z
    - an XOR outputting Z must input a XOR that inputs X or Y
    
    This is sufficient at least for my input. If a wire is found that
    contradicts with the above, that wire must be misplaced.

--- 24/24.rs
+++ 24/24.rs
@@ -1,16 +1,16 @@
 #![feature(let_chains)]
 
 use std::io::{self, Read};
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Clone)]
 enum GateOp {
     And,
     Or,
     Xor
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Gate {
     op: GateOp,
     in_a: String,
@@ -62,6 +62,135 @@ fn simulate_z(device: &Device) -> u64 {
     z_number(&states)
 }
 
+fn xy_into_z(x: u64, y: u64) -> States {
+    let mut states = States::new();
+    for i in 0..64 {
+        states.insert(format!("x{i:0>2}"), (x & (1 << i)) != 0);
+        states.insert(format!("y{i:0>2}"), (y & (1 << i)) != 0);
+    }
+    states
+}
+
+fn swapped_pairs(device: &Device) -> String {
+    if false {
+        for ai in 0..45 {
+            for bi in 0..45 {
+                let a = 1 << ai;
+                let b = 1 << bi;
+                let z = a + b;
+                let states = xy_into_z(a, b);
+                if simulate_z(&Device { states, gates: device.gates.clone() }) != z {
+                    panic!("boo");
+                }
+            }
+        }
+    }
+
+    let mut confusing = HashSet::<String>::new();
+    for gate in &device.gates {
+        if gate.out.starts_with("z") {
+            let x = gate.out.replace("z", "x");
+            // this isn't the final carry bit?
+            if device.gates.iter().any(|g| g.in_a == x || g.in_b == x) {
+                if gate.op != GateOp::Xor {
+                    println!("z {} gets input from {:?}", gate.out, gate.op);
+                    confusing.insert(gate.out.clone());
+                }
+            }
+        }
+        match gate.op {
+            // each and goes only to or
+            // except the x00&y00 goes to and, Xor
+            // that would be the or outputs
+            // because the or is optimized away
+            // because there is no carry bit yet
+            // (skt in my graph)
+            GateOp::And => {
+                if !(gate.in_a == "x00" || gate.in_a == "y00") {
+                    for g in &device.gates {
+                        if gate.out == g.in_a || gate.out == g.in_b {
+                            if g.op != GateOp::Or {
+                                println!("wire {} looks confusing, goes from and to {:?}", gate.out, g.op);
+                                confusing.insert(gate.out.clone());
+                            }
+                        }
+                    }
+                }
+            },
+            // each or receives only and
+            GateOp::Or => {
+                for g in &device.gates {
+                    if gate.in_a == g.out || gate.in_b == g.out {
+                        if g.op != GateOp::And {
+                            println!("wire {} looks confusing, goes to or from {:?}", g.out, g.op);
+                            confusing.insert(g.out.clone());
+                        }
+                    }
+                }
+            },
+            GateOp::Xor => {
+                // for a wire connected to a xor:
+                // - wire is x or y and input
+                // - or wire is z and output
+                // - or downstream is xor to z
+                // - or upstream is xor from x/y
+                // anyway let's try starting from x and y
+                // that did not work, in between can be ok but if z isnt z then error
+                let xor_in_matches = |name: &str, fun: &mut dyn FnMut(&Gate) -> bool| {
+                    for g in &device.gates {
+                        if (g.in_a == name || g.in_b == name) && g.op == GateOp::Xor && fun(g) {
+                            return true;
+                        }
+                    }
+                    return false;
+                };
+                if gate.in_a.starts_with("x") || gate.in_a.starts_with("y") {
+                    if !xor_in_matches(&gate.out, &mut |xorgate| {
+                        if xorgate.out.starts_with("z") {
+                            true
+                        } else {
+                            println!("wire {} looks confusing, is not z", xorgate.out);
+                            confusing.insert(xorgate.out.clone());
+                            false
+                        }
+                    }) {
+                        // hmm, not too reliable?
+                        // println!("wire {} looks confusing, downstream bad", gate.out);
+                    }
+                }
+
+                let upstream_xor_matches = |g: &Gate, fun: &mut dyn FnMut(&Gate) -> bool| {
+                    for parent in &device.gates {
+                        if (g.in_a == parent.out || g.in_b == parent.out) && parent.op == GateOp::Xor && fun(parent) {
+                            return true;
+                        }
+                    }
+                    return false;
+                };
+
+                if gate.out.starts_with("z") {
+                    if !upstream_xor_matches(&gate, &mut |xorgate: &Gate| {
+                        if xorgate.in_a.starts_with("x") || xorgate.in_a.starts_with("y") {
+                            true
+                        } else {
+                            println!("wire {} looks confusing, is not between input xor and output xor", xorgate.out);
+                            confusing.insert(xorgate.out.clone());
+                            false
+                        }
+                    }) {
+                        // hmm, not too reliable?
+                        //println!("NO x or y found for {:?}", gate);
+                    }
+                }
+            },
+        }
+    }
+
+    let mut confusing = confusing.into_iter().collect::<Vec<_>>();
+    confusing.sort();
+    confusing.join(",")
+}
+
 fn parse_gate(line: &str) -> Gate {
     // x00 AND y00 -> z00
     let mut words = line.split(' ');
@@ -94,5 +223,20 @@ fn main() {
     let mut file = String::new();
     io::stdin().read_to_string(&mut file).unwrap();
     let device = parse(&file);
-    println!("{}", simulate_z(&device));
+    if false {
+        println!("digraph G {{");
+        for g in &device.gates {
+            println!("{} [label={} shape=circle]", g.in_a, g.in_a);
+            println!("{} [label={} shape=circle]", g.in_b, g.in_b);
+            println!("{} [label={} shape=circle]", g.out, g.out);
+            println!("{}{:?}{} [label={:?} shape=rect]", g.in_a, g.op, g.in_b, g.op);
+            println!("{} -> {}{:?}{}", g.in_a, g.in_a, g.op, g.in_b);
+            println!("{} -> {}{:?}{}", g.in_b, g.in_a, g.op, g.in_b);
+            println!("{}{:?}{} -> {}", g.in_a, g.op, g.in_b, g.out);
+        }
+        println!("}}");
+    } else {
+        println!("{}", simulate_z(&device));
+        println!("{}", swapped_pairs(&device));
+    }
 }
