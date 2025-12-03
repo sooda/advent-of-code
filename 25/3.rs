a9f1080a576cc157317c9dbf8820bc9896a73c6d
Konsta Hölttä <sooda@iki.fi>
2025-12-03 08:59:32 +0200

    3b overcome the static friction
    Now the max joltage is composed of 12 batteries, not just two. That's
    too many to bruteforce through. For each digit with N other digits of 12
    remaining, find the max of 1) that digit and the same max of the next
    with N-1, or 2) the next with N remaining, skipping that one. Memoize
    the results to save on repetition.
    
    This is rather slow, might be better to start from the other end or do
    some heuristics on finding the greatest digit with still enough digits
    on the right.

--- 25/3.rs
+++ 25/3.rs
@@ -1,22 +1,44 @@
 use std::io::{self, BufRead};
+use std::collections::HashMap;
 
-fn max_joltage(bank: &[i32]) -> i32 {
-    bank.iter().enumerate()
-        .flat_map(|(i, &a)| bank.iter().skip(i + 1).map(move |&b| (a, b)))
-        .map(|(a, b)| 10 * a + b)
-        .max()
-        .unwrap()
+fn find_max<'a>(bank: &'a [i64], remaining: usize, mem: &mut HashMap<(&'a [i64], usize), i64>) -> Option<i64> {
+    if let Some(&m) = mem.get(&(bank, remaining)) {
+        Some(m)
+    } else if remaining > bank.len() {
+        // cannot satisfy
+        None
+    } else if remaining == 0 {
+        // ok, nothing to add to call site
+        Some(0)
+    } else {
+        let use_digit = 10i64.pow(remaining as u32 - 1) * bank[0] + (1..bank.len())
+            .flat_map(|i| find_max(&bank[i..], remaining - 1, mem))
+            .max()
+            .unwrap_or(0);
+        let skip_digit = (1..bank.len())
+            .flat_map(|i| find_max(&bank[i..], remaining, mem))
+            .max()
+            .unwrap_or(0);
+        let m = use_digit.max(skip_digit);
+        mem.insert((bank, remaining), m);
+        Some(m)
+    }
 }
 
-fn total_joltage(ratings: &[Vec<i32>]) -> i32 {
+fn max_joltage(bank: &[i64], batteries: usize) -> i64 {
+    find_max(bank, batteries, &mut HashMap::new()).unwrap()
+}
+
+fn total_joltage(ratings: &[Vec<i64>], batteries: usize) -> i64 {
     ratings.iter()
-        .map(|r| max_joltage(r))
+        .map(|r| max_joltage(r, batteries))
         .sum()
 }
 
 fn main() {
-    let ratings: Vec<Vec<i32>> = io::stdin().lock().lines()
+    let ratings: Vec<Vec<i64>> = io::stdin().lock().lines()
         .map(|line| line.unwrap().bytes().map(|b| (b - b'0').into()).collect())
         .collect();
-    println!("{}", total_joltage(&ratings));
+    println!("{}", total_joltage(&ratings, 2));
+    println!("{}", total_joltage(&ratings, 12));
 }
