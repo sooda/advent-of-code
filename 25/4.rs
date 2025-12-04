fd6da0e229d87ac54efd6a7e027bbe1a48a9177e
Konsta Hölttä <sooda@iki.fi>
2025-12-04 07:21:48 +0200

    4 printing department
    Observe each roll of paper and count the ones whose immediate
    surroundings include less than four other rolls of paper.
    
    Again a vec^2 isn't a good structure for a map like this with bounds
    checks. Using get() wouldn't work because 0-1 on usize isn't great.

---
+++ 25/4.rs
@@ -0,0 +1,36 @@
+use std::io::{self, BufRead};
+
+fn paper(map: &[Vec<char>], pos: (i32, i32), dir: (i32, i32)) -> bool {
+    let w = map[0].len() as i32;
+    let h = map.len() as i32;
+    let pos = (pos.0 + dir.0, pos.1 + dir.1);
+    let bad = pos.0 < 0 || pos.0 >= w || pos.1 < 0 || pos.1 >= h;
+    !bad && map[pos.1 as usize][pos.0 as usize] == '@'
+}
+
+fn paper_rolls(map: &[Vec<char>], pos: (i32, i32)) -> usize {
+    let delta = &[
+        (-1, -1), (0, -1), (1, -1),
+        (-1,  0),          (1,  0),
+        (-1,  1), (0,  1), (1,  1),
+    ];
+    delta.into_iter().filter(|&&d| paper(map, pos, d)).count()
+}
+
+fn forklift_accessible(map: &[Vec<char>]) -> usize {
+    let w = map[0].len();
+    let h = map.len();
+    (0..h)
+        .flat_map(|y| (0..w).map(move |x| (x, y)))
+        .filter(|&(x, y)| map[y][x] == '@')
+        .filter(|&(x, y)| paper_rolls(map, (x as i32, y as i32)) < 4)
+        .count()
+}
+
+fn main() {
+    let map: Vec<Vec<char>> = io::stdin().lock().lines()
+        .map(
+            |line| line.unwrap().chars().collect()
+            ).collect();
+    println!("{}", forklift_accessible(&map));
+}
