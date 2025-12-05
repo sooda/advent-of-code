e0955b63587290ef7dd1834f0322c21df42012e8
Konsta Hölttä <sooda@iki.fi>
2025-12-05 20:21:07 +0200

    3: speedup memoization
    The bank digits are small so store u8 instead of i64. More cpu cache
    friendly. Also do the remaining len checks first to avoid lookups.

--- 25/3.rs
+++ 25/3.rs
@@ -1,17 +1,17 @@
 use std::io::{self, BufRead};
 use std::collections::HashMap;
 
-fn find_max<'a>(bank: &'a [i64], remaining: usize, mem: &mut HashMap<(&'a [i64], usize), i64>) -> Option<i64> {
-    if let Some(&m) = mem.get(&(bank, remaining)) {
-        Some(m)
-    } else if remaining > bank.len() {
+fn find_max<'a>(bank: &'a [u8], remaining: usize, mem: &mut HashMap<(&'a [u8], usize), i64>) -> Option<i64> {
+    if remaining > bank.len() {
         // cannot satisfy
         None
     } else if remaining == 0 {
         // ok, nothing to add to call site
         Some(0)
+    } else if let Some(&m) = mem.get(&(bank, remaining)) {
+        Some(m)
     } else {
-        let use_digit = 10i64.pow(remaining as u32 - 1) * bank[0] + (1..bank.len())
+        let use_digit = 10i64.pow(remaining as u32 - 1) * i64::from(bank[0]) + (1..bank.len())
             .flat_map(|i| find_max(&bank[i..], remaining - 1, mem))
             .max()
             .unwrap_or(0);
@@ -25,19 +25,19 @@ fn find_max<'a>(bank: &'a [i64], remaining: usize, mem: &mut HashMap<(&'a [i64],
     }
 }
 
-fn max_joltage(bank: &[i64], batteries: usize) -> i64 {
+fn max_joltage(bank: &[u8], batteries: usize) -> i64 {
     find_max(bank, batteries, &mut HashMap::new()).unwrap()
 }
 
-fn total_joltage(ratings: &[Vec<i64>], batteries: usize) -> i64 {
+fn total_joltage(ratings: &[Vec<u8>], batteries: usize) -> i64 {
     ratings.iter()
         .map(|r| max_joltage(r, batteries))
         .sum()
 }
 
 fn main() {
-    let ratings: Vec<Vec<i64>> = io::stdin().lock().lines()
-        .map(|line| line.unwrap().bytes().map(|b| (b - b'0').into()).collect())
+    let ratings: Vec<Vec<u8>> = io::stdin().lock().lines()
+        .map(|line| line.unwrap().bytes().map(|b| b - b'0').collect())
         .collect();
     println!("{}", total_joltage(&ratings, 2));
     println!("{}", total_joltage(&ratings, 12));
