2da4cdd18f83e9a66b54c16fb90dd24b4dfe8847
Konsta Hölttä <sooda@iki.fi>
2025-12-06 08:56:36 +0200

    6b how to read cephalopod math
    Store just the lines after all. Scan the characters back to front and
    sum up once the operator arrives so we know what to do with the
    collected data that is both summed and multiplied temporarily. This ugly
    imperative manual transpose is not my best work.

--- 25/6.rs
+++ 25/6.rs
@@ -1,15 +1,50 @@
 use std::io::{self, BufRead};
 
-fn grand_total(input: &[Vec<String>]) -> i64 {
+fn grand_total_human(input: &[String]) -> i64 {
+    let input = input.iter().map(|line| {
+        line.split_whitespace()
+            .collect::<Vec<_>>()
+    }).collect::<Vec<_>>();
+    let operators = input.last().unwrap();
+    operators.iter()
+        .enumerate()
+        .map(|(i, op)| {
+            let inp = input.iter()
+                .take(input.len() - 1)
+                .map(|row| row[i].parse::<i64>().unwrap());
+            match op as &str {
+                "+" => inp.sum(),
+                "*" => inp.fold(1, |acc, x| acc * x),
+                _ => panic!("odd operator"),
+            }
+        })
+    .sum()
+}
+
+fn grand_total_cephalopod(input: &[String]) -> i64 {
+    let cols = input[0].len();
+    let operators = input.last().unwrap().as_bytes();
     let mut tot = 0;
-    for (i, op) in input.last().unwrap().iter().enumerate() {
-        let inp = input.iter()
+    let mut sum = 0;
+    let mut prod = 1;
+    for col in (0..cols).rev() {
+        let numb = input.iter()
             .take(input.len() - 1)
-            .map(|row| row[i].parse::<i64>().unwrap());
-        tot += match op as &str {
-            "+" => inp.sum(),
-            "*" => inp.fold(1, |acc, x| acc * x),
-            _ => panic!("odd operator"),
+            .map(|i| i.as_bytes()[col] as char)
+            .collect::<String>();
+        if numb.trim() == "" {
+            sum = 0;
+            prod = 1;
+        } else {
+            let num = numb.trim().parse::<i64>().unwrap();
+            sum += num;
+            prod *= num;
+            match operators[col] as char {
+                '+' => tot += sum,
+                '*' => tot += prod,
+                ' ' => {},
+                _ => panic!("bad operator"),
+            };
         }
     }
     tot
@@ -17,10 +52,8 @@ fn grand_total(input: &[Vec<String>]) -> i64 {
 
 fn main() {
     let input = io::stdin().lock().lines()
-        .map(|line| line.unwrap()
-             .split_whitespace()
-             .map(|a| a.to_owned()).collect()
-         )
+        .map(|line| line.unwrap())
         .collect::<Vec<_>>();
-    println!("{}", grand_total(&input));
+    println!("{}", grand_total_human(&input));
+    println!("{}", grand_total_cephalopod(&input));
 }
