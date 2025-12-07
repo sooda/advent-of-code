8714d05c02d43481eb42a6c9254502ebffbc7e05
Konsta Hölttä <sooda@iki.fi>
2025-12-07 08:26:00 +0200

    7 laboratories
    Beam-splitters form a tree-like directed graph. Some paths cross later
    and could cause duplicates in counting splits by unique paths, so count
    how many new splits happen from a single position and skip revisits by
    keeping track of visited splitter positions. This might have been easier
    as BFS but it's now DFS and it's fine.

---
+++ 25/7.rs
@@ -0,0 +1,32 @@
+use std::io::{self, BufRead};
+use std::collections::HashSet;
+
+type Map = Vec<Vec<char>>;
+
+fn splits(input: &Map, x: usize, y: usize, visited: &mut HashSet<(usize, usize)>) -> usize {
+    if y == input.len() {
+        0
+    } else if visited.contains(&(x, y)) {
+        0
+    } else if input[y][x] == '.' {
+        splits(input, x, y + 1, visited)
+    } else if input[y][x] == '^' {
+        visited.insert((x, y));
+        // safe, the input does not have splitters at the edges
+        1 + splits(input, x - 1, y + 1, visited) + splits(input, x + 1, y + 1, visited)
+    } else {
+        panic!("invalid input")
+    }
+}
+
+fn total_splits(input: &Map) -> usize {
+    let start = input[0].iter().position(|&a| a == 'S').unwrap();
+    splits(&input, start, 1, &mut HashSet::new())
+}
+
+fn main() {
+    let input = io::stdin().lock().lines()
+        .map(|line| line.unwrap().chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+    println!("{}", total_splits(&input));
+}
