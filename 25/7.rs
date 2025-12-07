ed6e729b907222178c4b97d4e2184c97a1ff25f4
Konsta Hölttä <sooda@iki.fi>
2025-12-07 08:35:06 +0200

    7b replace the broken manifold
    Now instead of counting splits, count the number of unique paths to the
    end. Fork the part 1 DFS and instead of skipping memoized positions, add
    up the number of memoized possible paths (timelines).

--- 25/7.rs
+++ 25/7.rs
@@ -1,5 +1,6 @@
 use std::io::{self, BufRead};
 use std::collections::HashSet;
+use std::collections::HashMap;
 
 type Map = Vec<Vec<char>>;
 
@@ -24,9 +25,32 @@ fn total_splits(input: &Map) -> usize {
     splits(&input, start, 1, &mut HashSet::new())
 }
 
+fn timelines(input: &Map, x: usize, y: usize, visited: &mut HashMap<(usize, usize), usize>) -> usize {
+    if y == input.len() {
+        1
+    } else if let Some(&x) = visited.get(&(x, y)) {
+        x
+    } else if input[y][x] == '.' {
+        timelines(input, x, y + 1, visited)
+    } else if input[y][x] == '^' {
+        // safe, the input does not have splitters at the edges
+        let n = timelines(input, x - 1, y + 1, visited) + timelines(input, x + 1, y + 1, visited);
+        visited.insert((x, y), n);
+        n
+    } else {
+        panic!("invalid input")
+    }
+}
+
+fn total_timelines(input: &Map) -> usize {
+    let start = input[0].iter().position(|&a| a == 'S').unwrap();
+    timelines(&input, start, 1, &mut HashMap::new())
+}
+
 fn main() {
     let input = io::stdin().lock().lines()
         .map(|line| line.unwrap().chars().collect::<Vec<_>>())
         .collect::<Vec<_>>();
     println!("{}", total_splits(&input));
+    println!("{}", total_timelines(&input));
 }
