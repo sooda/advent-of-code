af50939b2010b5a282fe78cf78af7dac9258dfaa
Konsta Hölttä <sooda@iki.fi>
2025-12-08 21:05:19 +0200

    8b enough extension cables
    Now the question is how many connections are needed until there is only
    one circuit. Any connections after that are just extra. The first part's
    logic gives results for n=1000, we don't know n, so reuse the logic and
    binary search the correct n and take the password-like X coordinates of
    the last two junction boxes connected.
    
    This is an awful hack; a proper iterative solution shall be devised at
    once.

--- 25/8.rs
+++ 25/8.rs
@@ -19,7 +19,7 @@ fn join(adjmap: &HashMap<usize, Vec<usize>>, visited: &mut HashSet<usize>, i: us
     }
 }
 
-fn three_largest_mul(boxes: &[Point], pairings: usize) -> i64 {
+fn stuff(boxes: &[Point], pairings: usize) -> (Vec<i64>, Vec<(i64, usize, usize)>) {
     let mut pairs = boxes.iter()
         .enumerate()
         .flat_map(|(i, &bi)| {
@@ -40,6 +40,13 @@ fn three_largest_mul(boxes: &[Point], pairings: usize) -> i64 {
             .and_modify(|neighs: &mut Vec<_>| neighs.push(i))
             .or_insert(vec![i]);
     }
+    // pad with zero to make the groups set decreasing with increasing pairings
+    for &(_di, i, j) in pairs.iter().skip(pairings) {
+        adjmap.entry(i)
+            .or_insert(vec![]);
+        adjmap.entry(j)
+            .or_insert(vec![]);
+    }
     let mut groups = HashSet::new();
     for (i, _js) in &adjmap {
         let mut visits = HashSet::new();
@@ -52,7 +59,34 @@ fn three_largest_mul(boxes: &[Point], pairings: usize) -> i64 {
         .map(|g| -(g.len() as i64))
         .collect::<Vec<_>>();
     lengths.sort_unstable();
+    (lengths, pairs)
+}
+
+fn three_largest_mul(boxes: &[Point], pairings: usize) -> i64 {
+    let (lengths, pairs) = stuff(boxes, pairings);
     -lengths[0] * lengths[1] * lengths[2]
+}
+
+fn last_connection_xs(boxes: &[Point]) -> i64 {
+    let mut lo = 2;
+    let mut hi = boxes.len() * boxes.len();
+    // binary search: under X too much groups, at or over X only one group
+    // YYYNNN
+    //   LH   at the edge, and lo = mid + 1 becomes the first N
+    let mut circuiting_pair = None;
+    while lo <= hi {
+        let mid = (lo + hi) / 2;
+        let (lengths, pairs) = stuff(boxes, mid);
+        circuiting_pair = pairs.get(mid).copied();
+        let not_yet = lengths.len() != 1;
+        if not_yet {
+            lo = mid + 1;
+        } else {
+            hi = mid - 1;
+        }
+    }
+    let (_, a, b) = circuiting_pair.unwrap();
+    boxes[a].0 * boxes[b].0
 }
 
 fn parse(line: &str) -> Point {
@@ -67,5 +101,5 @@ fn main() {
         .map(|line| parse(&line.unwrap()))
         .collect::<Vec<_>>();
     println!("{:?}", three_largest_mul(&boxes, 10));
-    println!("{:?}", three_largest_mul(&boxes, 1000));
+    println!("{:?}", last_connection_xs(&boxes));
 }
