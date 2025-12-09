95ea37cc5114fbfc5ecfb36d675a03921fb1974a
Konsta Hölttä <sooda@iki.fi>
2025-12-09 22:30:23 +0200

    9b the elves just remembered
    Now not all tiles can contribute to the grand-area rectangle but only
    those that are within the region specified by the given input that is
    like fencepost coordinates. The area is not necessarily even convex. Go
    over each pair again and filter out rectangles that would be cut by any
    line in the input.
    
    Somehow this works; the input happens to be sufficiently convex. With
    e.g. a T-shaped region the "best" might still be found from the bottom
    left or right but it couldn't be the best because the area would be
    outside the valid region.

--- 25/9.rs
+++ 25/9.rs
@@ -13,6 +13,58 @@ fn greatest_area(tiles: &[Pos]) -> i64 {
         .max().unwrap()
 }
 
+fn axis_flip(p: Pos) -> Pos {
+    (p.1, p.0)
+}
+
+// a > b completely, "inside box from this corner"
+fn gt(a: Pos, b: Pos) -> bool {
+    a.0 > b.0 && a.1 > b.1
+}
+
+// as a and b specify a rectangle, get top left and bottom right
+fn psort(a: Pos, b: Pos) -> (Pos, Pos) {
+    ((a.0.min(b.0), a.1.min(b.1)), (a.0.max(b.0), a.1.max(b.1)))
+}
+
+// e slices box vertically? don't consider left/right edges
+fn vert_crosses(e: (Pos, Pos), a: Pos, b: Pos) -> bool {
+    e.0.0 > a.0 && e.1.0 < b.0 && e.0.1 <= a.1 && e.1.1 >= b.1
+}
+
+// e.0 inside box, not on the edges?
+fn head_crosses(e: (Pos, Pos), a: Pos, b: Pos) -> bool {
+    gt(e.0, a) && gt(b, e.0)
+}
+
+fn crosses(e: (Pos, Pos), a: Pos, b: Pos) -> bool {
+    let (a, b) = psort(a, b);
+    let e = psort(e.0, e.1);
+    let x = head_crosses(e, a, b);
+    let y = head_crosses((e.1, e.0), a, b);
+    let z = vert_crosses(e, a, b);
+    let w = vert_crosses((axis_flip(e.0), axis_flip(e.1)), axis_flip(a), axis_flip(b));
+    x || y || z || w
+}
+
+fn crossing(edges: &[(Pos, Pos)], a: Pos, b: Pos) -> bool {
+    edges.iter().any(|&e| crosses(e, a, b))
+}
+
+fn greatest_noncrossing_area(tiles: &[Pos]) -> i64 {
+    // could just make these as we go but this is maybe simpler
+    let edges = tiles.iter()
+        .zip(tiles.iter().cycle().skip(1))
+        .map(|(&a, &b)| (a, b))
+        .collect::<Vec<(Pos, Pos)>>();
+    tiles.iter()
+        .enumerate()
+        .flat_map(|(i, &a)| tiles.iter().skip(i + 1).map(move |&b| (a, b)))
+        .filter(|&(a, b)| !crossing(&edges, a, b))
+        .map(|(a, b)| area(a, b))
+        .max().unwrap()
+}
+
 fn parse(line: &str) -> Pos {
     let ab = line.split_once(',').unwrap();
     (ab.0.parse().unwrap(), ab.1.parse().unwrap())
@@ -23,5 +75,5 @@ fn main() {
         .map(|line| parse(&line.unwrap()))
         .collect::<Vec<_>>();
     println!("{}", greatest_area(&tiles));
+    println!("{}", greatest_noncrossing_area(&tiles));
 }
-
